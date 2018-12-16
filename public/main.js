620541dc1da1feed2431ccafd8e15a0a8b7c105f
Alex Komoroske <jkomoros@gmail.com>
2018-12-16 14:30:05 -0800

    Actually extract the slide id from the URL if /s/SLUG-NAME.

--- public/main.js
+++ public/main.js
@@ -1,18 +1,22 @@
 
 var PRESENTATION_URL = "https://docs.google.com/presentation/d/196n9NQEGJmdVxsShE3b8dBz8UUIee4oC7e8SC193HcQ/present"
 
-
-function fallbackRedirect() {
-
-}
+var DEBUG_MODE = true;
 
 function redirectToSlides(slideId) {
 	if (!slideId) slideId = "id.p";
-	window.location.href = PRESENTATION_URL + "#slide=" + slideId;
+	//If it's debug mode, then only navigate if they confirm
+	if (!DEBUG_MODE || confirm(slideId)) window.location.href = PRESENTATION_URL + "#slide=" + slideId;
 }
 
 function extractSlideSlug() {
-	return "";
+	var path = window.location.pathname;
+	//Remove "/" from front
+	path = path.substring(1);
+	var pieces = path.split("/");
+	if (!pieces) return "";
+	if (pieces[0] != "s") return "";
+	return pieces[1];
 }
 
 function main() {
