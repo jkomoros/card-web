7b0b54a6ff81f939dcfcc0ac055242e1746195f2
Alex Komoroske <jkomoros@gmail.com>
2018-12-16 14:50:46 -0800

    Also extra warn

--- public/main.js
+++ public/main.js
@@ -21,13 +21,20 @@ function redirectToSlides(slideId) {
 }
 
 function extractSlideSlug() {
+
+	var result = ["", false];
+
 	var path = window.location.pathname;
 	//Remove "/" from front
 	path = path.substring(1);
 	var pieces = path.split("/");
-	if (!pieces) return "";
-	if (pieces[0] != "s") return "";
-	return pieces[1];
+	if (!pieces) return result;
+	if (pieces[0] == "s") result[0] = pieces[1];
+	if (pieces.length > 2) {
+		var warnPiece = pieces[2];
+		if (warnPiece && warnPiece.substring(0, 1) == "w") result[1] = true;
+	}
+	return result;
 }
 
 function fallbackRedirect(e) {
@@ -46,8 +53,9 @@ function main() {
 		resp.json().then(
 			function(json) {
 				SLIDE_DATA = json;
-				var slug = extractSlideSlug();
-				redirectToSlides(slug);
+				var result = extractSlideSlug();
+				console.log(result);
+				redirectToSlides(result[0]);
 			}
 		)
 	}).catch(
