acee24444e5cd1d9465204598185f7ead99c34d0
Alex Komoroske <jkomoros@gmail.com>
2018-12-16 14:17:28 -0800

    Have a fallback redirect.

--- public/main.js
+++ public/main.js
@@ -1,1 +1,11 @@
-console.log("sup");
+
+var PRESENTATION_URL = "https://docs.google.com/presentation/d/196n9NQEGJmdVxsShE3b8dBz8UUIee4oC7e8SC193HcQ/present#slide=id.p"
+
+
+function fallbackRedirect() {
+	window.location.href = PRESENTATION_URL;
+}
+
+//TODO: do more complex behavior
+fallbackRedirect();
+
