987cc4f3a81ae083d59dd2d45c38b92f3f31e5a0
Alex Komoroske <jkomoros@gmail.com>
2018-12-21 16:18:40 -0800

    Put in more realistic placeholder content. Part of #2.

--- webapp/src/components/my-view1.js
+++ webapp/src/components/my-view1.js
@@ -22,9 +22,12 @@ class MyView1 extends PageViewElement {
     return html`
       ${SharedStyles}
       <base-card>
-        <h1>This is some content</h1>
+        <h1>Collaborative debate is the best tool in complex spaces</h1>
         <section>
-          This is other content
+          <p>Adversarial debate is more familiar, but it presumes that the answer is one of two options. That only applies in complicated problem spaces, not complex.</p>
+          <p>The fundamental difference with collaborative debate is a <strong>positive-sum </strong>perspective. All participants must be <a card="g487aed6370_0_141">curious</a>, keep an open mind, and seek to understand. The debate should be <a card="g487aed6370_0_171">intellectually but not emotionally intense</a>.</p>
+          <p>No single person has the right skill sets, experiences, and perspectives to fully understand a complex problem space. Diversity of perspective (both underlying data and <a card="g487aed6370_0_111">lenses</a>) is the raw material for comprehensive <a card="g487aed6370_0_50">insight</a>.</p>
+          <p><strong>The conditions for collaborative debate are not natural occurring and must be </strong><strong><a card="g487aed6370_0_166">actively created</a></strong><em>. See also the essay </em><em><a href="https://medium.com/@komorama/debate-should-be-collaborative-not-combative-185ff37f1d34">Debate should be collaborative, not combative</a></em><em>.</em></p>
         </section>
       </base-card>
     `;
