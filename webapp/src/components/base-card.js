4b9176421813172a6c83408f69e37cefba0e5b13
Alex Komoroske <jkomoros@gmail.com>
2018-12-21 16:32:35 -0800

    Got rendering closer to real. The font of source sans doesn't appear to be used yet. Part of #2.

--- webapp/src/components/base-card.js
+++ webapp/src/components/base-card.js
@@ -7,7 +7,7 @@ class BaseCard extends LitElement {
       <style>
 
         @import url('https://fonts.googleapis.com/css?family=Raleway:400,700');
-        @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
+        @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700');
 
         :host {
           display:block;
@@ -15,27 +15,32 @@ class BaseCard extends LitElement {
           height: 540px;
           width: 960px;
           box-shadow: 0 1px 6px #CCC;
-          
+          box-sizing: border-box;
+          padding: 23px 33px;
         }
 
         .container {
           height:100%;
           width:100%;
-          box-sizing: border-box;
-          margin: 23px 33px;
+
         }
 
         ::slotted(h1) {
           font-family: 'Raleway', sans-serif;
           font-weight:bold;
           color: var(--app-primary-color);
+          margin-top:0;
         }
 
         ::slotted(section) {
           font-family: 'Source Sans Pro', sans-serif;
-          font-size: 18px;
+          font-size: 22px;
           color: var(--app-dark-text-color);
           background-color:transparent;
+        }
+
+        ::slotted(* a) {
+          color: var(--app-secondary-color);
         }
       </style>
       <div class="container">
