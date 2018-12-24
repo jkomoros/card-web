ebfacbf2d79308b7999b4b3831013f9ac7dcc9ba
Alex Komoroske <jkomoros@gmail.com>
2018-12-23 17:07:18 -0800

    Tweak button styles. Pretty ugly. Part of #11.

--- webapp/src/components/button-shared-styles.js
+++ webapp/src/components/button-shared-styles.js
@@ -15,9 +15,16 @@ export const ButtonSharedStyles = html`
   button {
     font-size: inherit;
     vertical-align: middle;
-    background: transparent;
+    color: var(--app-light-text-color);
+    background: var(--app-secondary-color);
+    padding: 0.5em;
+    box-shadow: 0 2px 2px #999;
     border: none;
     cursor: pointer;
+    margin: 0.5em;
+  }
+  button:hover {
+    box-shadow: 0 4px 4px #999;
   }
   button:hover svg {
     fill: var(--app-primary-color);
