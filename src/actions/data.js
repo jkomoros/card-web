b9ebbc74cbb9c992a46a3b3bd604b949e3a0c1ca
Alex Komoroske <jkomoros@gmail.com>
2018-12-29 05:57:16 -0800

    Make it so modifyCard will allow you to orphan a card. Part of #51.

--- src/actions/data.js
+++ src/actions/data.js
@@ -87,8 +87,7 @@ export const modifyCard = (card, update, substantive) => (dispatch, getState) =>
     cardUpdateObject.title = update.title;
   }
 
-  //It's never legal to not have a name, so only update if it's not falsey.
-  if (update.name) {
+  if (update.name !== undefined) {
     //TODO: really we should verify that this name is legal--that is, either the id or one of the slugs.
     cardUpdateObject.name = update.name;
   }
