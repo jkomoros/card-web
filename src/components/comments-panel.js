51b5213ddfa60ebc9c4d6b35c965c15977dfe7e8
Alex Komoroske <jkomoros@gmail.com>
2018-12-30 07:35:38 -0800

    Fix a bug introduced a few commits ago where the reply button was erroneously disabled. Part of #16.

--- src/components/comments-panel.js
+++ src/components/comments-panel.js
@@ -74,7 +74,7 @@ class CommentsPanel extends connect(store)(PageViewElement) {
         <h3>Comments</h3>
         <div class='comments'>
         ${repeat(this._composedThreads, (thread) => thread.id, (item, index) => html`
-                <comment-thread .userId=${this._userId} .thread=${item} @add-message='${this._handleAddMessage}' @edit-message='${this._handleEditMessage}' @resolve-thread=${this._handleResolveThread}.userMayComment=${this._userMayComment}></comment-thread>`)}
+                <comment-thread .userId=${this._userId} .thread=${item} @add-message='${this._handleAddMessage}' @edit-message='${this._handleEditMessage}' @resolve-thread=${this._handleResolveThread} .userMayComment=${this._userMayComment}></comment-thread>`)}
         </div>
         <button class='round' ?disabled='${!this._userMayComment}' title='${this._userMayComment ? 'Start new comment thread' : 'Sign in to start new comment thread'}' @click='${this._handleCreateThreadClicked}'>${addCommentIcon}</button>
       </div>
