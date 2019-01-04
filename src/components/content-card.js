jkomoros/card-web

commit fac104ccbd60de44ece598eba0b444e7de547ad8

Implement a total hack so that the findCardToLink can work with selection states and shadow roots. Part of #55. Part of #12. The work here should be refactored better later, which is captured in #91.

src/components/content-card.js
@@ -6,7 +6,8 @@ import './card-link.js';
 
 import {
   normalizeBodyToContentEditable,
-  normalizeBodyHTML
+  normalizeBodyHTML,
+  reportSelectionRange
 } from '../actions/editor.js';
 
 let loadingTemplate = html`<span class='loading'>Loading...<span>`
@@ -36,8 +37,18 @@ export class ContentCard extends BaseCard {
     this.dispatchEvent(new CustomEvent('body-updated', {composed:true, detail: {html: this._sectionElement.innerHTML}}));
   }
 
+  _selectionChanged(e) {
+    let selection = this.shadowRoot.getSelection();
+    if (!selection.focusNode) return;
+    reportSelectionRange(selection.getRangeAt(0));
+  }
+
   get _emptyTemplate() {
     return this.id ? blankTemplate : loadingTemplate;
+  }
+
+  firstUpdated() {
+    document.addEventListener('selectionchange', this._selectionChanged.bind(this));
   }
 
   _makeSection(body) {

src/actions/editor.js
@@ -24,6 +24,31 @@ import {
   isWhitespace
 } from './util.js';
 
+let lastReportedSelectionRange = null;
+//TODO: figure out a pattenr that doesn't have a single shared global
+let savedSelectionRange = null;
+
+//selection range is weird; you can only listen for changes at the document
+//level, but selections wihtin a shadow root are hidden from outside. Certain
+//opeartions, like execCommand, operate on a selection state--but if you have
+//to do actions in between, like pop a dialog to ask follow-up questions--your
+//selection will be lost. The solution we go with is that elements who might
+//have a content editable target (e.g. content-card in editing mode) report
+//their selected ranged when they have one here, globally.
+export const reportSelectionRange = (range) => {
+  lastReportedSelectionRange = range;
+}
+
+export const saveSelectionRange = () => {
+  savedSelectionRange = lastReportedSelectionRange;
+}
+
+export const restoreSelectionRange = () => {
+  let selection = document.getSelection();
+  selection.removeAllRanges();
+  selection.addRange(savedSelectionRange);
+}
+
 export const editingStart = () => (dispatch, getState) => {
   const state = getState();
   if (!userMayEdit(state)) {
@@ -79,6 +104,7 @@ export const linkCard = (card) => (dispatch, getState) => {
   if (!state.editor.editing) return;
   //TODO: it's weird we do this here, it really should be done on the card-
   //editor component.
+  restoreSelectionRange();
   document.execCommand('createLink', null, card.id);
 }
 

src/actions/find.js
@@ -3,6 +3,10 @@ export const FIND_DIALOG_CLOSE ='FIND_DIALOG_CLOSE';
 export const FIND_UPDATE_QUERY = 'FIND_UPDATE_QUERY';
 export const FIND_CARD_TO_LINK = 'FIND_CARD_TO_LINK';
 
+import {
+  saveSelectionRange
+} from './editor.js';
+
 export const openFindDialog = () => {
   return {
     type: FIND_DIALOG_OPEN
@@ -23,6 +27,7 @@ export const updateQuery = (query) => {
 }
 
 export const findCardToLink = () => {
+  saveSelectionRange();
   return {
     type: FIND_CARD_TO_LINK
   }

commit 7b26f935da79ac4a75a430755f9c3e8103604b31

Fix the bug introduced in fac104ccbd60de44ece598eba0b444e7de547ad8 where we didn't register the click handler for a's on cards we'd edit so links would navigate. Part of #55.

src/components/content-card.js
@@ -47,7 +47,8 @@ export class ContentCard extends BaseCard {
     return this.id ? blankTemplate : loadingTemplate;
   }
 
-  firstUpdated() {
+  firstUpdated(changedProps) {
+    super.firstUpdated(changedProps);
     document.addEventListener('selectionchange', this._selectionChanged.bind(this));
   }
 