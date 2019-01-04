cd59a74fbdad86ce43958b0a09b204495086e79e
Alex Komoroske <jkomoros@gmail.com>
2019-01-04 09:36:34 -0500

    Created find.query actions. Part of #12.

--- src/actions/find.js
+++ src/actions/find.js
@@ -1,6 +1,6 @@
 export const FIND_DIALOG_OPEN = 'FIND_DIALOG_OPEN';
 export const FIND_DIALOG_CLOSE ='FIND_DIALOG_CLOSE';
-
+export const FIND_UPDATE_QUERY = 'FIND_UPDATE_QUERY';
 
 export const openFindDialog = () => {
   return {
@@ -11,5 +11,12 @@ export const openFindDialog = () => {
 export const closeFindDialog = () => {
   return {
     type: FIND_DIALOG_CLOSE
+  }
+}
+
+export const updateQuery = (query) => {
+  return {
+    type: FIND_UPDATE_QUERY,
+    query
   }
 }
--- src/reducers/find.js
+++ src/reducers/find.js
@@ -1,10 +1,12 @@
 import {
   FIND_DIALOG_OPEN,
-  FIND_DIALOG_CLOSE
+  FIND_DIALOG_CLOSE,
+  FIND_UPDATE_QUERY
 } from '../actions/find.js';
 
 const INITIAL_STATE = {
-  open: false
+  open: false,
+  query: ""
 }
 
 const app = (state = INITIAL_STATE, action) => {
@@ -18,6 +20,11 @@ const app = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         open: false
+      }
+    case FIND_UPDATE_QUERY: 
+      return {
+        ...state,
+        query: action.query
       }
     default:
       return state;
