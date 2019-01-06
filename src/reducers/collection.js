dd0fcf5930eac3194b4e1ad1f74e9a35fb1dd4de
Alex Komoroske <jkomoros@gmail.com>
2019-01-06 00:32:54 -0500

    Make it so that when sections are updated we also save a filter. Part of #60.

--- src/reducers/collection.js
+++ src/reducers/collection.js
@@ -3,12 +3,17 @@ import {
   UPDATE_COLLECTION
 } from '../actions/collection.js';
 
+import {
+  UPDATE_SECTIONS
+} from '../actions/data.js';
+
 const DEFAULT_SET_NAME = 'default';
 
 export const SET_NAMES = [DEFAULT_SET_NAME];
 
 const INITIAL_STATE = {
   setName: DEFAULT_SET_NAME,
+  filters: {},
   requestedCard: "",
   activeSectionId: "",
   activeCardId: "",
@@ -30,9 +35,25 @@ const app = (state = INITIAL_STATE, action) => {
         ...state,
         setName: action.setName
       }
+    case UPDATE_SECTIONS:
+      return {
+        ...state,
+        filters: {...state.filters, ...makeFilterFromSection(action.sections)}
+      }
     default:
       return state;
   }
+}
+
+const makeFilterFromSection = (sections) => {
+  let result = {};
+  for (let key of Object.keys(sections)) {
+    let filter = {};
+    let section = sections[key];
+    section.cards.forEach(card => filter[card] = true)
+    result[key] = filter;
+  }
+  return result;
 }
 
 export const getSetName = (state) => state.collection.setName;
