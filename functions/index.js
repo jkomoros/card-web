2fbc293a069949e70ac577907d8313fdd3861858
Alex Komoroske <jkomoros@gmail.com>
2019-01-13 12:43:13 -0800

    Silence an annoying warning on every update. Part of #70.

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,7 @@ const functions = require('firebase-functions');
 
 const admin = require('firebase-admin');
 admin.initializeApp();
+admin.firestore().settings({timestampsInSnapshots:true});
 
 const db = admin.firestore();
 
