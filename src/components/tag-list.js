4a718f3f5f055d5cc67000bf9a150fa601fe96ea
Alex Komoroske <jkomoros@gmail.com>
2019-01-14 05:26:21 -0800

    Show a select drop down if editing is true. Actions not yet hooked up. Part of #61.

--- src/components/tag-list.js
+++ src/components/tag-list.js
@@ -13,12 +13,36 @@ class TagList  extends LitElement {
 		let effectivePreviousTags = this.previousTags && this.previousTags.length ? this.previousTags : this.tags;
 		let [additions, deletions] = arrayDiffAsSets(effectiveTags, effectivePreviousTags);
 		return html`
-			<div>
+			<style>
+				select {
+					display:none;
+				}
+				.editing select {
+					display:inline;
+				}
+			</style>
+			<div class='${this.editing ? 'editing' : ''}'>
 			${this.tags && this.tags.length ?
 		this.tags.map(item => html`<tag-chip .tag=${item} .addition=${additions[item]} .deletion=${deletions[item]} .editing=${this.editing} @remove-tag=${this._handleRemoveTag}></tag-chip>`) :
 		html`<em>No tags</em>`}
+			<select @change=${this._handleSelectChanged}>
+				<option value='#noop' selected>Add Tag...</option>
+				<option value='#new'>New Tag</option>
+			</select>
 			</div>
 			`;
+	}
+
+	_handleSelectChanged(e) {
+		let ele = e.composedPath()[0];
+		if (ele.value == "#noop") return;
+		if (ele.value == "#new") {
+			console.log('Adding a new tag is not yet implemented');
+		} else {
+			//Add a tag if it is a normal one
+		}
+		//Set it back to #noop.
+		ele.value = '#noop';
 	}
 
 	_handleRemoveTag(e) {
