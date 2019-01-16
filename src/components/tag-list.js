jkomoros/card-web

commit 580cc1731ce2de7618bf9a7d4cdac17500d3c0a1

Tags that have been edited also show up slightly differently. Needed to fix a bug in diffing for adding first tags. Part of #61.

src/components/tag-list.js
@@ -11,7 +11,7 @@ import {
 class TagList  extends LitElement {
 	render() {
 		let effectiveTags = this.tags;
-		let effectivePreviousTags = this.previousTags && this.previousTags.length ? this.previousTags : this.tags;
+		let effectivePreviousTags = this.previousTags && this.previousTags.length ? this.previousTags : [];
 		let [additionsArray, deletionsArray] = arrayDiff(effectivePreviousTags, effectiveTags);
 		let additions = arrayToSet(additionsArray);
 		let deletions = arrayToSet(deletionsArray);

src/components/tag-chip.js
@@ -17,6 +17,9 @@ class TagChip  extends LitElement {
 					font-style: italic;
 					text-decoration-line:line-through;
 				}
+				span.addition {
+					font-style:italic;
+				}
 				span a {
 					display:none;
 					color: var(--app-light-text-color);

commit e9e724c316328dcfbeedfa8da559dad2bfbf50de

Made it so tags in a non-editing list don't erroneously get styled as italic. Broken in 580cc1731ce2de7618bf9a7d4cdac17500d3c0a1. Part of #61.

src/components/tag-list.js
@@ -11,7 +11,7 @@ import {
 class TagList  extends LitElement {
 	render() {
 		let effectiveTags = this.tags;
-		let effectivePreviousTags = this.previousTags && this.previousTags.length ? this.previousTags : [];
+		let effectivePreviousTags = this.previousTags ? (this.previousTags.length ? this.previousTags : []) : effectiveTags;
 		let [additionsArray, deletionsArray] = arrayDiff(effectivePreviousTags, effectiveTags);
 		let additions = arrayToSet(additionsArray);
 		let deletions = arrayToSet(deletionsArray);