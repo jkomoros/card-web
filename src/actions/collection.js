jkomoros/card-web

commit aab7e02afd41af4a69b652d36806fc2c7781cfd5

Refactor selectors for active collection, because in a bit only the expanded collection will be correct because only it can be sorted. Part of #104.

src/actions/collection.js
@@ -22,7 +22,7 @@ import {
 	getIdForCard,
 	getCard,
 	selectDataIsFullyLoaded,
-	selectActiveCollection,
+	selectExpandedActiveCollection,
 	selectActiveSetName,
 	selectActiveCardId,
 	selectActiveSectionId,
@@ -226,7 +226,7 @@ export const redirectIfInvalidCardOrCollection = () => (dispatch, getState) => {
 	const state = getState();
 	if (!selectDataIsFullyLoaded(state)) return;
 	let card = selectActiveCard(state);
-	let collection = selectActiveCollection(state);
+	let collection = selectExpandedActiveCollection(state);
 	if (!card) {
 		
 		//If we get here, we could navigate to a default card (we know that the
@@ -258,7 +258,7 @@ export const showCard = (requestedCard) => (dispatch, getState) => {
 	//on the current collection.
 	if (cardIdIsPlaceholder(requestedCard)) {
 		if (!selectDataIsFullyLoaded(state)) return;
-		let collection = selectActiveCollection(state);
+		let collection = selectExpandedActiveCollection(state);
 		cardId = cardIdForPlaceholder(requestedCard, collection);
 		//If there's no valid card then give up.
 		if (!cardId) return;

src/selectors.js
@@ -370,7 +370,7 @@ const selectActiveBaseCollection = createSelector(
 );
 
 //selectActiveCollection includes start_cards if applicable, but only the cardIds.
-export const selectActiveCollection = createSelector(
+const selectActiveUnsortedCollection = createSelector(
 	selectActiveStartCards,
 	selectActiveBaseCollection,
 	(startCards, baseCollection) => [...startCards, ...baseCollection]
@@ -378,7 +378,7 @@ export const selectActiveCollection = createSelector(
 
 //Expanded means it includes the full cards in place.
 export const selectExpandedActiveCollection = createSelector(
-	selectActiveCollection,
+	selectActiveUnsortedCollection,
 	selectCards,
 	(collection, cards) => collection.map(id => cards[id] || null)
 );
@@ -415,13 +415,13 @@ export const selectActiveCollectionLabels = createSelector(
 
 export const selectActiveCardIndex = createSelector(
 	selectActiveCardId,
-	selectActiveCollection,
-	(cardId, collection) => collection.indexOf(cardId)
+	selectExpandedActiveCollection,
+	(cardId, collection) => collection.map(card => card.id).indexOf(cardId)
 );
 
 export const getCardIndexForActiveCollection = (state, cardId) => {
-	let collection = selectActiveCollection(state);
-	return collection.indexOf(cardId);
+	let collection = selectExpandedActiveCollection(state);
+	return collection.map(card => card.id).indexOf(cardId);
 };
 
 const SIMPLE_FILTER_REWRITES = ['is:', 'section:', 'tag:'];

commit cf9ce28cd3970bbf6f1b0e8f802b06a9c997692e

Fixed a bug introduced in aab7e02afd41af4a69b652d36806fc2c7781cfd5 where `_` placeholders wouldn't select the first item in the set. Part of #160.

src/actions/collection.js
@@ -268,10 +268,11 @@ const cardIdIsPlaceholder = (cardId) => {
 };
 
 const cardIdForPlaceholder = (requestedCard, collection) => {
+	//Collection is an expanded collection of cards, not card ids.
 	if (!cardIdIsPlaceholder(requestedCard)) return '';
 	if (!collection || !collection.length) return '';
 	//TODO: support random, _popular, _recent, etc.
-	return collection[0];
+	return collection[0].id;
 };
 
 export const redirectIfInvalidCardOrCollection = () => (dispatch, getState) => {