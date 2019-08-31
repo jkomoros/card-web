jkomoros/card-web

commit 6be6b075c60e7ef66e3f97072dc2f57a4bc53a58

card-preview grows the ability to show the current user's stars + reads. Not currently  used with the top-level card-preview. Part of #144.

src/components/card-preview.js
@@ -3,6 +3,7 @@ import { LitElement, html } from '@polymer/lit-element';
 import './card-renderer.js';
 import './star-count.js';
 import './thread-count.js';
+import './read-decorator.js';
 
 import { 
 	CARD_WIDTH_IN_EMS,
@@ -15,6 +16,10 @@ class CardPreview extends LitElement {
 		const cardHeightInPixels = CARD_HEIGHT_IN_EMS * this.previewSize;
 		const positionLeft = (this.x + cardWidthInPixels) > window.innerWidth;
 		const positionUp = (this.y + cardHeightInPixels) > window.innerHeight;
+
+		const starred = this.stars && this.card ? this.stars[this.card.id] : false;
+		const read = this.reads && this.card ? this.reads[this.card.id] : false;
+
 		return html`
 		<style>
 			:host {
@@ -42,8 +47,9 @@ class CardPreview extends LitElement {
       <div ?hidden='${!this.card}'>
 		<card-renderer .card=${this.card}></card-renderer>
 		<div class='decorators'>
-			<star-count .count=${this.card ? this.card.star_count : 0}></star-count>
+			<star-count .count=${this.card ? this.card.star_count : 0} .higlighted=${starred}></star-count>
 			<thread-count .count=${this.card ? this.card.thread_count : 0}></thread-count>
+			<read-decorator .visible=${read}></read-decorator>
 		</div>
       </div>
     `;
@@ -60,6 +66,8 @@ class CardPreview extends LitElement {
 			card: {type: Object},
 			x: { type: Number },
 			y: { type: Number },
+			stars: { type: Object },
+			reads: { type: Object },
 			/* size of font for card in px*/
 			previewSize: { type: Number },
 			/* offset from the cursor in pixels */

commit 6fea9f1bb35f4700e0412f9ed592cbc56efacbfe

Fixed a bug introduced in 6be6b075c60e7ef66e3f97072dc2f57a4bc53a58 where the user's personal stars wouldn't be highlighted. Part of #144.

src/components/card-preview.js
@@ -47,7 +47,7 @@ class CardPreview extends LitElement {
       <div ?hidden='${!this.card}'>
 		<card-renderer .card=${this.card}></card-renderer>
 		<div class='decorators'>
-			<star-count .count=${this.card ? this.card.star_count : 0} .higlighted=${starred}></star-count>
+			<star-count .count=${this.card ? this.card.star_count : 0} .highlighted=${starred}></star-count>
 			<thread-count .count=${this.card ? this.card.thread_count : 0}></thread-count>
 			<read-decorator .visible=${read}></read-decorator>
 		</div>