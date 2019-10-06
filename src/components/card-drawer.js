026bd17f027a4e959936f47a5252ed705f1c6455
Alex Komoroske <jkomoros@gmail.com>
2019-10-06 09:17:00 -0700

    Card-drawer passes the list of items that will be removed in the next filter commit down to the card-thumbnails (if it's provided). Part of #191.

--- src/components/card-drawer.js
+++ src/components/card-drawer.js
@@ -93,7 +93,7 @@ class CardDrawer extends LitElement {
 				${repeat(this.collection, (i) => i.id, (i, index) => html`
 					<div class='spacer' .index=${index} @dragover='${this._handleDragOver}' @dragenter='${this._handleDragEnter}' @dragleave='${this._handleDragLeave}' @drop='${this._handleDrop}'></div>
 					${this.labels && this.labels[index] ? html`<div class='label'><span>${this.labelName} <strong>${this.labels[index]}</strong></span></div>` : html``}
-					<card-thumbnail @dragstart='${this._handleDragStart}' @dragend='${this._handleDragEnd}' .card=${i} .userMayEdit=${this.editable} .id=${i.id} .name=${i.name} .title=${this._titleForCard(i)} .cardType=${i.card_type} .selected=${i.id == this.selectedCardId} .index=${index} .starred=${this.stars[i.id] || false} .read=${this.reads[i.id] || false}></card-thumbnail>`)}
+					<card-thumbnail @dragstart='${this._handleDragStart}' @dragend='${this._handleDragEnd}' .card=${i} .userMayEdit=${this.editable} .id=${i.id} .name=${i.name} .title=${this._titleForCard(i)} .cardType=${i.card_type} .selected=${i.id == this.selectedCardId} .index=${index} .starred=${this.stars[i.id] || false} .read=${this.reads[i.id] || false} .willBeRemovedOnPendingFilterCommit=${this.collectionItemsThatWillBeRemovedOnPendingFilterCommit[i.id] || false}></card-thumbnail>`)}
 				</div>
 				<button class='round' @click='${this._handleAddSlide}' ?hidden='${!this.editable}'>${plusIcon}</button>
 			</div>
@@ -106,6 +106,7 @@ class CardDrawer extends LitElement {
 		this.collection = [];
 		this.stars = {};
 		this.reads = {};
+		this.collectionItemsThatWillBeRemovedOnPendingFilterCommit = {};
 	}
 
 	_titleForCard(card) {
@@ -192,6 +193,7 @@ class CardDrawer extends LitElement {
 			selectedCardId: { type:String },
 			stars: { type: Object },
 			reads: { type: Object },
+			collectionItemsThatWillBeRemovedOnPendingFilterCommit: { type: Object },
 			reorderPending: {type:Boolean},
 			//_showing is more complicated than whether we're open or yet.
 			showing: {type:Boolean},
