jkomoros/card-web

commit 2bfebcca6f05b2ed3c4aca24b05f7cf4d14ffcda

Changed 'todo' --> 'freeform-todo' in all user-visible places, since freeform todo is a special case of general todos (which might be auto set in the future). Part of #200.

src/reducers/collection.js
@@ -136,7 +136,7 @@ const INITIAL_STATE = {
 		'has-comments': {},
 		'has-content': {},
 		'has-notes': {},
-		'has-todo': {},
+		'has-freeform-todo': {},
 		'has-links': {},
 		'has-inbound-links': {},
 		'has-tags': {},
@@ -154,7 +154,7 @@ const INITIAL_STATE = {
 		'has-comments': {},
 		'has-content': {},
 		'has-notes': {},
-		'has-todo': {},
+		'has-freeform-todo': {},
 		'has-links': {},
 		'has-inbound-links': {},
 		'has-tags': {},
@@ -337,7 +337,7 @@ const makeFilterFromCards = (cards, previousFilters) => {
 		'has-comments': setUnion(setRemove(previousFilters['has-comments'],  newCardsWithoutComments), newCardsWithComments),
 		'has-content': setUnion(setRemove(previousFilters['has-content'], newCardsWithoutContent), newCardsWithContent),
 		'has-notes': setUnion(setRemove(previousFilters['has-notes'], newCardsWithoutNotes), newCardsWithNotes),
-		'has-todo': setUnion(setRemove(previousFilters['has-todo'], newCardsWithoutTodo), newCardsWithTodo),
+		'has-freeform-todo': setUnion(setRemove(previousFilters['has-freeform-todo'], newCardsWithoutTodo), newCardsWithTodo),
 		'has-links': setUnion(setRemove(previousFilters['has-links'], newCardsWithoutLinks), newCardsWithLinks),
 		'has-inbound-links': setUnion(setRemove(previousFilters['has-inbound-links'], newCardsWithoutInboundLinks), newCardsWithInboundLinks),
 		'has-tags': setUnion(setRemove(previousFilters['has-tags'], newCardsWithoutTags), newCardsWithTags),

src/components/card-editor.js
@@ -185,11 +185,11 @@ class CardEditor extends connect(store)(LitElement) {
 			<div class='tabs' @click=${this._handleTabClicked}>
 				<label name='${TAB_CONTENT}' ?selected=${this._selectedTab == TAB_CONTENT} ?empty=${!hasContent} ?modified=${contentModified}>Content</label>
 				<label name='${TAB_NOTES}' ?selected=${this._selectedTab == TAB_NOTES} ?empty=${!hasNotes} ?modified=${notesModified}>Notes</label>
-				<label name='${TAB_TODO}' ?selected=${this._selectedTab == TAB_TODO} ?empty=${!hasTodo} ?modified=${todoModified}>TODO</label>
+				<label name='${TAB_TODO}' ?selected=${this._selectedTab == TAB_TODO} ?empty=${!hasTodo} ?modified=${todoModified}>Freeform TODO</label>
 				<span class='flex'></span>
 				<label class='help' ?hidden=${this._selectedTab !== TAB_CONTENT}>Content is what shows up on the main body of the card</label>
 				<label class='help' ?hidden=${this._selectedTab !== TAB_NOTES}>Notes are visible in the info panel to all readers and are for permanent asides</label>
-				<label class='help' ?hidden=${this._selectedTab !== TAB_TODO}>TODOs are only visible to editors and mark a temporary thing to do so it shows up in the has-todo filter</label>
+				<label class='help' ?hidden=${this._selectedTab !== TAB_TODO}>Freeform TODOs are only visible to editors and mark a temporary thing to do so it shows up in the has-freeform-todo filter</label>
 
 			</div>
 			<textarea ?hidden=${this._selectedTab !== TAB_CONTENT} @input='${this._handleBodyUpdated}' .value=${this._card.body}></textarea>

commit a910e64fe5df91499a59aa05a07103896b39d7e2

Fixed error introduced in 2bfebcc where the inverse freeform-todo filter wasn't introduced. Part of #200.

src/reducers/collection.js
@@ -113,7 +113,7 @@ export const INVERSE_FILTER_NAMES = {
 	'no-comments': 'has-comments',
 	'no-content': 'has-content',
 	'no-notes' : 'has-notes',
-	'no-todo' : 'has-todo',
+	'no-freeform-todo' : 'has-freeform-todo',
 	'no-links' : 'has-links',
 	'no-inbound-links' : 'has-inbound-links',
 	'no-tags' : 'has-tags',