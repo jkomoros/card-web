jkomoros/card-web

commit 2d652027d3a14016e78b6c14124112f974b53e2c

Make it so that trailing slashes are equivalent to "_" (first card). This is much prettier.
Things will canonicalize to it if it doesn't have it. Part of #107.

src/actions/collection.js
@@ -64,8 +64,8 @@ export const updateCardSelector = (cardSelector) => (dispatch, getState) => {
 
 	let parts = path.split('/');
 
-	//Remove trailing slash
-	if (!parts[parts.length - 1]) parts.pop();
+	//We do not remove a trailing slash; we take a trailing slash to mean
+	//"deafult item in the collection".
 
 	//in some weird situations, like during editing commit, we might be at no
 	//route even when our view is active. Not entirely clear how, but it
@@ -86,10 +86,8 @@ export const updateCardSelector = (cardSelector) => (dispatch, getState) => {
 		}
 	}
 
-	//Get last part
+	//Get last part, which is the card selector (and might be "").
 	let cardIdOrSlug = parts.pop();
-
-	//TODO: detect if it's one of the weird cardIdOrSlugs (e.g. '.', '.default');
 
 	let [filters, sortName, sortReversed] = extractFilterNamesAndSort(parts);
 
@@ -306,6 +304,8 @@ export const canonicalizeURL = () => (dispatch, getState) => {
 
 	let requestedCard = selectRequestedCard(state);
 	if (cardIdIsPlaceholder(requestedCard)) {
+		//If the selector was "_" then canonically replace it with just blank.
+		if (requestedCard == PLACEHOLDER_CARD_ID_CHARACTER) requestedCard = '';
 		//If it was a special placeholder that was requested, then leave it in
 		//the URL. If they arrow down and back up it's OK for it go back to its
 		//canonical URL.
@@ -321,8 +321,12 @@ export const canonicalizeURL = () => (dispatch, getState) => {
 	dispatch(navigatePathTo(path, true));
 };
 
+//cardIdIsPlaceholder is whether the cardId (the last part of the URL) either
+//starts with a "_" or is blank (which is equivalent to "_", which means, the
+//first card).
 const cardIdIsPlaceholder = (cardId) => {
-	if (!cardId) return false;
+	//Blank cardId is equivalent to "_" which means default.
+	if (!cardId) return true;
 	return cardId[0] == PLACEHOLDER_CARD_ID_CHARACTER;
 };
 

commit dc5ac4aa607dc518412679a82fd33b01c51b61ac

Fix a bug introduced in 2d65202, so that URLS without a trailing "_" work on load. Fixes #204.
The problem was that 2d65202 changed it so that a requestedCard of "" meant "default card in that collection, equivalent to '_'". The problem was that logic in showCard compares the current activeCardId to the requestedCard, and if they're the same it doesn't do the logic to convert it from a placeholder to a real card. The intial state has '' for the activeCardId, so on the very first load it was bailing.

The solution is to have under the covers the requested card be "_" even when the trailing part of the URL is "", so that it's clear it's not just initial value. Then we just take care to never show it to users in the URL.

src/actions/collection.js
@@ -88,6 +88,12 @@ export const updateCardSelector = (cardSelector) => (dispatch, getState) => {
 
 	//Get last part, which is the card selector (and might be "").
 	let cardIdOrSlug = parts.pop();
+	//If the requestedCard is actually "" we'll pretend throughout the pipeline
+	//it's actually "_", and just never show it to the user. This is because the
+	//identifier "" also happens to be the intial active card ID, which confuses
+	//caching logic, so it's better to have it be explicit (just never shown to
+	//the user).
+	if (cardIdOrSlug == '') cardIdOrSlug = PLACEHOLDER_CARD_ID_CHARACTER;
 
 	let [filters, sortName, sortReversed] = extractFilterNamesAndSort(parts);
 
@@ -321,12 +327,10 @@ export const canonicalizeURL = () => (dispatch, getState) => {
 	dispatch(navigatePathTo(path, true));
 };
 
-//cardIdIsPlaceholder is whether the cardId (the last part of the URL) either
-//starts with a "_" or is blank (which is equivalent to "_", which means, the
-//first card).
+//cardIdIsPlaceholder is whether the cardId (the last part of the URL) starts
+//with a "_"
 const cardIdIsPlaceholder = (cardId) => {
-	//Blank cardId is equivalent to "_" which means default.
-	if (!cardId) return true;
+	if (!cardId) return false;
 	return cardId[0] == PLACEHOLDER_CARD_ID_CHARACTER;
 };
 