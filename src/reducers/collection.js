41e59327eb198464b77371e244a065d7112b67aa
Alex Komoroske <jkomoros@gmail.com>
2020-01-26 20:03:22 -0800

    Introduce a twiddler for the section the card is in, with the first main section and second getting a boost, about and stubs getting no-op, and last one getting a negative.
    Memoized so it only has to be generated once. Part of #212.

--- src/reducers/collection.js
+++ src/reducers/collection.js
@@ -48,6 +48,44 @@ const sectionNameForCard = (card, sections) => {
 	return section ? section.title : '';
 };
 
+//cachedSectionTwidderMapForSections is the sections map we were last passed; if
+//it's different then we should regenerate cachedSectionTwidderMap.
+let cachedSectionTwidderMapForSections = null;
+//The cached map to pass back out if cachedSectionTwidderMapForSections is the same as last time.
+let cachedSectionTwiddlerMap = null;
+
+const SECTION_TWIDDLE_AMOUNT = 0.15;
+
+//sectionTwiddler map returns a map of section names to amount to twiddle those
+//values up for earlier sections. It's memoized so if sections doesn't change it
+//returns the same thing--and generally it should only have to be generated
+//once.
+const sectionTwiddlerMap = (sections) => {
+
+	if (cachedSectionTwidderMapForSections === sections && cachedSectionTwiddlerMap) return cachedSectionTwiddlerMap;
+
+	let twiddlerMap = new Map();
+	const sectionKeys = Object.keys(sections);
+	for (let i = 0; i < sectionKeys.length; i++) {
+		let multiplier = 1.0;
+		const sectionKey = sectionKeys[i];
+		//Section 0 is 'about', which isn't good... or bad.
+		if (i != 0) {
+			//!st sectino is the best, but the last section (typically
+			//!'random_thoughts') is actively bad, and the second-to-last
+			//!(typically 'stubs') is neutral.
+			multiplier = 1.0 + ((sectionKeys.length - i) * SECTION_TWIDDLE_AMOUNT) - (SECTION_TWIDDLE_AMOUNT * 2);
+		}
+		twiddlerMap.set(sectionKey, multiplier);
+	}
+
+	cachedSectionTwiddlerMap = twiddlerMap;
+	cachedSectionTwidderMapForSections = sections;
+
+	return twiddlerMap;
+
+};
+
 //EAch sort is an extractor, a description (currently just useful for
 //documentation; not shown anywhere), and a labelName to show in the drawer next
 //to the label that extractor returns. The extractor is given the card object
@@ -104,7 +142,7 @@ export const SORTS = {
 		labelName: 'Last Activity',
 	},
 	'tweet-order': {
-		extractor: (card) => {
+		extractor: (card, sections) => {
 			//Note: this logic is just manually equivalent to the logic that
 			//will be applied server-side, and is thus duplicated there.
 		
@@ -114,10 +152,14 @@ export const SORTS = {
 				return [0, 'Not to be tweeted'];
 			}
 
+			const twiddlerMap = sectionTwiddlerMap(sections);
+
 			//The baseValue is the more time that has passed since the last time it was tweeted. 
 			const updatedSeconds = card.updated_substantive ? card.updated_substantive.seconds : 1000;
 			const lastTweetedSeconds = card.last_tweeted ? card.last_tweeted.seconds : 0;
 			let baseValue = updatedSeconds - lastTweetedSeconds;
+			//Twiddle by section
+			baseValue *= twiddlerMap.get(card.section) || 1.0;
 			//TODO: include a negative multiplier for how many times it's been tweeted already.
 			//TODO: includ a positive multiplier for how many times it's been starred.
 			return [baseValue, prettyTime(lastTweetedSeconds)];
