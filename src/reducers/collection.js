963591ae91e8aa9071610e9044d77de0346b3dcf
Alex Komoroske <jkomoros@gmail.com>
2020-01-27 17:17:04 -0800

    Pop out the tweet order stuff into a separate file so that it's easy to use in the cloud functions. Part of #212.

--- src/reducers/collection.js
+++ src/reducers/collection.js
@@ -27,6 +27,10 @@ import {
 	cardMatchingFilters
 } from '../util.js';
 
+import {
+	tweetOrderExtractor,
+} from './tweet-helpers.js';
+
 export const DEFAULT_SET_NAME = 'all';
 //reading-list is a set (as well as filters, e.g. `in-reading-list`) since the
 //order matters and is customizable by the user. Every other collection starts
@@ -46,67 +50,6 @@ export const RECENT_SORT_NAME = 'recent';
 const sectionNameForCard = (card, sections) => {
 	let section = sections[card.section];
 	return section ? section.title : '';
-};
-
-//cachedSectionTwidderMapForSections is the sections map we were last passed; if
-//it's different then we should regenerate cachedSectionTwidderMap.
-let cachedSectionTwidderMapForSections = null;
-//The cached map to pass back out if cachedSectionTwidderMapForSections is the same as last time.
-let cachedSectionTwiddlerMap = null;
-
-const SECTION_TWIDDLE_AMOUNT = 0.15;
-
-//sectionTwiddler map returns a map of section names to amount to twiddle those
-//values up for earlier sections. It's memoized so if sections doesn't change it
-//returns the same thing--and generally it should only have to be generated
-//once.
-const sectionTwiddlerMap = (sections) => {
-
-	if (cachedSectionTwidderMapForSections === sections && cachedSectionTwiddlerMap) return cachedSectionTwiddlerMap;
-
-	let twiddlerMap = new Map();
-	const sectionKeys = Object.keys(sections);
-	for (let i = 0; i < sectionKeys.length; i++) {
-		let multiplier = 1.0;
-		const sectionKey = sectionKeys[i];
-		//Section 0 is 'about', which isn't good... or bad.
-		if (i != 0) {
-			//!st sectino is the best, but the last section (typically
-			//!'random_thoughts') is actively bad, and the second-to-last
-			//!(typically 'stubs') is neutral.
-			multiplier = 1.0 + ((sectionKeys.length - i) * SECTION_TWIDDLE_AMOUNT) - (SECTION_TWIDDLE_AMOUNT * 2);
-		}
-		twiddlerMap.set(sectionKey, multiplier);
-	}
-
-	cachedSectionTwiddlerMap = twiddlerMap;
-	cachedSectionTwidderMapForSections = sections;
-
-	return twiddlerMap;
-
-};
-
-const tweetOrderExtractor = (card, sections) => {
-	//Note: this logic is just manually equivalent to the logic that
-	//will be applied server-side, and is thus duplicated there.
-
-	//Rate the cards that shouldn't actually be shown (that should be
-	//filtered out) very low just to ensure they don't get tweeted.
-	if (!card.published || !card.slugs || card.slugs.length == 0 || card.card_type != 'content') {
-		return [0, 'Not to be tweeted'];
-	}
-
-	const twiddlerMap = sectionTwiddlerMap(sections);
-
-	//The baseValue is the more time that has passed since the last time it was tweeted. 
-	const updatedSeconds = card.updated_substantive ? card.updated_substantive.seconds : 1000;
-	const lastTweetedSeconds = card.last_tweeted ? card.last_tweeted.seconds : 0;
-	let baseValue = updatedSeconds - lastTweetedSeconds;
-	//Twiddle by section
-	baseValue *= twiddlerMap.get(card.section) || 1.0;
-	//TODO: include a negative multiplier for how many times it's been tweeted already.
-	//TODO: includ a positive multiplier for how many times it's been starred.
-	return [baseValue, prettyTime(lastTweetedSeconds)];
 };
 
 //EAch sort is an extractor, a description (currently just useful for
---
+++ src/reducers/tweet-helpers.js
@@ -0,0 +1,71 @@
+//This file is popped out separately so it can be reused substantially as is in
+//the cloud function file.
+
+//cachedSectionTwidderMapForSections is the sections map we were last passed; if
+//it's different then we should regenerate cachedSectionTwidderMap.
+let cachedSectionTwidderMapForSections = null;
+//The cached map to pass back out if cachedSectionTwidderMapForSections is the same as last time.
+let cachedSectionTwiddlerMap = null;
+
+const SECTION_TWIDDLE_AMOUNT = 0.15;
+
+//date may be a firestore timestamp or a date object.
+const prettyTime = (date) => {
+	//Recreated from util.js so this file doesn't have any imports
+	if (!date) return '';
+	if (typeof date.toDate == 'function') date = date.toDate();
+	return date.toDateString();
+};
+
+//sectionTwiddler map returns a map of section names to amount to twiddle those
+//values up for earlier sections. It's memoized so if sections doesn't change it
+//returns the same thing--and generally it should only have to be generated
+//once.
+const sectionTwiddlerMap = (sections) => {
+
+	if (cachedSectionTwidderMapForSections === sections && cachedSectionTwiddlerMap) return cachedSectionTwiddlerMap;
+
+	let twiddlerMap = new Map();
+	const sectionKeys = Object.keys(sections);
+	for (let i = 0; i < sectionKeys.length; i++) {
+		let multiplier = 1.0;
+		const sectionKey = sectionKeys[i];
+		//Section 0 is 'about', which isn't good... or bad.
+		if (i != 0) {
+			//!st sectino is the best, but the last section (typically
+			//!'random_thoughts') is actively bad, and the second-to-last
+			//!(typically 'stubs') is neutral.
+			multiplier = 1.0 + ((sectionKeys.length - i) * SECTION_TWIDDLE_AMOUNT) - (SECTION_TWIDDLE_AMOUNT * 2);
+		}
+		twiddlerMap.set(sectionKey, multiplier);
+	}
+
+	cachedSectionTwiddlerMap = twiddlerMap;
+	cachedSectionTwidderMapForSections = sections;
+
+	return twiddlerMap;
+
+};
+
+export const tweetOrderExtractor = (card, sections) => {
+	//Note: this logic is just manually equivalent to the logic that
+	//will be applied server-side, and is thus duplicated there.
+
+	//Rate the cards that shouldn't actually be shown (that should be
+	//filtered out) very low just to ensure they don't get tweeted.
+	if (!card.published || !card.slugs || card.slugs.length == 0 || card.card_type != 'content') {
+		return [0, 'Not to be tweeted'];
+	}
+
+	const twiddlerMap = sectionTwiddlerMap(sections);
+
+	//The baseValue is the more time that has passed since the last time it was tweeted. 
+	const updatedSeconds = card.updated_substantive ? card.updated_substantive.seconds : 1000;
+	const lastTweetedSeconds = card.last_tweeted ? card.last_tweeted.seconds : 0;
+	let baseValue = updatedSeconds - lastTweetedSeconds;
+	//Twiddle by section
+	baseValue *= twiddlerMap.get(card.section) || 1.0;
+	//TODO: include a negative multiplier for how many times it's been tweeted already.
+	//TODO: includ a positive multiplier for how many times it's been starred.
+	return [baseValue, prettyTime(lastTweetedSeconds)];
+};
