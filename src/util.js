5df33ce5982e4082c83935e4ab9d363ee5daffd8
Alex Komoroske <jkomoros@gmail.com>
2020-04-13 16:45:03 -0700

    Update MultiBatch so that it splits up into precisely the smallest number of batches it can, by counting updates and sets that include the three field values that lead to an operation counting double.
    Part of #215.

--- src/util.js
+++ src/util.js
@@ -5,10 +5,10 @@ import dompurify from 'dompurify';
 //other imports.
 export const _PAGE_BASIC_CARD = 'basic-card';
 
-//When the multi batch should create a new batch. The limit in Firebase is 500,
-//but some operations (like arrayUpdate) count as multiple, so set it well
-//below.
-const MULTI_BATCH_NEW_BATCH_LIMIT = 100;
+//The limit that Firebase enacts for writes in one batch. MultiBatch will count
+//writes correctly, including double-counting updates that use one of the
+//readback sentinels.
+const FIRESTORE_BATCH_LIMIT = 500;
 
 //MultiBatch is a thing that can be used as a drop-in replacement firebase db
 //batch, and will automatically split into multiple underlying batches if it's
@@ -23,7 +23,7 @@ export const MultiBatch = class {
 	}
 
 	get _batch() {
-		if (this._currentBatchOperationCount > MULTI_BATCH_NEW_BATCH_LIMIT) {
+		if (this._currentBatchOperationCount >= FIRESTORE_BATCH_LIMIT) {
 			this._currentBatch = null;
 		}
 		if (!this._currentBatch) {
@@ -34,6 +34,25 @@ export const MultiBatch = class {
 		return this._currentBatch;
 	}
 
+	_writeCountForUpdate(update) {
+		//Firestore treats updates as counting for 1, unless there are 1 or more
+		//of {serverTimestamp, arrayUnion, or arrayRemove}.
+		
+		//Note: this function is very tied to the implementation of
+		//firestore.FieldValue and may need to change if it changes.
+		for (let val of Object.values(update)) {
+			if (typeof val !== 'object') continue;
+			if (!val['_methodName']) continue;
+			if (typeof val['_methodName'] !== 'string') continue;
+			const parts = val['_methodName'].split('.');
+			if (parts.length !== 2) continue;
+			if (parts[0] !== 'FieldValue') continue;
+			if (parts[1] !== 'serverTimestamp' && parts[1] !== 'arrayRemove' && parts[1] != 'arrayUnion') continue;
+			return 2;
+		}
+		return 1;
+	}
+
 	delete(ref) {
 		this._batch.delete(ref);
 		this._currentBatchOperationCount++;
@@ -42,7 +61,7 @@ export const MultiBatch = class {
 
 	set(ref, data, options) {
 		this._batch.set(ref, data, options);
-		this._currentBatchOperationCount++;
+		this._currentBatchOperationCount += this._writeCountForUpdate(data);
 		return this;
 	}
 
@@ -50,7 +69,7 @@ export const MultiBatch = class {
 		//TODO: the signature in the documentation is kind of weird for this
 		//one. Are there two different modes?
 		this._batch.update(ref, data);
-		this._currentBatchOperationCount++;
+		this._currentBatchOperationCount += this._writeCountForUpdate(data);
 		return this;
 	}
 
