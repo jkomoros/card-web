fac6cbb53cc63eed00f0662eb7ddfbda90311969
Alex Komoroske <jkomoros@gmail.com>
2020-04-16 11:06:42 -0700

    Got hello-world security tests set up and running. Part of #190.

---
+++ test/security/test.js
@@ -0,0 +1,38 @@
+
+//based on https://github.com/firebase/quickstart-nodejs/tree/master/firestore-emulator/javascript-quickstart
+/*eslint-env node*/
+const firebase = require('@firebase/testing');
+const fs = require('fs');
+
+const projectId = 'compendium-tester';
+const firebasePort = require('../../firebase.json').emulators.firestore.port;
+const port = firebasePort ? firebasePort : 8080;
+const coverageUrl = `http://localhost:${port}/emulator/v1/projects/${projectId}:ruleCoverage.html`;
+
+const rules = fs.readFileSync('firestore.rules', 'utf8');
+
+function authedApp(auth) {
+	return firebase.initializeTestApp({ projectId, auth }).firestore();
+}
+
+beforeEach(async () => {
+	// Clear the database between tests
+	await firebase.clearFirestoreData({ projectId });
+});
+
+before(async () => {
+	await firebase.loadFirestoreRules({ projectId, rules });
+});
+
+after(async () => {
+	await Promise.all(firebase.apps().map(app => app.delete()));
+	console.log(`View rule coverage information at ${coverageUrl}\n`);
+});
+
+describe('Compendium Rules', () => {
+	it('allows anyone to read a card', async () => {
+		const db = authedApp(null);
+		const card = db.collection('cards').doc('foo');
+		await firebase.assertSucceeds(card.get());
+	});
+});
