27eedc71c1cbfbe0becbfb51bd3f779f19b94280
Alex Komoroske <jkomoros@gmail.com>
2020-04-26 12:58:06 -0700

    Update the updateInboundLinks cloud function to keep the links_inbound_text up to date. Non-trivial change in logic. Part of #282.

--- functions/update.js
+++ functions/update.js
@@ -1,6 +1,7 @@
 const common = require('./common.js');
 const db = common.db;
 const FieldValue = common.FieldValue;
+const fromEntries = require('fromentries');
 
 const arrayDiff = (before, after) => {
     let afterMap = new Map();
@@ -8,24 +9,59 @@ const arrayDiff = (before, after) => {
         afterMap.set(item, true);
     }
     let deletions = [];
+    let same = [];
     for (let item of before) {
         if (afterMap.has(item)) {
             //Keep track of that we've seen this one
             afterMap.delete(item);
+            same.push(item);
         } else {
             deletions.push(item);
         }
     }
     //Additions is the keys not remved in afterMap
     let additions = [...afterMap.keys()];
-    return [additions, deletions];
+    return [additions, deletions, same];
 };
+
+//Returns an objectof key names: true where its value in before and its value in
+//after are different (either because they both have the key and the value
+//changed, or because a key is one but not the other.
+const updatedKeys = (before, after) => {
+    if (!before && !after) return {};
+    if (!before) return fromEntries(Object.keys(after).map(item => [item, true]));
+    if (!after) return fromEntries(Object.keys(before).map(item => [item, true]));
+
+    let keys = new Map();
+    for (let key of Object.keys(before)) {
+        keys.set(key, true);
+    }
+    for (let key of Object.keys(after)) {
+        keys.set(key, true);
+    }
+
+    let result = {};
+    for (let key of keys.keys()) {
+        if (before[key] === after[key]) continue;
+        result[key] = true;
+    }
+
+    return result;
+
+}
 
 const inboundLinks = (change, context) => {
 
-    let [additions, deletions] = arrayDiff(change.before.data().links, change.after.data().links);
+    let [additions, deletions, same] = arrayDiff(change.before.data().links, change.after.data().links);
 
-    if (additions.length === 0 && deletions.length === 0) return Promise.resolve();
+    let linkTextMap = change.after.data().links_text || {};
+    let beforeLinkTextMap = change.before.data().links_text || {};
+
+    let textChangedCards = updatedKeys(beforeLinkTextMap, linkTextMap);
+
+    let updatedText = same.map(id => textChangedCards[id] ? id : '').filter(item => item);
+
+    if (additions.length === 0 && deletions.length === 0 && updatedText.length === 0) return Promise.resolve();
 
     let cardId = context.params.cardId;
 
@@ -33,16 +69,28 @@ const inboundLinks = (change, context) => {
 
     for (let card of additions) {
         let ref = db.collection('cards').doc(card);
-        batch.update(ref, {
-            links_inbound: FieldValue.arrayUnion(cardId)
-        });
+        let update = {
+            links_inbound: FieldValue.arrayUnion(cardId),
+        };
+        update['links_inbound_text.' + cardId] = linkTextMap[card];
+        batch.update(ref, update);
+    }
+
+    //Now the cards that were neither added or deleted but did update the text.
+    for (let card of updatedText) {
+        let ref = db.collection('cards').doc(card);
+        let update = {};
+        update['links_inbound_text.' + cardId] = linkTextMap[card];
+        batch.update(ref, update);
     }
 
     for (let card of deletions) {
         let ref = db.collection('cards').doc(card);
-        batch.update(ref, {
+        let update = {
             links_inbound: FieldValue.arrayRemove(cardId)
-        });
+        };
+        update['links_inbound_text.' + cardId] = FieldValue.delete();
+        batch.update(ref, update);
     }
 
     return batch.commit();
