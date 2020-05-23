jkomoros/card-web

commit 01f572335bf79d7dfbb4b8a02262d8a8e2b94f90

Add 'prose' and 'citations' TODO types.
These are only ever applied manually, because their test function returns false in all instances. They can be manually set on cards that need their prose cleaned up to flow coherently, or needing citations.

Fixes #239.

src/reducers/collection.js
@@ -200,6 +200,9 @@ const CARD_FILTER_CONFIGS = {
 	'tags': [defaultCardFilterName('tags'), card => card.tags && card.tags.length, TODO_TYPE_AUTO, 1.0, 'Whether the card has any tags'],
 	'published': [['published', 'unpublished', 'does-not-need-to-be-published', 'needs-to-be-published'], card => card.published, TODO_TYPE_AUTO, 0.5, 'Whether the card is published'],
 	'tweet': [defaultCardFilterName('tweet'), card => card.tweet_count > 0, TODO_TYPE_NA, 0.0, 'Whether the card has any tweets from the bot'],
+	//The following TODO types will never be automatically applied, because their test function always returns false, but they can be manually applied.
+	'prose': [defaultCardFilterName('prose'), () => false, TODO_TYPE_AUTO, 0.5, 'Whether the card has manually been marked as needing to be turned into flowing prose, as opposed to disjoint details'],
+	'citations': [defaultCardFilterName('citations'), () => false, TODO_TYPE_AUTO, 0.3, 'Whether the card has citations that need to be formally represented'],
 	//TODO_COMBINED_FILTERS looks for the fourth key in the filtername array, so
 	//we just duplicate the first two since they're the same (the reason they'd
 	//differ is if there's an override key and that could make the has- and

commit e466df338a233c850b4ae9664809ef0d41b68d33

Fixed a bug broken in 01f572335bf79d7dfbb4b8a02262d8a8e2b94f90 as part of #239, where ctiations and prose show up on EVERY card.
TODO filters should return true for the GOOD outcome, but they were defaulting to false.

Noticed while working on #301.

src/reducers/collection.js
@@ -195,8 +195,8 @@ const CARD_FILTER_CONFIGS = {
 	'published': [['published', 'unpublished', 'does-not-need-to-be-published', 'needs-to-be-published'], card => card.published, TODO_TYPE_AUTO, 0.5, 'Whether the card is published'],
 	'tweet': [defaultCardFilterName('tweet'), card => card.tweet_count > 0, TODO_TYPE_NA, 0.0, 'Whether the card has any tweets from the bot'],
 	//The following TODO types will never be automatically applied, because their test function always returns false, but they can be manually applied.
-	'prose': [defaultCardFilterName('prose'), () => false, TODO_TYPE_AUTO, 0.5, 'Whether the card has manually been marked as needing to be turned into flowing prose, as opposed to disjoint details'],
-	'citations': [defaultCardFilterName('citations'), () => false, TODO_TYPE_AUTO, 0.3, 'Whether the card has citations that need to be formally represented'],
+	'prose': [defaultCardFilterName('prose'), () => true, TODO_TYPE_AUTO, 0.5, 'Whether the card has manually been marked as needing to be turned into flowing prose, as opposed to disjoint details'],
+	'citations': [defaultCardFilterName('citations'), () => true, TODO_TYPE_AUTO, 0.3, 'Whether the card has citations that need to be formally represented'],
 	//TODO_COMBINED_FILTERS looks for the fourth key in the filtername array, so
 	//we just duplicate the first two since they're the same (the reason they'd
 	//differ is if there's an override key and that could make the has- and