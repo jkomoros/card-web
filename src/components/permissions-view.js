b007771d0d9e9b67e10eb1c23c20333e3def4162
Alex Komoroske <jkomoros@gmail.com>
2020-09-02 12:59:18 -0700

    Create a stub, lazy-loaded reducer for permissions data. Part of #275.

--- src/components/permissions-view.js
+++ src/components/permissions-view.js
@@ -5,6 +5,12 @@ import { connect } from 'pwa-helpers/connect-mixin.js';
 // This element is connected to the Redux store.
 import { store } from '../store.js';
 
+//lazy-load the permissions reducer
+import permissions from '../reducers/permissions.js';
+store.addReducers({
+	permissions,
+});
+
 import { 
 	selectUserMayEditPermissions
 } from '../selectors.js';
---
+++ src/reducers/permissions.js
@@ -0,0 +1,10 @@
+const INITIAL_STATE = {};
+
+const app = (state = INITIAL_STATE, action) => {
+	switch (action.type) {
+	default:
+		return state;
+	}
+};
+
+export default app;
