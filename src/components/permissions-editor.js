jkomoros/card-web

commit dcf6da547fa0a78f1dfe4601347df6d0291316a9

Allow adding a cards permission to a user even if that user doesn't yet have cards overrides. Part of #275.

src/components/permissions-editor.js
@@ -27,7 +27,8 @@ import {
 } from '../actions/permissions.js';
 
 import {
-	PERMISSIONS_INFO
+	PERMISSIONS_INFO,
+	PERMISSIONS_LEGAL_ON_CARD_INFO
 } from '../permissions.js';
 
 import './tag-list.js';
@@ -92,13 +93,15 @@ class PermissionsEditor extends connect(store)(LitElement) {
 				<p><strong>${this._title}</strong> ${this.description ? html`<em>${this.description}</em>` : ''}&nbsp;&nbsp;&nbsp;<strong>Notes</strong> ${this._effectivePermissions.notes || html`<em>No notes</em>`} <span class='edit' ?hidden=${!this._editable} @click=${this._handleEditNotes}>${EDIT_ICON}</span><span class='edit' ?hidden=${!this._editable} @click=${this._handleDelete}>${DELETE_FOREVER_ICON}</span></p>
 				<tag-list .tags=${this._enabledLockedPermissions} .tagInfos=${LOCKED_PERMISSIONS} .overrideTypeName=${'Permission'} .defaultColor=${lockedPermissionColor} .hideOnEmpty=${true}></tag-list>
 				<tag-list .tags=${this._enabledModifiablePermissions} .tagInfos=${MODIFIABLE_PERMISSIONS} .editing=${this._editable} .disableNew=${true} @add-tag=${this._handleAddEnabled} @remove-tag=${this._handleRemove} .overrideTypeName=${'Permission'} .defaultColor=${enabledPermissionColor}></tag-list>
-				${this._effectivePermissionsForCards ? 
-		html`
 				<div>
 					<p><strong>Cards</strong> <em>These are permissions that are specific to an individual card. Edit the card to modify them.</em></p>
 		${Object.entries(this._effectivePermissionsForCards).map(entry => 
-		html`<span>${entry[0]}</span> <tag-list .permission=${entry[0]} .tags=${entry[1]} .tagInfos=${this._tagInfosForCards} .tapEvents=${true} .editing=${true} .disableAdd=${true} @remove-tag=${this._handleRemoveCardPermission}></tag-list> <button @click=${this._handleAddCardPermission} .permission=${entry[0]}>+</button>`)}`
-		: ''}
+		html`<span>${entry[0]}</span> <tag-list .permission=${entry[0]} .tags=${entry[1]} .tagInfos=${this._tagInfosForCards} .tapEvents=${true} .editing=${true} .disableAdd=${true} @remove-tag=${this._handleRemoveCardPermission}></tag-list> <button @click=${this._handleAddCardPermission} .permission=${entry[0]}>+</button>`)}
+				${this._unusedCardPermissions.length ? 
+		html`<select @change=${this._handleAddPermissionType}>
+					<option value=''><em>Add a cards permission type...</option>
+					${this._unusedCardPermissions.map(item => html`<option value=${item}>${item}</option>`)}
+				</select>` : ''}
 			</div>
 			`;
 	}
@@ -137,7 +140,13 @@ class PermissionsEditor extends connect(store)(LitElement) {
 	}
 
 	get _effectivePermissionsForCards() {
-		return this._userPermissionsForCardsMap[this.uid];
+		return this._userPermissionsForCardsMap[this.uid] || {};
+	}
+
+	get _unusedCardPermissions() {
+		//the card permissions that aren't currently showing for this uid but could be
+		const effectivePerms = this._effectivePermissionsForCards;
+		return Object.keys(PERMISSIONS_LEGAL_ON_CARD_INFO).filter(key => !effectivePerms[key]);
 	}
 
 	get _enabledModifiablePermissions() {
@@ -153,6 +162,15 @@ class PermissionsEditor extends connect(store)(LitElement) {
 		this._authors = selectAuthors(state);
 		this._userPermissionsForCardsMap = selectUserPermissionsForCardsMap(state);
 		this._tagInfosForCards = selectTagInfosForCards(state);
+	}
+
+	_handleAddPermissionType(e) {
+		const ele = e.composedPath()[0];
+		if (!ele.value) return;
+		const value = ele.value;
+		//Set it back to default
+		ele.value = '';
+		store.dispatch(selectCardToAddPermissionTo(value, this.uid));
 	}
 
 	_handleEditNotes() {

commit 82f2c4b909f3a34f127992d7bc07e535e3963055

Fixed a bug broken in dcf6da547fa0a78f1dfe4601347df6d0291316a9 where you could add cards permissions to the pre-configured permissions blobs that aren't tied to a user. Part of #275.

src/components/permissions-editor.js
@@ -97,7 +97,7 @@ class PermissionsEditor extends connect(store)(LitElement) {
 					<p><strong>Cards</strong> <em>These are permissions that are specific to an individual card. Edit the card to modify them.</em></p>
 		${Object.entries(this._effectivePermissionsForCards).map(entry => 
 		html`<span>${entry[0]}</span> <tag-list .permission=${entry[0]} .tags=${entry[1]} .tagInfos=${this._tagInfosForCards} .tapEvents=${true} .editing=${true} .disableAdd=${true} @remove-tag=${this._handleRemoveCardPermission}></tag-list> <button @click=${this._handleAddCardPermission} .permission=${entry[0]}>+</button>`)}
-				${this._unusedCardPermissions.length ? 
+				${this._unusedCardPermissions.length && this.uid ? 
 		html`<select @change=${this._handleAddPermissionType}>
 					<option value=''><em>Add a cards permission type...</option>
 					${this._unusedCardPermissions.map(item => html`<option value=${item}>${item}</option>`)}