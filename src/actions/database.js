jkomoros/card-web

commit e7046261e8abd01deda553cf9bce1c91f1d3969c

Factor out single snapshot receiver for cards. part of #307.

src/actions/database.js
@@ -225,46 +225,32 @@ export const connectLiveAuthors = () => {
 	});
 };
 
+const cardSnapshotReceiver = (snapshot) => {
+
+	let cards = {};
+
+	snapshot.docChanges().forEach(change => {
+		if (change.type === 'removed') return;
+		let doc = change.doc;
+		let id = doc.id;
+		let card = doc.data();
+		card.id = id;
+		cardSetNormalizedTextProperties(card);
+		cards[id] = card;
+	});
+
+	store.dispatch(updateCards(cards, true));
+
+};
+
 export const connectLivePublishedCards = () => {
 	if (!selectUserMayViewApp(store.getState())) return;
-	db.collection(CARDS_COLLECTION).where('published', '==', true).onSnapshot(snapshot => {
-
-		let cards = {};
-
-		snapshot.docChanges().forEach(change => {
-			if (change.type === 'removed') return;
-			let doc = change.doc;
-			let id = doc.id;
-			let card = doc.data();
-			card.id = id;
-			cardSetNormalizedTextProperties(card);
-			cards[id] = card;
-		});
-
-		store.dispatch(updateCards(cards, false));
-
-	});
+	db.collection(CARDS_COLLECTION).where('published', '==', true).onSnapshot(cardSnapshotReceiver);
 };
 
 export const connectLiveUnpublishedCards = () => {
 	if (!selectUserMayViewApp(store.getState())) return;
-	db.collection(CARDS_COLLECTION).where('published', '==', false).onSnapshot(snapshot => {
-
-		let cards = {};
-
-		snapshot.docChanges().forEach(change => {
-			if (change.type === 'removed') return;
-			let doc = change.doc;
-			let id = doc.id;
-			let card = doc.data();
-			card.id = id;
-			cardSetNormalizedTextProperties(card);
-			cards[id] = card;
-		});
-
-		store.dispatch(updateCards(cards, true));
-
-	});
+	db.collection(CARDS_COLLECTION).where('published', '==', false).onSnapshot(cardSnapshotReceiver);
 };
 
 export const connectLiveSections = () => {

commit abeb4499d1894466db1c8740a4deaec91b0e85cb

Fixed a bug, broken in e704626, where switching to a tab that didn't link to a specific card, but rather a section, wouldn't load.
The unpublished param was set the same for each one, which caused the app to not realize that all of the data was loaded.

Part of #274.

src/actions/database.js
@@ -200,27 +200,29 @@ export const connectLiveAuthors = () => {
 	});
 };
 
-const cardSnapshotReceiver = (snapshot) => {
+const cardSnapshotReceiver = (unpublished) =>{
+	
+	return (snapshot) => {
+		let cards = {};
 
-	let cards = {};
+		snapshot.docChanges().forEach(change => {
+			if (change.type === 'removed') return;
+			let doc = change.doc;
+			let id = doc.id;
+			let card = doc.data();
+			card.id = id;
+			cardSetNormalizedTextProperties(card);
+			cards[id] = card;
+		});
 
-	snapshot.docChanges().forEach(change => {
-		if (change.type === 'removed') return;
-		let doc = change.doc;
-		let id = doc.id;
-		let card = doc.data();
-		card.id = id;
-		cardSetNormalizedTextProperties(card);
-		cards[id] = card;
-	});
-
-	store.dispatch(updateCards(cards, true));
+		store.dispatch(updateCards(cards, unpublished));
+	};
 
 };
 
 export const connectLivePublishedCards = () => {
 	if (!selectUserMayViewApp(store.getState())) return;
-	db.collection(CARDS_COLLECTION).where('published', '==', true).onSnapshot(cardSnapshotReceiver);
+	db.collection(CARDS_COLLECTION).where('published', '==', true).onSnapshot(cardSnapshotReceiver(false));
 };
 
 let liveUnpublishedCardsForUserAuthorUnsubscribe = null;
@@ -230,8 +232,8 @@ export const connectLiveUnpublishedCardsForUser = (uid) => {
 	if (!selectUserMayViewApp(store.getState())) return;
 	disconnectLiveUnpublishedCardsForUser();
 	if (!uid) return;
-	liveUnpublishedCardsForUserAuthorUnsubscribe = db.collection(CARDS_COLLECTION).where('author', '==', uid).where('published', '==', false).onSnapshot(cardSnapshotReceiver);
-	liveUnpublishedCardsForUserEditorUnsubscribe = db.collection(CARDS_COLLECTION).where('permissions.' + PERMISSION_EDIT_CARD, 'array-contains', uid).where('published', '==', false).onSnapshot(cardSnapshotReceiver);
+	liveUnpublishedCardsForUserAuthorUnsubscribe = db.collection(CARDS_COLLECTION).where('author', '==', uid).where('published', '==', false).onSnapshot(cardSnapshotReceiver(true));
+	liveUnpublishedCardsForUserEditorUnsubscribe = db.collection(CARDS_COLLECTION).where('permissions.' + PERMISSION_EDIT_CARD, 'array-contains', uid).where('published', '==', false).onSnapshot(cardSnapshotReceiver(true));
 };
 
 const disconnectLiveUnpublishedCardsForUser = () => {
@@ -248,7 +250,7 @@ const disconnectLiveUnpublishedCardsForUser = () => {
 export const connectLiveUnpublishedCards = () => {
 	if (!selectUserMayViewApp(store.getState())) return;
 	disconnectLiveUnpublishedCardsForUser();
-	db.collection(CARDS_COLLECTION).where('published', '==', false).onSnapshot(cardSnapshotReceiver);
+	db.collection(CARDS_COLLECTION).where('published', '==', false).onSnapshot(cardSnapshotReceiver(true));
 };
 
 export const connectLiveSections = () => {