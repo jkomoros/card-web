a0e0f96a46b07052e4cfefa7d90e1662ce35cd47
Alex Komoroske <jkomoros@gmail.com>
2020-09-27 19:08:49 -0700

    card-renderer doesn't stamp out even inert cards for the content type it didn't render.
    This fixes a few odd things, like all of the content being selected when you open a section-head for editing. Part of #206.

--- src/components/card-renderer.js
+++ src/components/card-renderer.js
@@ -11,15 +11,13 @@ import './section-head-card.js';
 // This element is *not* connected to the Redux store.
 export class CardRenderer extends LitElement {
 	render() {
-		return html`
-      <style>
-        [hidden] {
-          display:none;
-        }
-      </style>
-      <section-head-card .dataIsFullyLoaded=${this.dataIsFullyLoaded} ?hidden=${this._cardType != CARD_TYPE_SECTION_HEAD} .updatedFromContentEditable=${this.updatedFromContentEditable} .editing=${this.editing} .card=${this.card}></section-head-card>
-      <content-card .dataIsFullyLoaded=${this.dataIsFullyLoaded} ?hidden=${this._cardType != CARD_TYPE_CONTENT} .updatedFromContentEditable=${this.updatedFromContentEditable} .editing=${this.editing} .card=${this.card}></content-card>
-    `;
+		switch(this._cardType) {
+		case CARD_TYPE_SECTION_HEAD:
+			return html` <section-head-card .dataIsFullyLoaded=${this.dataIsFullyLoaded} ?hidden=${this._cardType != CARD_TYPE_SECTION_HEAD} .updatedFromContentEditable=${this.updatedFromContentEditable} .editing=${this.editing} .card=${this.card}></section-head-card>`;
+		case CARD_TYPE_CONTENT:
+		default:
+			return html`<content-card .dataIsFullyLoaded=${this.dataIsFullyLoaded} ?hidden=${this._cardType != CARD_TYPE_CONTENT} .updatedFromContentEditable=${this.updatedFromContentEditable} .editing=${this.editing} .card=${this.card}></content-card>`;
+		}
 	}
 
 	static get properties() {
