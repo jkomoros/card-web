82d406104c3c9c286956250c3acdd14b8dc49fe9
Alex Komoroske <jkomoros@gmail.com>
2020-10-18 16:13:55 -0700

    Fixed bug where we didn't treat fingerprints correctly (theyr'e actually maps). Still doesn't print anything because only content cards get fingerprints. Part of #353.

--- src/card_finishers.js
+++ src/card_finishers.js
@@ -7,12 +7,13 @@ import {
 } from './selectors.js';
 
 const WORKING_NOTES_TITLE_PREFIX = '>';
+const NUM_TERMS_OF_FINGERPRINT = 8;
 
 const workingNotesExtractor = (card,state) => {
 	//TODO: also include first part of semantic fingerprint.
 	const date = card.updated.toDate();
 	const fingerprint = getSemanticFingerprintForCard(state, card.id);
-	const prettyFingerprint = fingerprint ? fingerprint.join(' ') : '';
+	const prettyFingerprint = fingerprint ? [...fingerprint.keys()].slice(0, NUM_TERMS_OF_FINGERPRINT).join(' ') : '';
 	const title = WORKING_NOTES_TITLE_PREFIX + ' ' + date.toLocaleDateString('en-US', {month:'numeric', day:'numeric', year:'2-digit'}) + ' ' + prettyFingerprint;
 	return {
 		...card,
