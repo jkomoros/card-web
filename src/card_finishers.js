c4eec53b15eb17d1c1948185604cb34dffa5df75
Alex Komoroske <jkomoros@gmail.com>
2020-10-18 21:08:48 -0700

    Fixed a race introduced in last commit, where if the user had just typed and immediately committed, the normalized text content might not have been extracted yet, so the derived title might have been based on slightly stale content. Part of #353.

--- src/card_finishers.js
+++ src/card_finishers.js
@@ -6,13 +6,22 @@ import {
 	getSemanticFingerprintForCard
 } from './selectors.js';
 
+import {
+	cardSetNormalizedTextProperties
+} from './util.js';
+
 const WORKING_NOTES_TITLE_PREFIX = '>';
 const NUM_TERMS_OF_FINGERPRINT = 8;
 
 const workingNotesExtractor = (card,state) => {
 	//TODO: also include first part of semantic fingerprint.
 	const date = card.updated.toDate();
-	const fingerprint = getSemanticFingerprintForCard(state, card);
+	const cardCopy = {...card};
+	//The fingerprint requires these to be up to date, but we only update these
+	//on a timeout in textFieldUpdated so typing isn't expensive. It's possible
+	//that timeout hasn't fired yet, so make sure the card content is up to date.
+	cardSetNormalizedTextProperties(cardCopy);
+	const fingerprint = getSemanticFingerprintForCard(state, cardCopy);
 	const prettyFingerprint = fingerprint ? [...fingerprint.keys()].slice(0, NUM_TERMS_OF_FINGERPRINT).join(' ') : '';
 	const title = WORKING_NOTES_TITLE_PREFIX + ' ' + date.toLocaleDateString('en-US', {month:'numeric', day:'numeric', year:'2-digit'}) + ' ' + prettyFingerprint;
 	return {
