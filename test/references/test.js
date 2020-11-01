ce369343e8b8147e32a5cacc6b790687b0392d90
Alex Komoroske <jkomoros@gmail.com>
2020-11-01 10:01:07 -0800

    Test the rest of referencesDiff. Part of #354.

--- test/references/test.js
+++ test/references/test.js
@@ -4,6 +4,7 @@ import {
 	referencesLegal,
 	referencesDiff,
 	REFERENCE_TYPE_LINK,
+	REFERENCE_TYPE_DUPE_OF,
 } from '../../src/card_fields.js';
 
 import assert from 'assert';
@@ -159,4 +160,156 @@ describe('card referencesDiff util functions', () => {
 		const expectedResult = [expectedAdditions, {}, {}, {}];
 		assert.deepStrictEqual(result, expectedResult);
 	});
+
+	it('Add two items in new card', async () => {
+		const inputBefore = {
+		};
+		const inputAfter = {
+			'foo': {
+				[REFERENCE_TYPE_LINK]: 'value',
+				[REFERENCE_TYPE_DUPE_OF]: 'other-value',
+			},
+		};
+		const result = referencesDiff(inputBefore, inputAfter);
+		const expectedAdditions = {
+			['foo.' + REFERENCE_TYPE_LINK]: 'value',
+			['foo.' + REFERENCE_TYPE_DUPE_OF]: 'other-value',
+		};
+		const expectedResult = [expectedAdditions, {}, {}, {}];
+		assert.deepStrictEqual(result, expectedResult);
+	});
+
+	it('Add item to existing card', async () => {
+		const inputBefore = {
+			'foo': {
+				[REFERENCE_TYPE_LINK]: 'value',
+			},
+		};
+		const inputAfter = {
+			'foo': {
+				[REFERENCE_TYPE_LINK]: 'value',
+				[REFERENCE_TYPE_DUPE_OF]: 'other-value',
+			},
+		};
+		const result = referencesDiff(inputBefore, inputAfter);
+		const expectedAdditions = {
+			['foo.' + REFERENCE_TYPE_DUPE_OF]: 'other-value',
+		};
+		const expectedResult = [expectedAdditions, {}, {}, {}];
+		assert.deepStrictEqual(result, expectedResult);
+	});
+
+	it('Remove existing card', async () => {
+		const inputBefore = {
+			'foo': {
+				[REFERENCE_TYPE_LINK]: 'value',
+			},
+		};
+		const inputAfter = {
+		};
+		const result = referencesDiff(inputBefore, inputAfter);
+		const expectedCardDeletions = {
+			'foo': true,
+		};
+		const expectedResult = [{}, {}, {}, expectedCardDeletions];
+		assert.deepStrictEqual(result, expectedResult);
+	});
+
+	it('Remove item from existing card', async () => {
+		const inputBefore = {
+			'foo': {
+				[REFERENCE_TYPE_LINK]: 'value',
+				[REFERENCE_TYPE_DUPE_OF]: 'other-value',
+			},
+		};
+		const inputAfter = {
+			'foo': {
+				[REFERENCE_TYPE_LINK]: 'value',
+			},
+		};
+		const result = referencesDiff(inputBefore, inputAfter);
+		const expectedLeafDeletions = {
+			['foo.' + REFERENCE_TYPE_DUPE_OF]: true,
+		};
+		const expectedResult = [{}, {}, expectedLeafDeletions, {}];
+		assert.deepStrictEqual(result, expectedResult);
+	});
+
+	it('Modify single item in card', async () => {
+		const inputBefore = {
+			'foo': {
+				[REFERENCE_TYPE_LINK]: 'before-value',
+			},
+		};
+		const inputAfter = {
+			'foo': {
+				[REFERENCE_TYPE_LINK]: 'value',
+			},
+		};
+		const result = referencesDiff(inputBefore, inputAfter);
+		const expectedModifications = {
+			['foo.' + REFERENCE_TYPE_LINK]: 'value',
+		};
+		const expectedResult = [{}, expectedModifications, {}, {}];
+		assert.deepStrictEqual(result, expectedResult);
+	});
+
+	it('Modify multiple items in card', async () => {
+		const inputBefore = {
+			'foo': {
+				[REFERENCE_TYPE_LINK]: 'before-value',
+				[REFERENCE_TYPE_DUPE_OF]: 'before-other-value',
+			},
+		};
+		const inputAfter = {
+			'foo': {
+				[REFERENCE_TYPE_LINK]: 'value',
+				[REFERENCE_TYPE_DUPE_OF]: 'other-value',
+			},
+		};
+		const result = referencesDiff(inputBefore, inputAfter);
+		const expectedModifications = {
+			['foo.' + REFERENCE_TYPE_LINK]: 'value',
+			['foo.' + REFERENCE_TYPE_DUPE_OF]: 'other-value',
+		};
+		const expectedResult = [{}, expectedModifications, {}, {}];
+		assert.deepStrictEqual(result, expectedResult);
+	});
+
+	it('Multiple changes', async () => {
+		const inputBefore = {
+			'deletion-card': {
+				[REFERENCE_TYPE_LINK]: 'before-value',
+				[REFERENCE_TYPE_DUPE_OF]: 'before-other-value',
+			},
+			'modification-card': {
+				[REFERENCE_TYPE_LINK]: 'before-value',
+				[REFERENCE_TYPE_DUPE_OF]: 'before-other-value',
+			},
+		};
+		const inputAfter = {
+			'modification-card': {
+				[REFERENCE_TYPE_LINK]: 'after-value',
+			},
+			'addition-card': {
+				[REFERENCE_TYPE_LINK]: 'after-value',
+			},
+		};
+		const result = referencesDiff(inputBefore, inputAfter);
+		const expectedAdditions = {
+			['addition-card.' + REFERENCE_TYPE_LINK]: 'after-value',
+		};
+		const expectedModifications = {
+			['modification-card.' + REFERENCE_TYPE_LINK]: 'after-value',
+		};
+		const expectedLeafDeletions = {
+			['modification-card.' + REFERENCE_TYPE_DUPE_OF]: true,
+		};
+		const expectedCardDeletions = {
+			'deletion-card': true,
+		};
+		const expectedResult = [expectedAdditions, expectedModifications, expectedLeafDeletions, expectedCardDeletions];
+		assert.deepStrictEqual(result, expectedResult);
+	});
+
 });
