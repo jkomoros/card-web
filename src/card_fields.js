jkomoros/card-web

commit 071f2e781b45d267cf4555f4ca2409ea2916c234

Change how we setLinks on a card object to also be indirected. Left a TODO comment that this approach won't work when we're leaving some references the same. Part of #354.

src/card_fields.js
@@ -237,3 +237,12 @@ export const cardGetInboundLinksArray = (cardObj) => {
 export const cardGetInboundReferencesArray = (cardObj) => {
 	return cardGetInboundLinksArray(cardObj);
 };
+
+//linksObj should be a object with CARD_ID: link text (or '' if no link text).
+//Any previously-existing links on the card are cleared.
+export const cardSetLinks = (cardObj, linksObj) => {
+	if (!cardObj) return;
+	if (!linksObj) linksObj = {};
+	cardObj.links = Object.keys(linksObj);
+	cardObj.links_text = Object.values(linksObj);
+};

src/actions/data.js
@@ -89,7 +89,8 @@ import {
 	TEXT_FIELD_BODY,
 	CARD_TYPE_CONTENT,
 	CARD_TYPE_SECTION_HEAD,
-	CARD_TYPE_WORKING_NOTES
+	CARD_TYPE_WORKING_NOTES,
+	cardSetLinks
 } from '../card_fields.js';
 
 //When a new tag is created, it is randomly assigned one of these values.
@@ -198,8 +199,9 @@ export const modifyCard = (card, update, substantive, optBatch) => (dispatch, ge
 		cardUpdateObject[field] = update[field];
 		if (field != TEXT_FIELD_BODY) continue;
 		let linkInfo = extractCardLinksFromBody(update[field]);
-		cardUpdateObject.links = linkInfo[0];
-		cardUpdateObject.links_text = linkInfo[1];
+		//TODO: this won't work in cases where we're only partially updating the
+		//card's references.
+		cardSetLinks(cardUpdateObject, linkInfo);
 	}
 
 	if (update.notes !== undefined) {

src/reducers/editor.js
@@ -40,7 +40,8 @@ import {
 } from '../util.js';
 
 import {
-	cardSetNormalizedTextProperties
+	cardSetNormalizedTextProperties,
+	cardSetLinks
 } from '../card_fields.js';
 
 import {
@@ -131,7 +132,8 @@ const app = (state = INITIAL_STATE, action) => {
 		//links to be updated away. This is also when we do expensive processing
 		//of body, like re-extracting words to cause suggested tags to change.
 		let linkInfo = extractCardLinksFromBody(state.card.body);
-		card = {...state.card, links:linkInfo[0], links_text: linkInfo[1]};
+		card = {...state.card};
+		cardSetLinks(card, linkInfo);
 		cardSetNormalizedTextProperties(card);
 		return {
 			...state,

src/util.js
@@ -232,15 +232,17 @@ export const extractCardLinksFromBody = (body) => {
 	let ele = document.createElement('section');
 	//TODO: is there an XSS vulnerability here?
 	ele.innerHTML = body;
-	let result = [];
+	let result = {};
 	let nodes = ele.querySelectorAll('card-link[card]');
-	let text = {};
 	nodes.forEach(link => {
 		const id = link.getAttribute('card');
-		result.push(id);
-		text[id] = (text[id] ? text[id] + MULTIPLE_LINK_TEXT_DELIMITER : '') + link.innerText;
+		if (result[id]) {
+			result[id] = result[id] + MULTIPLE_LINK_TEXT_DELIMITER + link.innerText;
+		} else {
+			result[id] = link.innerText;
+		}
 	});
-	return [arrayUnique(result), text];
+	return result;
 };
 
 export const arrayRemove = (arr, items) => {

commit 35068f278f6217f4595acc9f57d96c5d8ca8612f

Fixed a bug introduced in 071f2e78 where links_text was an array, not a map as it's supposed to be. This bug will go away when the underlying data model changes. Part of #354.

src/card_fields.js
@@ -276,7 +276,7 @@ export const cardSetLinks = (cardObj, linksObj) => {
 	if (!cardObj) return;
 	if (!linksObj) linksObj = {};
 	cardObj.links = Object.keys(linksObj);
-	cardObj.links_text = Object.values(linksObj);
+	cardObj.links_text = {...linksObj};
 };
 
 //referencesLegal is a sanity check that the referencesBlock looks like it's expected to.