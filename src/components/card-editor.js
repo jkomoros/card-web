9c1d60be2f228caee72ace646f41cba37ef13b39
Alex Komoroske <jkomoros@gmail.com>
2020-11-08 11:30:52 -0800

    Render links in the editor as subtle. This fixes #354.

--- src/components/card-editor.js
+++ src/components/card-editor.js
@@ -357,7 +357,7 @@ class CardEditor extends connect(store)(LitElement) {
 					${Object.entries(REFERENCE_TYPES).filter(entry => referencesMap[entry[0]]).map(entry => {
 		return html`<div>
 							<label>${entry[1].name} ${this._help(entry[1].description, false)}</label>
-							<tag-list .overrideTypeName=${'Reference'} .referenceType=${entry[0]} .tagInfos=${this._cardTagInfos} .defaultColor=${entry[1].color} .tags=${referencesMap[entry[0]]} .editing=${entry[1].editable} .tapEvents=${true} .disableAdd=${true} @remove-tag=${this._handleRemoveReference}></tag-list>
+							<tag-list .overrideTypeName=${'Reference'} .referenceType=${entry[0]} .tagInfos=${this._cardTagInfos} .defaultColor=${entry[1].color} .tags=${referencesMap[entry[0]]} .editing=${entry[1].editable} .subtle=${!entry[1].editable} .tapEvents=${true} .disableAdd=${true} @remove-tag=${this._handleRemoveReference}></tag-list>
 						</div>`;
 	})}
 				</div>
