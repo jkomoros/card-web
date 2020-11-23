9b1ce0e81917bdb81b30e8d21f1a13b90d0e8be4
Alex Komoroske <jkomoros@gmail.com>
2020-11-22 16:14:02 -0800

    Add reference_inbound properties to the test. Part of #365.

--- test/fingerprint/test.js
+++ test/fingerprint/test.js
@@ -17,7 +17,10 @@ import {
 } from '../../src/nlp.js';
 
 import {
-	CARD_TYPE_CONTENT
+	CARD_TYPE_CONTENT,
+	REFERENCE_TYPE_LINK,
+	REFERENCE_TYPE_ACK,
+	REFERENCE_TYPE_DUPE_OF,
 } from '../../src/card_fields.js';
 
 import assert from 'assert';
@@ -39,6 +42,17 @@ const baseCards = (extras) => {
 				//Note: this is the content of card g487aed6370_0_25 from production
 				'body': '<p>The <card-link href="https://en.wikipedia.org/wiki/Cynefin_framework">Cynefin model</card-link> divides problems into four types, each with different properties.</p><ul><li><strong>Simple</strong> - Trivial problem spaces that require no special effort.</li><li><strong>Complicated</strong> - Knowably hard. Intricate and challenging, but concrete and black and white. Efficiency will require a focus on the right process and structure.</li><li><strong>Complex</strong> - <em>Unknowably</em> hard. Goals, methods, and even possible next actions are unclear, meaning <card-link card="g487aed6370_0_61">fundamentally different approaches are required</card-link>.</li><li><strong>Chaotic</strong> - So unknowably hard as to be inscrutable--impossible to control or predict. <card-link card="g487aed6370_0_30">Beware diagnosing problems as chaotic</card-link> because it’s effectively giving up.</li></ul><p>Distinguishing between <card-link card="g487aed6370_0_45">complex and complicated</card-link> is the most important in practice.</p><p class="small"><em>Note on terminology: in the past I called what Cynefin calls “complex” “ambiguous”, and what it calls “complicated”, “complex”.  I’ve shifted to use Cynfefin’s terminology consistently.</em></p',
 				'title': 'Using the cynefin model to understand problem spaces',
+				'references_info_inbound': {
+					'card_1': {
+						[REFERENCE_TYPE_LINK]: 'cynefin model\ncynefin\'s model',
+					},
+					'card_2': {
+						[REFERENCE_TYPE_ACK]: '',
+					},
+					'card_3': {
+						[REFERENCE_TYPE_DUPE_OF]: 'cynenfin model dupe',
+					}
+				}
 			}
 		}
 	};
@@ -65,7 +79,7 @@ describe('fingerprint generation', () => {
 				'body': 'the cynefin model divid problem into four type each with differ properti simpl trivial problem space that requir no special effort complic knowabl hard intric and challeng but concret and black and white effici will requir a focu on the right process and structur complex unknow hard goal method and even possibl next action ar unclear mean fundament differ approach ar requir chaotic so unknow hard as to be inscrut imposs to control or predict bewar diagnos problem as chaotic becaus it’ effect give up distinguish between complex and complic is the most import in practic note on terminolog in the past i call what cynefin call complex ambigu and what it call complic complex i’v shift to us cynfefin’ terminolog consist',
 				'title': 'us the cynefin model to understand problem space',
 				'subtitle': '',
-				'references_info_inbound': '',
+				'references_info_inbound': 'cynefin model cynefin\' model cynenfin model dupe',
 			},
 		};
 		for (let [cardID, normalized] of Object.entries(expectedNormalized)) {
