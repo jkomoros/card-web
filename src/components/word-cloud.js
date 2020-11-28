288795d5cefc7ca4fb39ed7d4e98bf978c18e24e
Alex Komoroske <jkomoros@gmail.com>
2020-11-28 09:14:24 -0800

    Wire it up so tapping on an item in a word cloud filters for that query item in the current collection by dispatchign navigateToCollectionWithQuery. Part of #372.

--- src/components/word-cloud.js
+++ src/components/word-cloud.js
@@ -1,22 +1,38 @@
 import { html, LitElement } from '@polymer/lit-element';
 
+import { connect } from 'pwa-helpers/connect-mixin.js';
+
+// This element is connected to the Redux store.
+import { store } from '../store.js';
+
 import {
 	emptyWordCloud
 } from '../nlp.js';
 
+import {
+	navigateToCollectionWithQuery
+} from '../actions/app.js';
+
 import './tag-list.js';
 
 // This element is *not* connected to the Redux store.
-export class WordCloud extends LitElement {
+export class WordCloud extends connect(store)(LitElement) {
 	render() {
 		return html`
 		<!-- --app-primary-color default color -->
-		<tag-list .tags=${this._effectiveWordCloud[0]} .tagInfos=${this._effectiveWordCloud[1]} defaultColor='#5e2b97'></tag-list>
+		<tag-list .tags=${this._effectiveWordCloud[0]} .tagInfos=${this._effectiveWordCloud[1]} defaultColor='#5e2b97' .tapEvents=${true} @tag-tapped=${this._handleTagTapped}></tag-list>
 	`;
 	}
 
 	get _effectiveWordCloud() {
 		return this.wordCloud || emptyWordCloud();
+	}
+
+	_handleTagTapped(e) {
+		const tagName = e.detail.tag;
+		const tagInfos = this._effectiveWordCloud[1];
+		const query = tagInfos[tagName] ? tagInfos[tagName].title : tagName;
+		store.dispatch(navigateToCollectionWithQuery(query.toLowerCase()));
 	}
 
 	static get properties() {
