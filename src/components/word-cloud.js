83d9eb0bb551104d6083335269c9d6f51da90d74
Alex Komoroske <jkomoros@gmail.com>
2020-11-28 08:52:56 -0800

    Define a starter word-cloud element. Not yet used. Part of #372.

---
+++ src/components/word-cloud.js
@@ -0,0 +1,30 @@
+import { html, LitElement } from '@polymer/lit-element';
+
+import {
+	emptyWordCloud
+} from '../nlp.js';
+
+import './tag-list.js';
+
+// This element is *not* connected to the Redux store.
+export class WordCloud extends LitElement {
+	render() {
+		return html`
+		<tag-list .tags=${this.wordCloud[0]} .tagInfos=${this.wordCloud[1]} .defaultColor='#5e2b97'></tag-list>
+	`;
+	}
+
+	constructor() {
+		super();
+		this.wordCloud = emptyWordCloud();
+	}
+
+	static get properties() {
+		return {
+			wordCloud: {type:Object},
+		};
+	}
+
+}
+
+window.customElements.define('word-cloud', WordCloud);
