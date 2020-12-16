jkomoros/card-web

commit 7799a473421777e612514c0ffc2fdb484e8d872a

Get rid of specialized card renderers, and just have base-card render all of the right fields in order.
There's still a layer of indirection between card-renderer and base-card for no good reason. That will be removed in next commit.

Part of #362.

src/components/card-renderer.js
@@ -1,31 +1,11 @@
 import { LitElement, html } from '@polymer/lit-element';
 
-import {
-	CARD_TYPE_CONCEPT,
-	CARD_TYPE_CONTENT,
-	CARD_TYPE_SECTION_HEAD,
-	CARD_TYPE_WORKING_NOTES,
-} from '../card_fields.js';
-
-import './content-card.js';
-import './section-head-card.js';
-import './working-notes-card.js';
-import './concept-card.js';
+import './base-card.js';
 
 // This element is *not* connected to the Redux store.
 export class CardRenderer extends LitElement {
 	render() {
-		switch(this._cardType) {
-		case CARD_TYPE_SECTION_HEAD:
-			return html` <section-head-card id='card' .dataIsFullyLoaded=${this.dataIsFullyLoaded} ?hidden=${this._cardType != CARD_TYPE_SECTION_HEAD} .updatedFromContentEditable=${this.updatedFromContentEditable} .editing=${this.editing} .card=${this.card}></section-head-card>`;
-		case CARD_TYPE_WORKING_NOTES:
-			return html`<working-notes-card id='card' .dataIsFullyLoaded=${this.dataIsFullyLoaded} ?hidden=${this._cardType != CARD_TYPE_WORKING_NOTES} .updatedFromContentEditable=${this.updatedFromContentEditable} .editing=${this.editing} .card=${this.card}></working-notes-card>`;
-		case CARD_TYPE_CONCEPT:
-			return html`<concept-card id='card' .dataIsFullyLoaded=${this.dataIsFullyLoaded} ?hidden=${this._cardType != CARD_TYPE_CONCEPT} .updatedFromContentEditable=${this.updatedFromContentEditable} .editing=${this.editing} .card=${this.card}></concept-card>`;
-		case CARD_TYPE_CONTENT:
-		default:
-			return html`<content-card id='card' .dataIsFullyLoaded=${this.dataIsFullyLoaded} ?hidden=${this._cardType != CARD_TYPE_CONTENT} .updatedFromContentEditable=${this.updatedFromContentEditable} .editing=${this.editing} .card=${this.card}></content-card>`;
-		}
+		return html`<base-card id='card' .dataIsFullyLoaded=${this.dataIsFullyLoaded} .updatedFromContentEditable=${this.updatedFromContentEditable} .editing=${this.editing} .card=${this.card}></base-card>`;
 	}
 
 	static get properties() {
@@ -35,10 +15,6 @@ export class CardRenderer extends LitElement {
 			updatedFromContentEditable: {type:Object},
 			dataIsFullyLoaded: {type:Boolean},
 		};
-	}
-
-	get _cardType() {
-		return this.card && Object.keys(this.card).length ? this.card.card_type : CARD_TYPE_CONTENT;
 	}
 
 	get activeCardEle () {

src/components/base-card.js
@@ -24,7 +24,9 @@ import {
 	TEXT_FIELD_BODY,
 	TEXT_FIELD_TITLE,
 	TEXT_FIELD_CONFIGURATION,
-	CARD_TYPE_CONFIGURATION
+	CARD_TYPE_CONFIGURATION,
+	CARD_TYPE_CONTENT,
+	editableFieldsForCardType
 } from '../card_fields.js';
 
 import { makeElementContentEditable } from '../util.js';
@@ -42,8 +44,10 @@ const EPSILON = 0.5;
 // This element is *not* connected to the Redux store.
 export class BaseCard extends GestureEventListeners(LitElement) {
 	render() {
-		const cardTypeConfig = CARD_TYPE_CONFIGURATION[this._card.card_type] || {};
+		const cardType = this._card.card_type || CARD_TYPE_CONTENT;
+		const cardTypeConfig = CARD_TYPE_CONFIGURATION[cardType] || {};
 		const styleBlock = cardTypeConfig.styleBlock || html``;
+		const fieldsToRender = editableFieldsForCardType(cardType);
 		return html`
 			${SharedStyles}
 			${badgeStyles}
@@ -167,15 +171,10 @@ export class BaseCard extends GestureEventListeners(LitElement) {
 			${styleBlock}
 			<div class="container ${this.editing ? 'editing' : ''} ${this._card.published ? 'published' : 'unpublished'}">
 				<div class='background'></div>
-				${this.innerRender()}
+				${Object.keys(fieldsToRender).map(fieldName => this._templateForField(fieldName))}
 				${starBadge(this._card.star_count)}
 			</div>
 		`;
-	}
-
-	innerRender() {
-		//Subclasess override this
-		return '';
 	}
 
 	get _card() {

src/components/concept-card.js
@@ -1,22 +1,0 @@
-import { html } from '@polymer/lit-element';
-
-import {BaseCard} from './base-card.js';
-
-import './card-link.js';
-
-import {
-	TEXT_FIELD_BODY,
-	TEXT_FIELD_TITLE,
-} from '../card_fields.js';
-
-// This element is *not* connected to the Redux store.
-export class ConceptCard extends BaseCard {
-	innerRender() {
-		return html`
-      ${this._templateForField(TEXT_FIELD_TITLE)}
-      ${this._templateForField(TEXT_FIELD_BODY)}
-    `;
-	}
-}
-
-window.customElements.define('concept-card', ConceptCard);

src/components/content-card.js
@@ -1,23 +1,0 @@
-import { html } from '@polymer/lit-element';
-
-import {BaseCard} from './base-card.js';
-
-import './card-link.js';
-
-
-import {
-	TEXT_FIELD_BODY,
-	TEXT_FIELD_TITLE,
-} from '../card_fields.js';
-
-// This element is *not* connected to the Redux store.
-export class ContentCard extends BaseCard {
-	innerRender() {
-		return html`
-      ${this._templateForField(TEXT_FIELD_TITLE)}
-      ${this._templateForField(TEXT_FIELD_BODY)}
-    `;
-	}
-}
-
-window.customElements.define('content-card', ContentCard);

src/components/section-head-card.js
@@ -1,20 +1,0 @@
-import { html } from '@polymer/lit-element';
-
-import {
-	TEXT_FIELD_TITLE,
-	TEXT_FIELD_SUBTITLE,
-} from '../card_fields.js';
-
-import {BaseCard} from './base-card.js';
-
-// This element is *not* connected to the Redux store.
-class SectionHeadCard extends BaseCard {
-	innerRender() {
-		return html`
-      ${this._templateForField(TEXT_FIELD_TITLE)}
-      ${this._templateForField(TEXT_FIELD_SUBTITLE)}
-    `;
-	}
-}
-
-window.customElements.define('section-head-card', SectionHeadCard);

src/components/working-notes-card.js
@@ -1,21 +1,0 @@
-import { html } from '@polymer/lit-element';
-
-import {BaseCard} from './base-card.js';
-
-import './card-link.js';
-
-
-import {
-	TEXT_FIELD_BODY,
-} from '../card_fields.js';
-
-// This element is *not* connected to the Redux store.
-export class WorkingNotesCard extends BaseCard {
-	innerRender() {
-		return html`
-      ${this._templateForField(TEXT_FIELD_BODY)}
-    `;
-	}
-}
-
-window.customElements.define('working-notes-card', WorkingNotesCard);

commit 3d95b75917214a6d0f093628db977ba247404724

Fixed #402, which was broken in 7799a473421777e612514c0ffc2fdb484e8d872a.
Card-renderer needs to import card-link, because basic-card-view doesn't load anything else and needs card-link to be defined for cards with a body.

src/components/card-renderer.js
@@ -31,6 +31,9 @@ import {
 
 import { makeElementContentEditable } from '../util.js';
 
+//Cards that include links need card-link
+import './card-link.js';
+
 export const CARD_WIDTH_IN_EMS = 43.63;
 export const CARD_HEIGHT_IN_EMS = 24.54;
 export const CARD_VERTICAL_PADDING_IN_EMS = 1.0;