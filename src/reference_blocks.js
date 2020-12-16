jkomoros/card-web

commit 2c2e4c913e0ddc28b3d1ba7fb84a919a34347326

Move enough logic into infoPanelReferenceBlocks that we could use it to generate the first section in the info panel. Not yet used. Part of #400.

src/reference_blocks.js
@@ -1,7 +1,8 @@
 import {
 	CARD_TYPE_CONCEPT,
 	REFERENCE_TYPE_ACK,
-	SELF_KEY_CARD_ID
+	SELF_KEY_CARD_ID,
+	REFERENCE_TYPES,
 } from './card_fields.js';
 
 import {
@@ -12,6 +13,7 @@ import {
 import {
 	EVERYTHING_SET_NAME,
 	DIRECT_REFERENCES_INBOUND_FILTER_NAME,
+	DIRECT_REFERENCES_OUTBOUND_FILTER_NAME,
 	referencesConfigurableFilterText,
 } from './filters.js';
 
@@ -34,7 +36,15 @@ const REFERENCE_BLOCKS_FOR_CARD_TYPE = {
 	]
 };
 
-const INFO_PANEL_REFERENCE_BLOCKS = [];
+const REFERENCE_TYPES_TO_EXCLUDE_FROM_INFO_PANEL = Object.entries(REFERENCE_TYPES).filter(entry => !entry[1].excludeFromInfoPanel).map(entry => entry[1]);
+
+const INFO_PANEL_REFERENCE_BLOCKS = [
+	{
+		title: 'Other referenced cards',
+		description: 'Cards that this card references that are not links',
+		collection: new CollectionDescription(EVERYTHING_SET_NAME, [referencesConfigurableFilterText(DIRECT_REFERENCES_OUTBOUND_FILTER_NAME, SELF_KEY_CARD_ID, REFERENCE_TYPES_TO_EXCLUDE_FROM_INFO_PANEL, true)])
+	}
+];
 
 export const primaryReferenceBlocksForCard = (card) => {
 	if (!card) return []; 

src/filters.js
@@ -380,7 +380,7 @@ const REFERENCES_INBOUND_FILTER_NAME = REFERENCES_FILTER_NAME + INBOUND_SUFFIX;
 const REFERENCES_OUTBOUND_FILTER_NAME = REFERENCES_FILTER_NAME + OUTBOUND_SUFFIX;
 const DIRECT_REFERENCES_FILTER_NAME = DIRECT_PREFIX + REFERENCES_FILTER_NAME;
 export const DIRECT_REFERENCES_INBOUND_FILTER_NAME = DIRECT_PREFIX + REFERENCES_INBOUND_FILTER_NAME;
-const DIRECT_REFERENCES_OUTBOUND_FILTER_NAME = DIRECT_PREFIX + REFERENCES_OUTBOUND_FILTER_NAME;
+export const DIRECT_REFERENCES_OUTBOUND_FILTER_NAME = DIRECT_PREFIX + REFERENCES_OUTBOUND_FILTER_NAME;
 const CARDS_FILTER_NAME = 'cards';
 const EXCLUDE_FILTER_NAME = 'exclude';
 const COMBINE_FILTER_NAME = 'combine';

commit ecbd2fe0cef99800d51174774b089daa24ec982e

Fix a bug introduced in 2c2e4c913e0ddc28b3d1ba7fb84a919a34347326 where the wrong card references were hidden from the "Other Cards This Card References" info panel segment. Part of #400.
It was a twofer: we returned the inverse of the right set, and also selected the config obj, not its name!

src/reference_blocks.js
@@ -45,7 +45,7 @@ const REFERENCE_BLOCKS_FOR_CARD_TYPE = {
 	]
 };
 
-const REFERENCE_TYPES_TO_EXCLUDE_FROM_INFO_PANEL = Object.entries(REFERENCE_TYPES).filter(entry => !entry[1].excludeFromInfoPanel).map(entry => entry[1]);
+const REFERENCE_TYPES_TO_EXCLUDE_FROM_INFO_PANEL = Object.entries(REFERENCE_TYPES).filter(entry => entry[1].excludeFromInfoPanel).map(entry => entry[0]);
 const SUBSTANTIVE_REFERENCE_TYPES = Object.entries(REFERENCE_TYPES).filter(entry => entry[1].substantive).map(entry => entry[0]);
 
 const NUM_SIMILAR_CARDS_TO_SHOW = 5;