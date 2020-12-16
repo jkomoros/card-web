jkomoros/card-web

commit fd57bbd4af5b5b42e313288afd8cdf86ddecb437

Removed a bit of state that was stored as a global variable to be properly stored in state, since in certain cases we'll need to unflip it. Part of #379.

src/actions/collection.js
@@ -42,7 +42,8 @@ import {
 	getCardIndexForActiveCollection,
 	selectActiveCollectionDescription,
 	selectActiveCollectionContainsCards, 
-	selectPendingNewCardID
+	selectPendingNewCardID,
+	selectAlreadyCommittedModificationsWhenFullyLoaded
 } from '../selectors.js';
 
 import {
@@ -50,7 +51,8 @@ import {
 } from '../card_fields.js';
 
 import {
-	navigatedToNewCard
+	navigatedToNewCard,
+	committedFiltersWhenFullyLoaded
 } from './data.js';
 
 import {
@@ -162,10 +164,6 @@ export const commitPendingCollectionModifications = () => {
 	return {type:COMMIT_PENDING_COLLECTION_MODIFICATIONS};
 };
 
-//Keep track of whether we've called refreshCardSelector once already when the
-//data is fully loaded.
-let refreshCardSelectorHasSeenDataFullyLoaded = false;
-
 export const refreshCardSelector = (forceCommit) => (dispatch, getState) => {
 	//Called when cards and sections update, just in case we now have
 	//information to do this better. Also called when stars and reads update,
@@ -182,8 +180,9 @@ export const refreshCardSelector = (forceCommit) => (dispatch, getState) => {
 	let pageExtra = selectPageExtra(state);
 
 	const dataIsFullyLoaded = selectDataIsFullyLoaded(state);
+	const alreadyCommittedModificationsWhenFullyLoaded = selectAlreadyCommittedModificationsWhenFullyLoaded(state);
 
-	if (!dataIsFullyLoaded || (dataIsFullyLoaded && !refreshCardSelectorHasSeenDataFullyLoaded) || forceCommit) {
+	if (!dataIsFullyLoaded || (dataIsFullyLoaded && !alreadyCommittedModificationsWhenFullyLoaded) || forceCommit) {
 		//This action creator gets called when ANYTHING that could have changed
 		//the collection gets called. If we got called before everything is
 		//fully loaded, then we should make sure that the more recent
@@ -200,7 +199,9 @@ export const refreshCardSelector = (forceCommit) => (dispatch, getState) => {
 		dispatch(commitPendingCollectionModifications());
 	}
 
-	if (dataIsFullyLoaded) refreshCardSelectorHasSeenDataFullyLoaded = true;
+	if (dataIsFullyLoaded && alreadyCommittedModificationsWhenFullyLoaded) {
+		dispatch(committedFiltersWhenFullyLoaded());
+	}
 
 	dispatch(updateCardSelector(pageExtra));
 };

src/actions/data.js
@@ -13,6 +13,7 @@ export const SET_PENDING_SLUG = 'SET_PENDING_SLUG';
 export const EXPECT_NEW_CARD = 'EXPECT_NEW_CARD';
 export const NAVIGATED_TO_NEW_CARD = 'NAVIGATED_TO_NEW_CARD';
 export const EXPECT_CARD_DELETIONS = 'EXPECT_CARD_DELETIONS';
+export const COMMITTED_PENDING_FILTERS_WHEN_FULLY_LOADED = 'COMMITTED_PENDING_FILTERS_WHEN_FULLY_LOADED';
 
 import {
 	slugLegal,
@@ -1298,5 +1299,13 @@ export const fetchTweets = (card) => async (dispatch) => {
 		type: UPDATE_TWEETS,
 		tweets
 	});
+};
+
+//Denotes that we just did a pending filters commit when the data was fully
+//loaded... and shouldn't do it again.
+export const committedFiltersWhenFullyLoaded = () => {
+	return {
+		type: COMMITTED_PENDING_FILTERS_WHEN_FULLY_LOADED,
+	};
 };
 

src/reducers/data.js
@@ -13,6 +13,7 @@ import {
 	EXPECT_NEW_CARD,
 	NAVIGATED_TO_NEW_CARD,
 	EXPECT_CARD_DELETIONS,
+	COMMITTED_PENDING_FILTERS_WHEN_FULLY_LOADED,
 } from '../actions/data.js';
 
 const INITIAL_STATE = {
@@ -34,6 +35,10 @@ const INITIAL_STATE = {
 	unpublishedCardsLoaded: false,
 	sectionsLoaded: false,
 	tagsLoaded: false,
+	//keeps track of whether we committed any pending collections on being fully
+	//loaded already. If so, then even if refreshCardSelector gets called again,
+	//we won't update the collection again.
+	alreadyCommittedModificationsWhenFullyLoaded: false,
 	//The modification that is pending
 	cardModificationPending: '',
 	cardModificationError: null,
@@ -130,6 +135,11 @@ const app = (state = INITIAL_STATE, action) => {
 		return {
 			...state,
 			reorderPending: action.pending
+		};
+	case COMMITTED_PENDING_FILTERS_WHEN_FULLY_LOADED:
+		return {
+			...state,
+			alreadyCommittedModificationsWhenFullyLoaded: true,
 		};
 	default:
 		return state;

src/selectors.js
@@ -109,6 +109,7 @@ export const selectSectionsLoaded = (state) => state.data ? state.data.sectionsL
 export const selectTagsLoaded = (state) => state.data ? state.data.tagsLoaded : false;
 export const selectMessagesLoaded = (state) => state.comments ? state.comments.messagesLoaded : false;
 export const selectThreadsLoaded = (state) => state.comments ? state.comments.threadsLoaded : false;
+export const selectAlreadyCommittedModificationsWhenFullyLoaded = (state) => state.data ? state.data.alreadyCommittedModificationsWhenFullyLoaded : false;
 const selectSlugIndex = (state) => state.data ? state.data.slugIndex : {};
 export const selectMessages = (state) => state.comments ? state.comments.messages : null;
 export const selectThreads = (state) => state.comments ? state.comments.threads : null;

commit 9748fd582b0b0d16b3e5a6ad68865e078787df90

Fix a bug introduced in fd57bbd4af5b5b42e313288afd8cdf86ddecb437 that led to cards never being ghosted, and collections always refreshing even after fully loaded.
Fixes #403.

src/actions/collection.js
@@ -199,7 +199,7 @@ export const refreshCardSelector = (forceCommit) => (dispatch, getState) => {
 		dispatch(commitPendingCollectionModifications());
 	}
 
-	if (dataIsFullyLoaded && alreadyCommittedModificationsWhenFullyLoaded) {
+	if (dataIsFullyLoaded && !alreadyCommittedModificationsWhenFullyLoaded) {
 		dispatch(committedFiltersWhenFullyLoaded());
 	}
 