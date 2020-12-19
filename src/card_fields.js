jkomoros/card-web

commit 8047e68e9e2ae7482f934f27babab13d5224db76

Factor styles for the cards that need it into styleBlock configuration on cardType.
The remaining card-type eles are now very simple. Part of #362.

src/card_fields.js
@@ -3,6 +3,8 @@ import firebase from '@firebase/app';
 import '@firebase/firestore';
 const deleteSentinel = firebase.firestore.FieldValue.delete;
 
+import { html } from '@polymer/lit-element';
+
 /*
 
 On each card is a references property and a references info.
@@ -75,14 +77,53 @@ orphanedByDefault: if true, then the confirmation of 'You're about to make this
 card orphaned' will be flipped, and the natural location of them will be
 orphaned.
 
+styleBlock: if provided, will be rendered as the style block in the card
+renderer when this card type is selected.
+
 */
 
 export const CARD_TYPE_CONFIGURATION = {
 	[CARD_TYPE_CONTENT]: {},
-	[CARD_TYPE_SECTION_HEAD]: {},
+	[CARD_TYPE_SECTION_HEAD]: {
+		styleBlock: html`
+			<style>
+				.background {
+					position:absolute;
+					display:block;
+					height:50%;
+					bottom:0;
+					width:100%;
+					background-color: var(--app-primary-color);
+					/* counteract the padding in the base card */
+					margin-left:-1.45em;
+				}
+				h1 {
+					font:var(--app-header-font-family);
+					font-weight:bold;
+					font-size:3.0em;
+					margin-top:2.25em;
+				}
+				h2 {
+					color: var(--app-primary-color-subtle);
+					font-size:1.2em;
+					font-weight:normal;
+					position:absolute;
+					bottom:1em;
+				}
+			</style>
+		`
+	},
 	[CARD_TYPE_WORKING_NOTES]: {
 		invertContentPublishWarning: true,
 		orphanedByDefault: true,
+		styleBlock: html`
+		<style>
+			section {
+				font-size:0.8em;
+				line-height:1.2;
+			}
+		</style>
+		`
 	},
 	[CARD_TYPE_CONCEPT]: {
 		orphanedByDefault: true,

src/components/base-card.js
@@ -23,7 +23,8 @@ import {
 import {
 	TEXT_FIELD_BODY,
 	TEXT_FIELD_TITLE,
-	TEXT_FIELD_CONFIGURATION
+	TEXT_FIELD_CONFIGURATION,
+	CARD_TYPE_CONFIGURATION
 } from '../card_fields.js';
 
 import { makeElementContentEditable } from '../util.js';
@@ -41,6 +42,8 @@ const EPSILON = 0.5;
 // This element is *not* connected to the Redux store.
 export class BaseCard extends GestureEventListeners(LitElement) {
 	render() {
+		const cardTypeConfig = CARD_TYPE_CONFIGURATION[this._card.card_type] || {};
+		const styleBlock = cardTypeConfig.styleBlock || html``;
 		return html`
 			${SharedStyles}
 			${badgeStyles}
@@ -161,6 +164,7 @@ export class BaseCard extends GestureEventListeners(LitElement) {
 	})}
 
 			</style>
+			${styleBlock}
 			<div class="container ${this.editing ? 'editing' : ''} ${this._card.published ? 'published' : 'unpublished'}">
 				<div class='background'></div>
 				${this.innerRender()}

src/components/section-head-card.js
@@ -11,31 +11,6 @@ import {BaseCard} from './base-card.js';
 class SectionHeadCard extends BaseCard {
 	innerRender() {
 		return html`
-      <style>
-        .background {
-          position:absolute;
-          display:block;
-          height:50%;
-          bottom:0;
-          width:100%;
-          background-color: var(--app-primary-color);
-          /* counteract the padding in the base card */
-          margin-left:-1.45em;
-        }
-        h1 {
-          font:var(--app-header-font-family);
-          font-weight:bold;
-          font-size:3.0em;
-          margin-top:2.25em;
-        }
-        h2 {
-          color: var(--app-primary-color-subtle);
-          font-size:1.2em;
-          font-weight:normal;
-          position:absolute;
-          bottom:1em;
-        }
-      </style>
       ${this._templateForField(TEXT_FIELD_TITLE)}
       ${this._templateForField(TEXT_FIELD_SUBTITLE)}
     `;

src/components/working-notes-card.js
@@ -13,12 +13,6 @@ import {
 export class WorkingNotesCard extends BaseCard {
 	innerRender() {
 		return html`
-		<style>
-			section {
-				font-size:0.8em;
-				line-height:1.2;
-			}
-		</style>
       ${this._templateForField(TEXT_FIELD_BODY)}
     `;
 	}

commit c24dd495adf8a119dc3b1f07fd3245d7ca3bf618

Fixed a bug broken in 8047e68 where `npm run test:fingerprint` broke because card_fields.js had dependencies that include lit-html. Noticed while working on #412.

src/card_fields.js
@@ -3,10 +3,6 @@ import firebase from '@firebase/app';
 import '@firebase/firestore';
 const deleteSentinel = firebase.firestore.FieldValue.delete;
 
-import { html } from '@polymer/lit-element';
-
-import * as icons from './components/my-icons.js';
-
 /*
 
 On each card is a references property and a references info.
@@ -87,13 +83,16 @@ is published. This is useful for example for concept cards, where the primary
 content is the reference list.
 
 styleBlock: if provided, will be rendered as the style block in the card
-renderer when this card type is selected.
+renderer when this card type is selected. A string that will be run through html
+tag. This isn't an html tag to avoid having heavyweight imports so this can be
+included in tests.
 
 dark: if true, the card is considered dark, and styles for e.g. thumbnails,
 including badge color, will swap.
 
-icon: a reference from icons to show in front of the title everywhere it shows
-up
+iconName: a reference from icons to show in front of the title everywhere it
+shows up. A string that indexes into icons. This isn't an html tag to avoid
+having heavyweight imports so this can be included in tests.
 
 */
 
@@ -101,7 +100,7 @@ export const CARD_TYPE_CONFIGURATION = {
 	[CARD_TYPE_CONTENT]: {},
 	[CARD_TYPE_SECTION_HEAD]: {
 		dark: true,
-		styleBlock: html`
+		styleBlock: String.raw`
 			<style>
 				.background {
 					position:absolute;
@@ -132,7 +131,7 @@ export const CARD_TYPE_CONFIGURATION = {
 	[CARD_TYPE_WORKING_NOTES]: {
 		invertContentPublishWarning: true,
 		orphanedByDefault: true,
-		styleBlock: html`
+		styleBlock: `
 		<style>
 			section {
 				font-size:0.8em;
@@ -140,12 +139,12 @@ export const CARD_TYPE_CONFIGURATION = {
 			}
 		</style>
 		`,
-		icon: icons.INSERT_DRIVE_FILE_ICON,
+		iconName: 'INSERT_DRIVE_FILE_ICON',
 	},
 	[CARD_TYPE_CONCEPT]: {
 		orphanedByDefault: true,
 		publishedByDefault: true,
-		icon: icons.MENU_BOOK_ICON,
+		iconName: 'MENU_BOOK_ICON',
 	},
 };
 

src/components/card-drawer.js
@@ -35,6 +35,8 @@ import {
 	CARD_TYPE_CONFIGURATION,
 } from '../card_fields.js';
 
+import * as icons from './my-icons.js';
+
 class CardDrawer extends connect(store)(LitElement) {
 	render() {
 		return html`
@@ -263,7 +265,7 @@ class CardDrawer extends connect(store)(LitElement) {
 
 		return html`
 			<div  .card=${card} .index=${index} id=${'id-' + card.id} @dragstart='${this._handleDragStart}' @dragend='${this._handleDragEnd}' @mousemove=${this._handleThumbnailMouseMove} @click=${this._handleThumbnailClick} draggable='${this.editable ? 'true' : 'false'}' class="thumbnail ${card.id == this.highlightedCardId ? 'highlighted' : ''} ${cardTypeConfig.dark ? 'dark' : ''} ${card && card.published ? '' : 'unpublished'} ${this._collectionItemsToGhost[card.id] ? 'ghost' : ''} ${this.fullCards ? 'full' : 'partial'}">
-					${this.fullCards ? html`<card-renderer .card=${card} .expandedReferenceBlocks=${getExpandedPrimaryReferenceBlocksForCard(this.collection.constructorArguments, card)}></card-renderer>` : html`<h3 class='${hasContent ? '' : 'nocontent'}'>${cardTypeConfig.icon || ''}${title ? title : html`<span class='empty'>[Untitled]</span>`}</h3>`}
+					${this.fullCards ? html`<card-renderer .card=${card} .expandedReferenceBlocks=${getExpandedPrimaryReferenceBlocksForCard(this.collection.constructorArguments, card)}></card-renderer>` : html`<h3 class='${hasContent ? '' : 'nocontent'}'>${icons[cardTypeConfig.iconName] || ''}${title ? title : html`<span class='empty'>[Untitled]</span>`}</h3>`}
 					${cardBadges(cardTypeConfig.dark, card, this._badgeMap)}
 			</div>
 		`;

src/components/card-link.js
@@ -24,6 +24,8 @@ import {
 	CARD_TYPE_CONTENT
 } from '../card_fields.js';
 
+import * as icons from './my-icons.js';
+
 class CardLink extends connect(store)(LitElement) {
 	render() {
 
@@ -166,7 +168,7 @@ class CardLink extends connect(store)(LitElement) {
 	get _icon() {
 		if (!this._cardObj) return '';
 		const cardTypeConfig = CARD_TYPE_CONFIGURATION[this._cardObj.card_type] || {};
-		return cardTypeConfig.icon || '';
+		return icons[cardTypeConfig.iconName] || '';
 	}
 
 	get _inReadingList() {

src/components/card-renderer.js
@@ -52,7 +52,7 @@ export class CardRenderer extends GestureEventListeners(LitElement) {
 	render() {
 		const cardType = this._card.card_type || CARD_TYPE_CONTENT;
 		const cardTypeConfig = CARD_TYPE_CONFIGURATION[cardType] || {};
-		const styleBlock = cardTypeConfig.styleBlock || html``;
+		const styleBlock = html([cardTypeConfig.styleBlock || '']);
 		const fieldsToRender = editableFieldsForCardType(cardType);
 		return html`
 			${SharedStyles}