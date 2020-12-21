jkomoros/card-web

commit 672d1dbe8cb68ef8e482497e5131e9cbe0856767

Make it so when cards with a type that has autoSlug are created, they have their name/slug auto updated after they're crated. Part of #399.

src/actions/data.js
@@ -54,6 +54,7 @@ import {
 	normalizeSlug,
 	extractCardLinksFromBody,
 	reasonCardTypeNotLegalForCard,
+	createSlugFromArbitraryString,
 } from '../util.js';
 
 import {
@@ -942,6 +943,14 @@ export const createCard = (opts) => async (dispatch, getState) => {
 		}
 	}
 
+	let autoSlug = '';
+	let autoSlugLegalPromise = null;
+	if (CARD_TYPE_CONFIG.autoSlug) {
+		autoSlug = normalizeSlug(cardType + '-' + createSlugFromArbitraryString(title));
+		//Kick this off in parallel. We'll await it later.
+		autoSlugLegalPromise = slugLegal(autoSlug);
+	}
+
 	if (section) {
 
 		let sectionRef = db.collection(SECTIONS_COLLECTION).doc(obj.section);
@@ -987,6 +996,23 @@ export const createCard = (opts) => async (dispatch, getState) => {
 	//updated will call navigateToNewCard once the data is fully loaded again
 	//(if EXPECT_NEW_CARD was dispatched above). If noSectionChange is true
 	//above, it will only wait for the card, not the section, to load.
+
+	if (!autoSlug) return;
+
+	await waitForCardToExist(id);
+	const autoSlugLegal = await autoSlugLegalPromise;
+
+	if (!autoSlugLegal) {
+		console.warn('The autoSlug, ' + autoSlug + ' was not legal, so it will not be proposed.');
+		return;
+	}
+
+	try {
+		await addLegalSlugToCard(id, autoSlug, true);
+	} catch(err) {
+		console.warn('Couldn\'t add slug to card: ' + err);
+	}
+
 };
 
 export const createForkedCard = (cardToFork) => async (dispatch, getState) => {

src/card_fields.js
@@ -90,6 +90,9 @@ iconName: a reference from icons to show in front of the title everywhere it
 shows up. A string that indexes into icons. This isn't an html tag to avoid
 having heavyweight imports so this can be included in tests.
 
+autoSlug: if true, then when a new card is created, it will try to automatically
+add a name to the card that is `CARD_TYPE-NORMALIZED-TITLE`.
+
 */
 
 export const CARD_TYPE_CONFIGURATION = {
@@ -141,6 +144,7 @@ export const CARD_TYPE_CONFIGURATION = {
 		orphanedByDefault: true,
 		publishedByDefault: true,
 		iconName: 'MENU_BOOK_ICON',
+		autoSlug: true,
 	},
 };
 

commit d2cca6005d3155f69839b0d5d8385b03f83eba04

Fixed a bug introduced in 672d1dbe8cb68ef8e482497e5131e9cbe0856767
It erroneously checked slugLegal as a bool, but it's an object, so it always thought it was legal.

Part of #399.

src/actions/data.js
@@ -1000,10 +1000,10 @@ export const createCard = (opts) => async (dispatch, getState) => {
 	if (!autoSlug) return;
 
 	await waitForCardToExist(id);
-	const autoSlugLegal = await autoSlugLegalPromise;
+	const autoSlugLegalResult = await autoSlugLegalPromise;
 
-	if (!autoSlugLegal) {
-		console.warn('The autoSlug, ' + autoSlug + ' was not legal, so it will not be proposed.');
+	if (!autoSlugLegalResult.legal) {
+		console.warn('The autoSlug, ' + autoSlug + ' was not legal, so it will not be proposed. Reason: ' + autoSlugLegalResult.reason);
 		return;
 	}
 

src/actions/database.js
@@ -64,6 +64,7 @@ export const TWEETS_COLLECTION = 'tweets';
 const legalCallable = functions.httpsCallable('legal');
 const statusCallable = functions.httpsCallable('status');
 
+//slugLegal returns an object with {legal: bool, reason: string}
 export const slugLegal = async (newSlug) => {
 	const result = await legalCallable({type:'slug', value:newSlug});
 	return result.data;