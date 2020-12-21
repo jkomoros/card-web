jkomoros/card-web

commit e35a454770b4409ac657fe2dc4917929032554c6

Changed how fallbackTextMap is passed around. cardWithNormalizedTextProperties takes it and then stashes it on the card clone, so other nlp things downstream have access to if they want it
This ensures that all of the things like destemmedWordMap and others can use it without having to change their signature and have everything pass it alongside everywhere. Part of #412.

src/nlp.js
@@ -145,11 +145,11 @@ const splitRuns = (text) => {
 };
 
 //This is the set of card field extractors for any field types that have
-//overrideExtractor in card_fields.js. The function should take a card, and a
-//(possibly null) fallbackTextMap.
+//overrideExtractor in card_fields.js. The function should take a card. It will
+//use the stashed fallbackTextMap if it exists.
 const OVERRIDE_EXTRACTORS = {
-	[TEXT_FIELD_REFERENCES_NON_LINK_OUTBOUND]: (card, fallbackTextMap) => {
-		const refsByType = references(card).withFallbackText(fallbackTextMap).byType;
+	[TEXT_FIELD_REFERENCES_NON_LINK_OUTBOUND]: (card) => {
+		const refsByType = references(card).withFallbackText(card.fallbackTextMap).byType;
 		let result = [];
 		for (const [referenceType, cardMap] of Object.entries(refsByType)) {
 			//Skip links because they're already represented in body
@@ -163,9 +163,8 @@ const OVERRIDE_EXTRACTORS = {
 };
 
 //extractContentWords returns an object with the field to the non-de-stemmed
-//normalized words for each of the main properties. optFallbackTextMap will be
-//passed to the extractor func if provided.
-const extractContentWords = (card, optFallbackTextMap) => {
+//normalized words for each of the main properties.
+const extractContentWords = (card) => {
 	const cardType = card.card_type || '';
 	//These three properties are expected to be set by TEXT_SEARCH_PROPERTIES
 	//Fields that are derived are calculated based on other fields of the card
@@ -181,7 +180,7 @@ const extractContentWords = (card, optFallbackTextMap) => {
 			if (config.overrideExtractor) {
 				const extractor = OVERRIDE_EXTRACTORS[fieldName];
 				if (!extractor) throw new Error(fieldName + ' had overrideExtractor set, but no entry in OVERRIDE_EXTRACTORS');
-				fieldValue = extractor(card, optFallbackTextMap);
+				fieldValue = extractor(card);
 			} else {
 				fieldValue = extractFieldValueForIndexing(card[fieldName]);
 			}
@@ -240,16 +239,19 @@ const DEBUG_COUNT_NORMALIZED_TEXT_PROPERTIES = false;
 let normalizedCount = {};
 
 //cardWithNormalizedTextProperties sets the properties that search and
-//fingerprints work over, on a copy of the card it returns.
-export const cardWithNormalizedTextProperties = (card, fallbackTextMap) => {
+//fingerprints work over, on a copy of the card it returns. If
+//optFallbackTextMap is set, the result will also have that stashed on the
+//returned card obj.
+export const cardWithNormalizedTextProperties = (card, optFallbackTextMap) => {
 	if (!card) return card;
 	if (DEBUG_COUNT_NORMALIZED_TEXT_PROPERTIES) {
 		normalizedCount[card.id] = (normalizedCount[card.id] || 0) + 1;
 		if(normalizedCount[card.id] > 1) console.log(card.id, normalizedCount[card.id]);
 	}
 	const result = {...card};
+	if (optFallbackTextMap) result.fallbackTextMap = optFallbackTextMap;
 	//Basically it takes the output of extractContentWords and then stems each run.
-	result.normalized = Object.fromEntries(Object.entries(extractContentWords(card, fallbackTextMap)).map(entry => [entry[0], entry[1].map(str => stemmedNormalizedWords(str))]));
+	result.normalized = Object.fromEntries(Object.entries(extractContentWords(card)).map(entry => [entry[0], entry[1].map(str => stemmedNormalizedWords(str))]));
 	return result;
 };
 

src/actions/data.js
@@ -766,6 +766,9 @@ export const defaultCardObject = (id, user, section, cardType) => {
 		//Defaul to epoch 1970 for things not yet tweeted
 		last_tweeted: new Date(0),
 		tweet_count: 0
+		//Note: there are two fields that are often set on cards but not persisted to database:
+		//normalized - this is set by cardWithNormalizedTextProperties and is where all of the nlp-based machinery is based on.
+		//fallbackTextMap - this is stashed there so that the cardWithNormalizedTextProperties machinery can fetch it if it wants.
 	};
 };
 

src/selectors.js
@@ -77,7 +77,7 @@ import {
 } from './permissions.js';
 
 import {
-	backportFallbackMapForCard
+	backportFallbackMapForCard,
 } from './util.js';
 
 import {

commit 60f8389206394a3f0b464807f0ac29d967d5f0a9

Fixed a subtle bug, introduced in e35a454770b4409ac657fe2dc4917929032554c6, where the actual cards we used in selectCards didn't have backported text normalized on them.
We were stashing the fallbackTextMap on an object... that we didn't pass to extractContentWords.

Took like two hours to figure out...

Part of #399.

src/nlp.js
@@ -251,7 +251,7 @@ export const cardWithNormalizedTextProperties = (card, optFallbackTextMap) => {
 	const result = {...card};
 	if (optFallbackTextMap) result.fallbackTextMap = optFallbackTextMap;
 	//Basically it takes the output of extractContentWords and then stems each run.
-	result.normalized = Object.fromEntries(Object.entries(extractContentWords(card)).map(entry => [entry[0], entry[1].map(str => stemmedNormalizedWords(str))]));
+	result.normalized = Object.fromEntries(Object.entries(extractContentWords(result)).map(entry => [entry[0], entry[1].map(str => stemmedNormalizedWords(str))]));
 	return result;
 };
 