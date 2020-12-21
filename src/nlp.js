jkomoros/card-web

commit 1a0972973c7531921f4f5bc4613ed7e9d8705eb4

ngrams will also extract an ngram for the whole string if the text is short. This means that short text runs (e.g. for reference text, bolded text, etc) will be processed as full ngrams even if they're too long to be the max-sized ngram. Part of #399.

src/nlp.js
@@ -255,12 +255,23 @@ export const cardWithNormalizedTextProperties = (card, optFallbackTextMap) => {
 	return result;
 };
 
+//ngrams will additionally return an ngram of the full string if the number of
+//terms is this or smaller.
+const WHOLE_NGRAM_MAX_SIZE = 6;
+
 //text should be normalized
 const ngrams = (text, size = 2) => {
 	if (!text) return [];
 	const pieces = text.split(' ');
 	if (pieces.length < size) size = pieces.length;
 	const result = [];
+	if (pieces.length > size && pieces.length < WHOLE_NGRAM_MAX_SIZE) {
+		//if the entire text snippet is small enough to be totally counted, and
+		//it wouldn't be automatically geneated (since it's larger than the
+		//ngram size), include it. This means that short snippets of text, like
+		//in references, will get fully indexed as an ngram.
+		result.push(pieces.join(' '));
+	}
 	for (let i = 0; i < (pieces.length - size + 1); i++) {
 		let subPieces = [];
 		for (let j = 0; j < size; j++) {

commit 0a305ccc94ac396aae80a65300b56fcff6745fd5

Fix a bug introduced in 1a0972973c7531921f4f5bc4613ed7e9d8705eb4 and 1eb05cb9c929fbc450eb111c109d7db64d4fee06.
ngrams is supposed to return ngrams of literally only X size, but it was returning smaller ones for small text. And, it was returning special full strings too often.

Changed it so the special long ngram is done in wordCountsForSemantics and WHOLE_NGRAMs are done there too, so they don't double count. Part of #399.

src/nlp.js
@@ -266,23 +266,12 @@ export const cardWithNormalizedTextProperties = (card, optFallbackTextMap) => {
 	return result;
 };
 
-//ngrams will additionally return an ngram of the full string if the number of
-//terms is this or smaller.
-const WHOLE_NGRAM_MAX_SIZE = 6;
-
 //text should be normalized
 const ngrams = (text, size = 2) => {
 	if (!text) return [];
 	const pieces = text.split(' ');
-	if (pieces.length < size) size = pieces.length;
+	if (pieces.length < size) return [];
 	const result = [];
-	if (pieces.length > size && pieces.length < WHOLE_NGRAM_MAX_SIZE) {
-		//if the entire text snippet is small enough to be totally counted, and
-		//it wouldn't be automatically geneated (since it's larger than the
-		//ngram size), include it. This means that short snippets of text, like
-		//in references, will get fully indexed as an ngram.
-		result.push(pieces.join(' '));
-	}
 	for (let i = 0; i < (pieces.length - size + 1); i++) {
 		let subPieces = [];
 		for (let j = 0; j < size; j++) {
@@ -474,6 +463,9 @@ const semanticOverlap = (fingerprintOne, fingerprintTwo) => {
 
 //How high to go for n-grams in fingerprint. 2 = bigrams and monograms.
 const MAX_N_GRAM_FOR_FINGERPRINT = 2;
+//ngrams will additionally return an ngram of the full string if the number of
+//terms is this or smaller.
+const WHOLE_NGRAM_MAX_SIZE = 6;
 
 const wordCountsForSemantics = (strsMap) => {
 	const cardMap = {};
@@ -488,6 +480,14 @@ const wordCountsForSemantics = (strsMap) => {
 					//proprition with how many there are.
 					cardMap[ngram] = (cardMap[ngram] || 0) + 1/n;
 				}
+			}
+			const splitWords = words.split(' ');
+			if (splitWords.length > MAX_N_GRAM_FOR_FINGERPRINT && splitWords.length < WHOLE_NGRAM_MAX_SIZE) {
+				//if the entire text snippet is small enough to be totally counted, and
+				//it wouldn't be automatically geneated (since it's larger than the
+				//ngram size), include it. This means that short snippets of text, like
+				//in references, will get fully indexed as an ngram.
+				cardMap[words] = (cardMap[words] || 0) + 1/splitWords.length;
 			}
 		}
 	}