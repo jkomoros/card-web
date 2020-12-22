jkomoros/card-web

commit 65665700bb34903f76965a5a7c4a9e133654fcb2

Show overflow indicators in main content of card-renderer when there's overflow.
Requires splitting out how the css var colors are set, and reducing the spread of the editing flow.

Part of #399. Part of #400.

src/components/card-renderer.js
@@ -61,6 +61,8 @@ export class CardRenderer extends GestureEventListeners(LitElement) {
 				:host {
 					display:block;
 					background-color: var(--card-color);
+					--effective-background-color: var(--card-color);
+					--effective-background-color-rgb-inner: var(--card-color-rgb-inner);
 					
 					width: ${CARD_WIDTH_IN_EMS}em;
 					height: ${CARD_HEIGHT_IN_EMS}em;
@@ -81,10 +83,12 @@ export class CardRenderer extends GestureEventListeners(LitElement) {
 
 				.container.unpublished {
 					background-color: var(--unpublished-card-color);
+					--effective-background-color: var(--unpublished-card-color);
+					--effective-background-color-rgb-inner: var(--unpublished-card-color-rgb-inner);
 				}
 
 				.container.editing {
-					box-shadow: inset 0 0 4em 1em var(--app-primary-color-light-transparent);
+					box-shadow: inset 0 0 1em 0.5em var(--app-primary-color-light-transparent);
 				}
 
 				.container.editing card-link[card] {
@@ -177,6 +181,30 @@ export class CardRenderer extends GestureEventListeners(LitElement) {
 				.reference-blocks {
 					overflow:scroll;
 					flex-shrink: 1;
+				}
+
+				.primary, .reference-blocks {
+					/* inspired by https://stackoverflow.com/questions/9333379/check-if-an-elements-content-is-overflowing */
+					overflow: auto;
+					background:
+						/* Shadow covers */
+						linear-gradient(var(--effective-background-color) 30%, rgba(var(--effective-background-color-rgb-inner),0)),
+						linear-gradient(rgba(var(--effective-background-color-rgb-inner),0), var(--effective-background-color) 70%) 0 100%,
+						
+						/* Shadows */
+						radial-gradient(50% 0, farthest-side, rgba(var(--card-overflow-shadow-rgb-inner),.2), rgba(var(--card-overflow-shadow-rgb-inner),0)),
+						radial-gradient(50% 100%,farthest-side, rgba(var(--card-overflow-shadow-rgb-inner),.2), rgba(var(--card-overflow-shadow-rgb-inner),0)) 0 100%;
+					background:
+						/* Shadow covers */
+						linear-gradient(var(--effective-background-color) 30%, rgba(var(--effective-background-color-rgb-inner),0)),
+						linear-gradient(rgba(var(--effective-background-color-rgb-inner),0), var(--effective-background-color) 70%) 0 100%,
+						
+						/* Shadows */
+						radial-gradient(farthest-side at 50% 0, rgba(var(--card-overflow-shadow-rgb-inner),.2), rgba(var(--card-overflow-shadow-rgb-inner),0)),
+						radial-gradient(farthest-side at 50% 100%, rgba(var(--card-overflow-shadow-rgb-inner),.2), rgba(var(--card-overflow-shadow-rgb-inner),0)) 0 100%;
+					background-repeat: no-repeat;
+					background-size: 100% 2.5em, 100% 2.5em, 100% 1.0em, 100% 1.0em;
+					background-attachment: local, local, scroll, scroll;
 				}
 
 				/* Google docs pasted output includes <p> inside of li a lot. This

src/components/card-web-app.js
@@ -69,9 +69,17 @@ class CardWebApp extends connect(store)(LitElement) {
 				--app-header-font-family: 'Raleway';
 				--app-default-font-family: 'Source Sans Pro';
 
-				--card-color: #FCFCFC;
+				/* these are where you change the color for card.
+				card-renderer's overflow scrolling expects these to be set */
+				--card-color-rgb-inner: 252, 252, 252;
+				--unpublished-card-color-rgb-inner: 238, 238, 238;
+				--card-overflow-shadow-rgb-inner: 0, 0, 0;
+
+				/* change the *-rgb-inner instead of these directly */
+				--card-color: rgb(var(--card-color-rgb-inner));
+				--unpublished-card-color: rgb(var(--unpublished-card-color-rgb-inner));
+
 				--shadow-color: #CCC;
-				--unpublished-card-color: #EEE;
 				--card-shadow-first-part: 0 2px 6px;
 				--card-shadow: var(--card-shadow-first-part) var(--shadow-color);
 

commit 19214ea175c8c75aaa87d90c04bea19efe4bb4d5

Fix a bug introduced in 65665700bb34903f76965a5a7c4a9e133654fcb2 where little bits of the overflow shadow could poke through even when there was nothing to overflow. Part of #399.

src/components/card-renderer.js
@@ -203,7 +203,7 @@ export class CardRenderer extends GestureEventListeners(LitElement) {
 						radial-gradient(farthest-side at 50% 0, rgba(var(--card-overflow-shadow-rgb-inner),.2), rgba(var(--card-overflow-shadow-rgb-inner),0)),
 						radial-gradient(farthest-side at 50% 100%, rgba(var(--card-overflow-shadow-rgb-inner),.2), rgba(var(--card-overflow-shadow-rgb-inner),0)) 0 100%;
 					background-repeat: no-repeat;
-					background-size: 100% 2.5em, 100% 2.5em, 100% 1.0em, 100% 1.0em;
+					background-size: 100% 2.5em, 100% 2.5em, 100% 0.8em, 100% 0.8em;
 					background-attachment: local, local, scroll, scroll;
 				}
 