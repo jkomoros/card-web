jkomoros/card-web

commit 0e27d0466afc582f3d84d96becaf5888e544d4a6

Make it so all title alternates for a concept are returned in concept map. conceptMap is now basically every string that woudl return a concpet card.
This makes it so suggestConceptReferences will now suggest the right concept if the card only refers to one of a concept's alternates. Part of #401.

src/nlp.js
@@ -53,8 +53,12 @@ export const getAllConceptStringsFromConceptCard = (rawConceptCard) => {
 	return [getConceptStringFromConceptCard(rawConceptCard), ...extractSynonymsFromCardTitleAlternates(rawConceptCard)];
 };
 
+//Returns all concept strings that, if provided to getConceptCardForConcept,
+//would return a card. Note that this is not a one-to-one erlationship to
+//concept cards, it might include multiple entries for each concept card if it
+//has title alternates.
 export const getConceptsFromConceptCards = (conceptCards) => {
-	return Object.fromEntries(Object.values(conceptCards).map(card => [getConceptStringFromConceptCard(card), true]));
+	return Object.fromEntries(Object.values(conceptCards).map(card => getAllConceptStringsFromConceptCard(card)).flat().map(item => [item, true]));
 };
 
 const cardMatchesConcept = (card, conceptStr) => {

commit ebfb558c7d511be328cf746abddbf43d47fb2510

Fixed a bug broken in 0e27d0466afc582f3d84d96becaf5888e544d4a6 where cards that included the real name of a concept and its synonym would both be suggested.
By the way, tag-list handles duplicates in a really not great way where it shows multiple copies, and x'd out, and all kinds of weirdness.

Part of #401.

src/nlp.js
@@ -1113,7 +1113,7 @@ export const possibleMissingConcepts = (cards) => {
 };
 
 export const suggestedConceptReferencesForCard = (card, fingerprint, allCardsOrConceptCards, concepts) => {
-	const candidates = [];
+	const candidates = {};
 	if (!card || !fingerprint) return [];
 	if (!BODY_CARD_TYPES[card.card_type]) return [];
 	const itemsFromConceptReferences = fingerprint.itemsFromConceptReferences();
@@ -1146,6 +1146,9 @@ export const suggestedConceptReferencesForCard = (card, fingerprint, allCardsOrC
 		if (!conceptCard) continue;
 		//Don't suggest that concept cards reference themselves
 		if (conceptCard.id == card.id) continue;
+		//Skip ones that we've already included (which could have happened if we
+		//already saw a synonym item)
+		if (candidates[conceptCard.id]) continue;
 		//Don't suggest things that already have references of type concept (and sub-types like syonym), or the generic ack.
 		for (const referenceType of REFERENCE_TYPES_THAT_SUPPRESS_SUGGESTED_CONCEPT) {
 			const referencesOfType = existingReferences[referenceType];
@@ -1157,7 +1160,7 @@ export const suggestedConceptReferencesForCard = (card, fingerprint, allCardsOrC
 		}
 		if (skipSuggestion) continue;
 		conceptStrForCandidateCard[conceptCard.id] = fingerprintItem;
-		candidates.push(conceptCard.id);
+		candidates[conceptCard.id] = true;
 	}
 	//Now we want to make sure we don't suggest any concepts that are subsets of
 	//larger concepts we might also suggest, since if the user were to select
@@ -1166,7 +1169,7 @@ export const suggestedConceptReferencesForCard = (card, fingerprint, allCardsOrC
 	//consider largest concepts down to smallest to guarantee that we pick the
 	//larger ones first. We'll sort a copy because we still want to keep the
 	//original order so we suggest higher fingerprint items first
-	let sortedCandidates = [...candidates];
+	let sortedCandidates = [...Object.keys(candidates)];
 	sortedCandidates.sort((a, b) => conceptStrForCandidateCard[b].length - conceptStrForCandidateCard[a].length);
 
 	let cardsToIncludeInResult = {};
@@ -1182,7 +1185,7 @@ export const suggestedConceptReferencesForCard = (card, fingerprint, allCardsOrC
 		cardsToIncludeInResult[candidate] = true;
 	}
 
-	return candidates.filter(id => cardsToIncludeInResult[id]);
+	return [...Object.keys(candidates)].filter(id => cardsToIncludeInResult[id]);
 };
 
 export const emptyWordCloud = () => {