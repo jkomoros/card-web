jkomoros/card-web

commit 69c9b46a6dbe35c799b75c08947b90d3726ade68

Switched the data model for the images block to just be an array of image objects. Part of #4.

src/images.js
@@ -1,51 +1,40 @@
 //card.images is an imagesBlock. It has the following shape:
 /*
-{
-	//May be any valid field name
-	body: [
-		{
-			//Must always be set to a fully resolved url
-			src: 'https://www.example.com/image.png',
-			//Natural height and width in pixels
-			height: 10,
-			width: 100,
-			//If the file is an uload, the path in the upload bucket. This is usef
-			uploadPath: 'path/to/upload/image.png',
-		}
-		//Other images may follow
-	]
-}
+[
+	{
+		//Must always be set to a fully resolved url
+		src: 'https://www.example.com/image.png',
+		//Natural height and width in pixels
+		height: 10,
+		width: 100,
+		//If the file is an uload, the path in the upload bucket. This is usef
+		uploadPath: 'path/to/upload/image.png',
+	}
+	//Other images may follow
+]
 
 */
 
 //Returns a new images block with the given image added
 export const addImageWithURL = (imagesBlock, fieldName, src, uploadPath = '') => {
-	if (!imagesBlock) imagesBlock = {};
-	const result = {...imagesBlock};
-	const existingFieldNameBlock = result[fieldName] || [];
+	if (!imagesBlock) imagesBlock = [];
 	const imgItem = {
 		src,
 		uploadPath
 	};
-	result[fieldName] = [...existingFieldNameBlock, imgItem];
-	return result;
+	return [...imagesBlock, imgItem];
 };
 
 export const imageBlocksEquivalent = (one, two) => {
 	if (one == two) return true;
 	if (!one || !two) return false;
-	if (Object.keys(one).length != Object.keys(two).length) return false;
-	for (const key of Object.keys(one)) {
-		const oneImgs = one[key];
-		const twoImgs = two[key];
-		if (oneImgs.length != twoImgs.length) return false;
-		for (let i = 0; i < oneImgs.length; i++) {
-			const oneImg = oneImgs[i];
-			const twoImg = twoImgs[i];
-			if (Object.keys(oneImg).length != Object.keys(twoImg).length) return false;
-			for (const imgKey of Object.keys(oneImg)) {
-				if (oneImg[imgKey] != twoImg[imgKey]) return false;
-			}
+	if (one.length != two.length) return false;
+	for (let i = 0; i < one.length; i++) {
+		const oneImg = one[i];
+		const twoImg = two[i];
+		if (Object.keys(oneImg).length != Object.keys(twoImg).length) return false;
+		for (const imgKey of Object.keys(oneImg)) {
+			if (oneImg[imgKey] != twoImg[imgKey]) return false;
 		}
 	}
 	return true;

src/actions/editor.js
@@ -575,10 +575,9 @@ export const manualCollaboratorAdded = (collaboratorUid) => {
 
 //src must be a fully qualified URL. uploadPath is the filename in the upload
 //bucket, if applicable.
-export const addImageWithURL = (fieldName, src, uploadPath = '') => {
+export const addImageWithURL = (src, uploadPath = '') => {
 	return {
 		type: EDITING_ADD_IMAGE_URL,
-		fieldName,
 		src,
 		uploadPath,
 	};

src/actions/maintenance.js
@@ -408,7 +408,7 @@ const addImagesProperty = async () => {
 	let snapshot = await db.collection(CARDS_COLLECTION).get();
 	snapshot.forEach(doc => {
 		const update = {
-			images: {},
+			images: [],
 		};
 		batch.update(doc.ref, update);
 	});

src/reducers/editor.js
@@ -295,7 +295,7 @@ const app = (state = INITIAL_STATE, action) => {
 	case EDITING_ADD_IMAGE_URL:
 		return {
 			...state,
-			card: {...state.card, images: addImageWithURL(state.card.images, action.fieldName, action.src, action.uploadPath)}
+			card: {...state.card, images: addImageWithURL(state.card.images, action.src, action.uploadPath)}
 		};
 	default:
 		return state;

commit 935dd654f05a90f300fdf03cc1bda7a03e501c6c

Fixed a bug introduced in 69c9b46a6dbe35c799b75c08947b90d3726ade68 where addImageWithURL didn't work. Part of #4

src/images.js
@@ -16,7 +16,7 @@
 */
 
 //Returns a new images block with the given image added
-export const addImageWithURL = (imagesBlock, fieldName, src, uploadPath = '') => {
+export const addImageWithURL = (imagesBlock, src, uploadPath = '') => {
 	if (!imagesBlock) imagesBlock = [];
 	const imgItem = {
 		src,