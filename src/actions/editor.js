jkomoros/card-web

commit 37e1a95d333724632ec824cb1e79072c14549525

Fix it so even with highlighted concepts in contenteditable, you can still add links. This was broken in 472edd7
This commit is pretty messy. When we stash a selection and restore it, we keep note on the element itself, and then in card-renderer we use that to figure out when we should not request an update when focus/blur happens.

Reasoning for the bug: The reason for the link bug: after the link dialog is closed, the contenteditable is refocused, which requests an update in the next micro tick. But before that, the content editable paste happens, which triggers the input, which updates the data model with the body having the link. But then the requestedUpdate runs, BEFORE the state has been changed and distributed to the card-renderer. It renders with the old state, which says that the value didn't come from the content editable, and blows away the updated state... but only temporarily; when another render happens it shows up correctly.

Part of #449.

src/actions/editor.js
@@ -103,10 +103,7 @@ import {
 } from '../firebase.js';
 
 let lastReportedSelectionRange = null;
-//TODO: figure out a pattenr that doesn't have a single shared global
 let savedSelectionRange = null;
-let selectionRangeStartOffset = -1;
-let selectionRangeEndOffset = -1;
 let selectionParent = null;
 
 
@@ -130,10 +127,10 @@ export const saveSelectionRange = () => {
 		parentEle = parentEle.parentNode;
 	}
 	if (!parentEle) return;
-	let [start, end] = startEndOffsetInEle(parentEle,savedSelectionRange, 0);
-	selectionRangeStartOffset = start;
-	selectionRangeEndOffset = end;
+	//a tuple of [startOffset, endOffset]
+	const offsets = startEndOffsetInEle(parentEle,savedSelectionRange, 0);
 	selectionParent = parentEle;
+	selectionParent.stashedSelectionOffset = offsets;
 };
 
 const startEndOffsetInEle = (ele, range, previousCount) => {
@@ -156,7 +153,7 @@ const startEndOffsetInEle = (ele, range, previousCount) => {
 	return [start, end];
 };
 
-const rangeFromOffsetsInEle = (ele, startOffset, endOffset) => {
+const rangeFromOffsetsInEle = (ele, startOffset = -1, endOffset = -1) => {
 	//startOffset and endOffset are within the given ele.
 	if (startOffset == -1 || endOffset == -1) return null;
 	const range = document.createRange();
@@ -177,12 +174,14 @@ const setOffsetRange = (node, range, startOffset, endOffset, offsetCount) => {
 };
 
 export const restoreSelectionRange = () => {
+	if (!selectionParent) return;
 	let selection = document.getSelection();
 	selection.removeAllRanges();
 	//Note that this assumes that selectionParent is still literally the same element as when selection was saved.
-	let range = rangeFromOffsetsInEle(selectionParent, selectionRangeStartOffset, selectionRangeEndOffset);
-	if (!range) range = savedSelectionRange;
+	const offsets = selectionParent.stashedSelectionOffset || [-1, -1];
+	let range = rangeFromOffsetsInEle(selectionParent, offsets[0], offsets[1]);
 	if (range) selection.addRange(range);
+	selectionParent.stashedSelectionOffset = undefined;
 };
 
 export const savedSelectionRangeIsLink = () => {

src/components/card-renderer.js
@@ -412,7 +412,8 @@ export class CardRenderer extends GestureEventListeners(LitElement) {
 		//we don't want to mess with its focus, so return the same thing. If
 		//it's not focused, then we can update it however we want.
 		const isActiveElement = deepActiveElement() == this._elements[field];
-		if (updatedFromContentEditable && this._elements[field] && isActiveElement) {
+		const doHighlightConcepts = !this.editing || !isActiveElement;
+		if (updatedFromContentEditable && this._elements[field] && isActiveElement && this._elements[field].conceptReferencesHighlighted == doHighlightConcepts) {
 			return this._elements[field];
 		}
 
@@ -423,7 +424,7 @@ export class CardRenderer extends GestureEventListeners(LitElement) {
 			//is blurred, show which items are concepts. We strip out any
 			//card-highlights that come from contenteditable, so even if they
 			//sneak in we won't save them.
-			if (!this.editing || !isActiveElement) {
+			if (doHighlightConcepts) {
 				value = highlightConceptReferences(this._card, field);
 			}
 			htmlToSet = value;
@@ -457,6 +458,8 @@ export class CardRenderer extends GestureEventListeners(LitElement) {
 			}
 		}
 
+		ele.conceptReferencesHighlighted = doHighlightConcepts;
+
 		if (this.editing && !config.noContentEditable) {
 			makeElementContentEditable(ele);
 			//Only install the content editable listeners once. Otherwise, you
@@ -469,9 +472,15 @@ export class CardRenderer extends GestureEventListeners(LitElement) {
 				//that there's a change we react to that's not in the
 				//state/properties of the object, but the focused node is state
 				//managed by the browser so :shrug:.
-				ele.addEventListener('blur', () => this.requestUpdate());
+				ele.addEventListener('blur', () => {
+					//If there's a stashed selection offset, then we're being defocused to go to a find dialog.
+					if (!ele.stashedSelectionOffset) this.requestUpdate();
+				});
 				//Highlights disappear on focus
-				ele.addEventListener('focus', () => this.requestUpdate());
+				ele.addEventListener('focus', () => {
+					//If there's a stashed selection offset, then when we're refocused, we'll have a paste happen.
+					if (!ele.stashedSelectionOffset) this.requestUpdate();
+				});
 				ele.hasContentEditableListeners = true;
 			}
 			if (config.html) htmlToSet = normalizeBodyToContentEditable(htmlToSet);

commit 1ba5468ce0005aeda321b1c7eb18786d98273a6a

Fix a bug introduced in 37e1a95d333724632ec824cb1e79072c14549525 where if you selected text to link but then cancelled it, the concept highlights would get wedged and never update again. Part of #449.

src/actions/editor.js
@@ -270,6 +270,10 @@ export const editingCommit = () => async (dispatch, getState) => {
 
 };
 
+export const cancelLink = () => () => {
+	restoreSelectionRange();
+};
+
 export const linkURL = (href) => (dispatch, getState) => {
 	const state = getState();
 	if (!state.editor.editing) return;

src/components/dialog-element.js
@@ -84,7 +84,7 @@ export class DialogElement extends LitElement {
 			</style>
 			<div class='background ${this.mobileMode ? 'mobile': ''}' @click=${this._handleBackgroundClicked}>
 				<div class='content'>
-					<button class='small' id='close' @click=${this._shouldClose}>${CANCEL_ICON}</button>
+					<button class='small' id='close' @click=${this.cancel}>${CANCEL_ICON}</button>
 					<h2>${this.title || ''}</h2>
 					<div id='inner'>
 					${this.innerRender()}
@@ -106,7 +106,7 @@ export class DialogElement extends LitElement {
 	_handleKeyDown(e) {
 		if (!this.open) return;
 		if (e.key == 'Escape') {
-			this._shouldClose();
+			this.cancel();
 			return true;
 		}
 	}
@@ -118,6 +118,11 @@ export class DialogElement extends LitElement {
 		this._shouldClose();
 	}
 
+	cancel() {
+		this._shouldClose(true);
+	}
+
+	//Will be called with a single argument of true if cancelled
 	_shouldClose() {
 		//Override point for sub classes
 		this.dispatchEvent(new CustomEvent('dialog-should-close'));

src/components/find-dialog.js
@@ -25,6 +25,7 @@ import {
 import {
 	linkCard,
 	linkURL,
+	cancelLink,
 	savedSelectionRangeIsLink,
 	setCardToReference,
 } from '../actions/editor.js';
@@ -134,7 +135,10 @@ class FindDialog extends connect(store)(DialogElement) {
 		this._legalCardTypeFilters = [];
 	}
 
-	_shouldClose() {
+	_shouldClose(cancelled) {
+		if (cancelled && this._linking) {
+			store.dispatch(cancelLink());
+		}
 		//Override base class.
 		store.dispatch(closeFindDialog());
 	}