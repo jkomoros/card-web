jkomoros/card-web

commit 472edd7da0946afc7d6d030c26a74622b21f416d

Highlight concepts even during editing when the field is blurred. Part of #449.

src/components/card-renderer.js
@@ -412,19 +412,22 @@ export class CardRenderer extends GestureEventListeners(LitElement) {
 		//already ahve an element, AND it's the currently selected element, then
 		//we don't want to mess with its focus, so return the same thing. If
 		//it's not focused, then we can update it however we want.
-		if (updatedFromContentEditable && this._elements[field] && deepActiveElement() == this._elements[field]) {
+		const isActiveElement = deepActiveElement() == this._elements[field];
+		if (updatedFromContentEditable && this._elements[field] && isActiveElement) {
 			return this._elements[field];
 		}
 
 		let value = this._card[field] || '';
 		let htmlToSet = '';
 		if (config.html) {
-			if (!this.editing) {
-				//Make sure not to do the highlighting if it's being edited, which
-				//would pollute the HTML to save with the highlights.
+			//If not editing, then highlight. But even if editing, if the field
+			//is blurred, show which items are concepts. We strip out any
+			//card-highlights that come from contenteditable, so even if they
+			//sneak in we won't save them.
+			if (!this.editing || !isActiveElement) {
 				value = highlightConceptReferences(this._card, field);
 			}
-			htmlToSet = normalizeBodyHTML(value);
+			htmlToSet = value;
 		}
 		if (value && config.htmlFormatter) htmlToSet = config.htmlFormatter(value);
 		if (value && config.displayPrefix) htmlToSet = '<span>' + config.displayPrefix + '</span> ' + value;
@@ -468,6 +471,8 @@ export class CardRenderer extends GestureEventListeners(LitElement) {
 				//state/properties of the object, but the focused node is state
 				//managed by the browser so :shrug:.
 				ele.addEventListener('blur', () => this.requestUpdate());
+				//Highlights disappear on focus
+				ele.addEventListener('focus', () => this.requestUpdate());
 				ele.hasContentEditableListeners = true;
 			}
 			if (config.html) htmlToSet = normalizeBodyToContentEditable(htmlToSet);

src/contenteditable.js
@@ -71,6 +71,13 @@ const cardAttributeToHref = (a) => {
 	a.removeAttribute('card');
 
 };
+
+const spliceEle = (ele) => {
+	const parent = ele.parentNode;
+	ele.replaceWith(...ele.childNodes);
+	parent.normalize();
+};
+
 const normalizeBodyFromContentEditable = (html) => {
 
 	//Rewrite elements from content editable form to canonical form (which is
@@ -90,6 +97,8 @@ const normalizeBodyFromContentEditable = (html) => {
 	//createLink will have <a href='cardid'>. We will have changed the <a> to
 	//<card-link> already, but the href should be a card attribute.
 	section.querySelectorAll('card-link').forEach(hrefToCardAttribute);
+	//If card-highlight somehow got back in, remove it.
+	section.querySelectorAll('card-highlight').forEach(spliceEle);
 
 	return section.innerHTML;
 };
@@ -112,6 +121,9 @@ export const normalizeBodyToContentEditable = (html) => {
 	section.innerHTML = html;
 
 	section.querySelectorAll('a').forEach(cardAttributeToHref);
+	//Make sure that highlights are inactive so if you click on a card highlight
+	//to focus the field it won't navigate.
+	section.querySelectorAll('card-highlight').forEach(ele => ele.disabled = true);
 
 	return section.innerHTML;
 

test/contenteditable/test.js
@@ -192,6 +192,13 @@ describe('content editable scrubbing', () => {
 		assert.equal(actual, expected);
 	});
 
+	it('content with a card-highlight or dir attribute is removed', async () => {
+		const input = '<p>Here\'s some <card-highlight>content</card-highlight></p>';
+		const actual = normalizeBodyHTML(input);
+		const expected = '<p>Here\'s some content</p>\n';
+		assert.equal(actual, expected);
+	});
+
 });
 
 describe('html highlighting', () => {

commit 37e1a95d333724632ec824cb1e79072c14549525

Fix it so even with highlighted concepts in contenteditable, you can still add links. This was broken in 472edd7
This commit is pretty messy. When we stash a selection and restore it, we keep note on the element itself, and then in card-renderer we use that to figure out when we should not request an update when focus/blur happens.

Reasoning for the bug: The reason for the link bug: after the link dialog is closed, the contenteditable is refocused, which requests an update in the next micro tick. But before that, the content editable paste happens, which triggers the input, which updates the data model with the body having the link. But then the requestedUpdate runs, BEFORE the state has been changed and distributed to the card-renderer. It renders with the old state, which says that the value didn't come from the content editable, and blows away the updated state... but only temporarily; when another render happens it shows up correctly.

Part of #449.

src/actions/editor.js
@@ -103,10 +103,7 @@ import {
 } from '../firebase.js';
 
 let lastReportedSelectionRange = null;
-//TODO: figure out a pattenr that doesn't have a single shared global
 let savedSelectionRange = null;
-let selectionRangeStartOffset = -1;
-let selectionRangeEndOffset = -1;
 let selectionParent = null;
 
 
@@ -130,10 +127,10 @@ export const saveSelectionRange = () => {
 		parentEle = parentEle.parentNode;
 	}
 	if (!parentEle) return;
-	let [start, end] = startEndOffsetInEle(parentEle,savedSelectionRange, 0);
-	selectionRangeStartOffset = start;
-	selectionRangeEndOffset = end;
+	//a tuple of [startOffset, endOffset]
+	const offsets = startEndOffsetInEle(parentEle,savedSelectionRange, 0);
 	selectionParent = parentEle;
+	selectionParent.stashedSelectionOffset = offsets;
 };
 
 const startEndOffsetInEle = (ele, range, previousCount) => {
@@ -156,7 +153,7 @@ const startEndOffsetInEle = (ele, range, previousCount) => {
 	return [start, end];
 };
 
-const rangeFromOffsetsInEle = (ele, startOffset, endOffset) => {
+const rangeFromOffsetsInEle = (ele, startOffset = -1, endOffset = -1) => {
 	//startOffset and endOffset are within the given ele.
 	if (startOffset == -1 || endOffset == -1) return null;
 	const range = document.createRange();
@@ -177,12 +174,14 @@ const setOffsetRange = (node, range, startOffset, endOffset, offsetCount) => {
 };
 
 export const restoreSelectionRange = () => {
+	if (!selectionParent) return;
 	let selection = document.getSelection();
 	selection.removeAllRanges();
 	//Note that this assumes that selectionParent is still literally the same element as when selection was saved.
-	let range = rangeFromOffsetsInEle(selectionParent, selectionRangeStartOffset, selectionRangeEndOffset);
-	if (!range) range = savedSelectionRange;
+	const offsets = selectionParent.stashedSelectionOffset || [-1, -1];
+	let range = rangeFromOffsetsInEle(selectionParent, offsets[0], offsets[1]);
 	if (range) selection.addRange(range);
+	selectionParent.stashedSelectionOffset = undefined;
 };
 
 export const savedSelectionRangeIsLink = () => {

src/components/card-renderer.js
@@ -412,7 +412,8 @@ export class CardRenderer extends GestureEventListeners(LitElement) {
 		//we don't want to mess with its focus, so return the same thing. If
 		//it's not focused, then we can update it however we want.
 		const isActiveElement = deepActiveElement() == this._elements[field];
-		if (updatedFromContentEditable && this._elements[field] && isActiveElement) {
+		const doHighlightConcepts = !this.editing || !isActiveElement;
+		if (updatedFromContentEditable && this._elements[field] && isActiveElement && this._elements[field].conceptReferencesHighlighted == doHighlightConcepts) {
 			return this._elements[field];
 		}
 
@@ -423,7 +424,7 @@ export class CardRenderer extends GestureEventListeners(LitElement) {
 			//is blurred, show which items are concepts. We strip out any
 			//card-highlights that come from contenteditable, so even if they
 			//sneak in we won't save them.
-			if (!this.editing || !isActiveElement) {
+			if (doHighlightConcepts) {
 				value = highlightConceptReferences(this._card, field);
 			}
 			htmlToSet = value;
@@ -457,6 +458,8 @@ export class CardRenderer extends GestureEventListeners(LitElement) {
 			}
 		}
 
+		ele.conceptReferencesHighlighted = doHighlightConcepts;
+
 		if (this.editing && !config.noContentEditable) {
 			makeElementContentEditable(ele);
 			//Only install the content editable listeners once. Otherwise, you
@@ -469,9 +472,15 @@ export class CardRenderer extends GestureEventListeners(LitElement) {
 				//that there's a change we react to that's not in the
 				//state/properties of the object, but the focused node is state
 				//managed by the browser so :shrug:.
-				ele.addEventListener('blur', () => this.requestUpdate());
+				ele.addEventListener('blur', () => {
+					//If there's a stashed selection offset, then we're being defocused to go to a find dialog.
+					if (!ele.stashedSelectionOffset) this.requestUpdate();
+				});
 				//Highlights disappear on focus
-				ele.addEventListener('focus', () => this.requestUpdate());
+				ele.addEventListener('focus', () => {
+					//If there's a stashed selection offset, then when we're refocused, we'll have a paste happen.
+					if (!ele.stashedSelectionOffset) this.requestUpdate();
+				});
 				ele.hasContentEditableListeners = true;
 			}
 			if (config.html) htmlToSet = normalizeBodyToContentEditable(htmlToSet);