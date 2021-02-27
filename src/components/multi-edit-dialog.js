4345666246ebf327bb92f087daea232e6c034679
Alex Komoroske <jkomoros@gmail.com>
2021-02-26 16:54:53 -0800

    multi-edit-dialog prints out the union of all card references. They don't do anything yet. Part of #174.

--- src/components/multi-edit-dialog.js
+++ src/components/multi-edit-dialog.js
@@ -17,14 +17,38 @@ import {
 
 import {
 	selectMultiEditDialogOpen,
+	selectSelectedCardsReferencesUnion,
+	selectTagInfosForCards
 } from '../selectors.js';
+
+import {
+	REFERENCE_TYPES,
+} from '../card_fields.js';
+
+import {
+	references,
+} from '../references.js';
+
+import {
+	help,
+	HelpStyles,
+} from './help-badges.js';
 
 class MultiEditDialog extends connect(store)(DialogElement) {
 	innerRender() {
 
+		const referencesMap = references(this._unionReferencesCard).byTypeArray();
+
 		return html`
-		<div><em>TODO: implement</em></div>
-	`;
+		${HelpStyles}
+		<div>
+		${Object.entries(REFERENCE_TYPES).filter(entry => referencesMap[entry[0]]).map(entry => {
+		return html`<div>
+							<label>${entry[1].name} ${help(entry[1].description, false)}</label>
+							<tag-list .overrideTypeName=${'Reference'} .referenceType=${entry[0]} .tagInfos=${this._cardTagInfos} .defaultColor=${entry[1].color} .tags=${referencesMap[entry[0]]} .editing=${entry[1].editable} .subtle=${!entry[1].editable} .tapEvents=${true} .disableAdd=${true} @remove-tag=${this._handleRemoveReference}></tag-list>
+						</div>`;
+	})}
+	</div>`;
 	}
 
 	constructor() {
@@ -37,9 +61,22 @@ class MultiEditDialog extends connect(store)(DialogElement) {
 		store.dispatch(closeMultiEditDialog());
 	}
 
+	_handleRemoveReference() {
+		alert('Not yet implemented');
+	}
+
+	static get properties() {
+		return {
+			_unionReferencesCard: {type: Object},
+			_cardTagInfos: {type: Object},
+		};
+	}
+
 	stateChanged(state) {
 		//tODO: it's weird that we manually set our superclasses' public property
 		this.open = selectMultiEditDialogOpen(state);
+		this._unionReferencesCard = selectSelectedCardsReferencesUnion(state);
+		this._cardTagInfos = selectTagInfosForCards(state);
 	}
 
 }
