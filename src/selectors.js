jkomoros/card-web

commit a6fc96c6a4a47aadbf42bf4d4d7a566ffbb5aaea

Make it so you can also set 'default' on a tag to have it be the default selected one, not just sections. Part of #472.

src/selectors.js
@@ -942,6 +942,12 @@ export const selectCardsLoaded = createSelector(
 	(permissionsFinal, publishedCardsLoaded, userMayViewUnpublished, unpublishedCardsLoaded) => permissionsFinal && publishedCardsLoaded && (userMayViewUnpublished ? unpublishedCardsLoaded : true)
 );
 
+export const selectSectionsAndTagsLoaded = createSelector(
+	selectSections,
+	selectTags,
+	(sectionsLoaded, tagsLoaded) => sectionsLoaded && tagsLoaded
+);
+
 //DataIsFullyLoaded returns true if we've loaded all of the card/section
 //information we're going to load.
 export const selectDataIsFullyLoaded = createSelector(
@@ -1044,16 +1050,14 @@ export const selectExpandedTabConfig = createSelector(
 //The CollectionDescription to load up if not provided one
 export const selectDefaultCollectionDescription = createSelector(
 	selectExpandedTabConfig,
-	selectSectionsLoaded,
-	(tabConfig, sectionsLoaded) => {
+	selectSectionsAndTagsLoaded,
+	(tabConfig, sectionsAndTagsLoaded) => {
 		for (const tab of tabConfig) {
 			if (tab.default) return tab.collection;
 		}
 		//If everything is laoded and we still don't have one, just navigate to
 		//the first tab item with a set collection description
-		if (sectionsLoaded) {
-			//TODO: this really should wait for data to be fully loaded, but
-			//card-view only calls us again if section status loads.
+		if (sectionsAndTagsLoaded) {
 			for (const tab of tabConfig) {
 				if (tab.collection) return tab.collection;
 			}

src/actions/app.js
@@ -192,12 +192,12 @@ export const navigateToComment = (commentId) => (dispatch, getState) => {
 		return;
 	}
 	alert('That comment does not exist. Redirecting to the default card.');
-	dispatch(navigateToDefaultIfSectionsLoaded(false));
+	dispatch(navigateToDefaultIfSectionsAndTagsLoaded(false));
 };
 
 //navigateToDefaultIfSectionsLoaded will navigate to default if sections are
 //loaded. If they aren't, it won't do anything.
-export const navigateToDefaultIfSectionsLoaded = (silent) => (dispatch, getState) => {
+export const navigateToDefaultIfSectionsAndTagsLoaded = (silent) => (dispatch, getState) => {
 	const defaultCollectionDescription = selectDefaultCollectionDescription(getState());
 	if (!defaultCollectionDescription) {
 		//must not have loaded yet
@@ -229,7 +229,7 @@ export const navigateToCardInCurrentCollection = (cardOrID, silent) => (dispatch
 export const navigateToCardInDefaultCollection = (cardOrId, silent) => (dispatch) => {
 	let path = urlForCard(cardOrId);
 	if (!path) {
-		dispatch(navigateToDefaultIfSectionsLoaded(silent));
+		dispatch(navigateToDefaultIfSectionsAndTagsLoaded(silent));
 	}
 	dispatch(navigatePathTo(path, silent));
 };

src/components/card-view.js
@@ -36,7 +36,7 @@ import {
 	selectCommentsAndInfoPanelOpen,
 	selectUserMayEditActiveCard,
 	selectUserMayCreateCard,
-	selectSectionsLoaded,
+	selectSectionsAndTagsLoaded,
 	selectEditingUpdatedFromContentEditable,
 	selectPendingNewCardIDToNavigateTo,
 	selectUserMayForkActiveCard,
@@ -100,7 +100,7 @@ import {
 
 import {
 	navigateToCardInCurrentCollection,
-	navigateToDefaultIfSectionsLoaded,
+	navigateToDefaultIfSectionsAndTagsLoaded,
 	openCardsDrawerPanel,
 	closeCardsDrawerPanel,
 	enablePresentationMode,
@@ -325,7 +325,7 @@ class CardView extends connect(store)(PageViewElement) {
 			_drawerReorderPending : {type: Boolean},
 			_activeSectionId: {type: String},
 			_dataIsFullyLoaded: {type:Boolean},
-			_sectionsLoaded: {type:Boolean},
+			_sectionsAndTagsLoaded: {type:Boolean},
 			_tagInfos: {type:Object},
 			_cardTodos: {type: Array},
 			_pendingNewCardIDToNavigateTo: {type:String},
@@ -508,7 +508,7 @@ class CardView extends connect(store)(PageViewElement) {
 		this._drawerReorderPending = state.data.reorderPending;
 		this._activeSectionId = selectActiveSectionId(state);
 		this._dataIsFullyLoaded = selectDataIsFullyLoaded(state);
-		this._sectionsLoaded = selectSectionsLoaded(state);
+		this._sectionsAndTagsLoaded = selectSectionsAndTagsLoaded(state);
 		this._cardTodos = selectActiveCardTodosForCurrentUser(state);
 		this._pendingNewCardIDToNavigateTo = selectPendingNewCardIDToNavigateTo(state);
 		this._infoExpanded = selectCardsDrawerInfoExpanded(state);
@@ -589,17 +589,17 @@ class CardView extends connect(store)(PageViewElement) {
 		if (changedProps.has('_pageExtra')) {
 			if (this._pageExtra) {
 				store.dispatch(updateCardSelector(this._pageExtra));
-			} else if(this._sectionsLoaded) {
+			} else if(this._sectionsAndTagsLoaded) {
 				//Dispatching to '' will use default. This will fail if sections
 				//aren't yet loaded; we'll try again when sections loaded.
-				store.dispatch(navigateToDefaultIfSectionsLoaded());
+				store.dispatch(navigateToDefaultIfSectionsAndTagsLoaded());
 			}
 		}
-		if (changedProps.has('_sectionsLoaded') && this._sectionsLoaded) {
+		if (changedProps.has('_sectionsAndTagsLoaded') && this._sectionsAndTagsLoaded) {
 			if (!this._pageExtra) {
 				//Dispatching to '' will use default. We will have also tried if
 				//_pageExtra loaded when sections were already loaded
-				store.dispatch(navigateToDefaultIfSectionsLoaded());
+				store.dispatch(navigateToDefaultIfSectionsAndTagsLoaded());
 			}
 		}
 		if ((changedProps.has('_pendingNewCardIDToNavigateTo') || changedProps.has('_dataIsFullyLoaded')) && this._dataIsFullyLoaded && this._pendingNewCardIDToNavigateTo) {

commit c99817b97ed880034ea9a214a750e9588a428f6a

Fixed a bug introduced in a6fc96c6a4a47aadbf42bf4d4d7a566ffbb5aaea that would cause the app, when navigated to with no collection, to end up at `c/main/`.
selectSectionsAndTagsLoaded was always returning true! :-[

Part of #472.

src/selectors.js
@@ -943,8 +943,8 @@ export const selectCardsLoaded = createSelector(
 );
 
 export const selectSectionsAndTagsLoaded = createSelector(
-	selectSections,
-	selectTags,
+	selectSectionsLoaded,
+	selectTagsLoaded,
 	(sectionsLoaded, tagsLoaded) => sectionsLoaded && tagsLoaded
 );
 