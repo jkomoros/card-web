de4e09504f98f189d9d9928a3fa9e14d35f74c8c
Alex Komoroske <jkomoros@gmail.com>
2021-04-26 07:54:19 -0700

    Plug in d3-force-graph to web-renderer. Although the wiring works, it doesn't yet actually show up visually. Part of #283.

--- src/components/web-renderer.js
+++ src/components/web-renderer.js
@@ -1,11 +1,27 @@
 import { html, LitElement } from '@polymer/lit-element';
 
+import { repeat } from 'lit-html/directives/repeat';
+
+import '@petitatelier/d3-force-graph';
+import 'd3/dist/d3.min.js';
+
 export class WebRenderer extends LitElement {
 	render() {
 		return html`
-			<pre>
-				${JSON.stringify(this.webInfo,'', 2)}
-			</pre>
+		<style>
+			:host {
+				height:100%;
+				width:100%;
+			}
+		</style>
+		<d3-force-graph>
+			<svg slot='nodes'>
+				${this.webInfo.nodes.length ? repeat(this.webInfo.nodes, o => o.id, o => html`<circle id=${o.id} title=${o.name} r=1></circle>`) : html`<circle id='1'></circle>`}
+			</svg>
+			<svg slot='links'>
+				${this.webInfo.edges.length ? repeat(this.webInfo.edges, o => o.source + o.target, o => html`<link source=${o.source} target=${o.target}></link>`) : html`<link source='1' target='1'></link>`}
+			</svg>
+		</d3-force-graph>
 	`;
 	}
 
@@ -14,6 +30,11 @@ export class WebRenderer extends LitElement {
 			//as returned from e.g. collection.webInfo
 			webInfo: {type:Object},
 		};
+	}
+
+	updated() {
+		//The web component doesn't notice that there are new items distributed to it, so tell it to.
+		setTimeout(() => this.shadowRoot.querySelector('d3-force-graph').reset(), 0);
 	}
 
 }
