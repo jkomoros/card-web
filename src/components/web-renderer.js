5fbe2ade82f9913a57043644fd16240d330b3565
Alex Komoroske <jkomoros@gmail.com>
2021-04-27 19:48:22 -0700

    Switch rendering of graph to use d3 directly with no animation. Not correct layout but it kind of works. Part of #283.

--- src/components/web-renderer.js
+++ src/components/web-renderer.js
@@ -1,9 +1,6 @@
-import { html, LitElement } from '@polymer/lit-element';
+import { html, LitElement, svg } from '@polymer/lit-element';
 
-import { repeat } from 'lit-html/directives/repeat';
-
-import '@petitatelier/d3-force-graph';
-import 'd3/dist/d3.min.js';
+import * as d3 from 'd3';
 
 export class WebRenderer extends LitElement {
 	render() {
@@ -13,15 +10,24 @@ export class WebRenderer extends LitElement {
 				height:100%;
 				width:100%;
 			}
+			svg {
+				height: 100%;
+				width: 100%;
+			}
+			circle {
+				fill: var(--app-primary-color);
+				z-index:1;
+			}
+			line {
+				stroke:var(--app-dark-text-color-light);
+				z-index:0;
+			}
 		</style>
-		<d3-force-graph>
-			<svg slot='nodes'>
-				${this.webInfo.nodes.length ? repeat(this.webInfo.nodes, o => o.id, o => html`<circle id=${o.id} title=${o.name} r=1></circle>`) : html`<circle id='1'></circle>`}
-			</svg>
-			<svg slot='links'>
-				${this.webInfo.edges.length ? repeat(this.webInfo.edges, o => o.source + o.target, o => html`<link source=${o.source} target=${o.target}></link>`) : html`<link source='1' target='1'></link>`}
-			</svg>
-		</d3-force-graph>
+
+		<svg>
+			${this._calculatedGraph.edges.map(node => svg`<line x1=${node.source.x} x2=${node.target.x} y1=${node.source.y} y2=${node.target.y} stroke-width='1'></line>`)}	
+			${this._calculatedGraph.nodes.map(node => svg`<circle id=${node.id} title=${node.name} r='4' cx=${node.x} cy=${node.y}></circle>`)}
+		</svg>
 	`;
 	}
 
@@ -29,12 +35,44 @@ export class WebRenderer extends LitElement {
 		return {
 			//as returned from e.g. collection.webInfo
 			webInfo: {type:Object},
+			_calculatedGraph : { type:Object }
 		};
 	}
 
-	updated() {
-		//The web component doesn't notice that there are new items distributed to it, so tell it to.
-		setTimeout(() => this.shadowRoot.querySelector('d3-force-graph').reset(), 0);
+	_recalcGraph() {
+		//Make a deep copy of the graph, because d3 will operate on it.
+		const graph = {...this.webInfo};
+		if (Object.keys(graph).length == 0) return {nodes:[], edges:[]};
+		graph.nodes = [...graph.nodes];
+		graph.edges = [...graph.edges];
+
+		//TODO: this isn't right
+		const width = this.offsetWidth;
+		const height = this.offsetHeight;
+
+		const simulation = d3.forceSimulation(graph.nodes)
+			.force('link', d3.forceLink().id(d => d.id))
+			.force('charge', d3.forceManyBody())
+			.force('center', d3.forceCenter(width / 2, height / 2))
+			.stop();
+		
+		simulation.force('link')
+			.links(graph.edges);
+		
+		for (var i = 0; i < 300; ++i) simulation.tick();
+
+		return graph;
+	}
+
+	constructor() {
+		super();
+		this._calculatedGraph = {nodes:[], edges:[]};
+	}
+
+	updated(changedProps) {
+		if (changedProps.has('webInfo')) {
+			this._calculatedGraph = this._recalcGraph();
+		}
 	}
 
 }
