5b65b9bcaec6154713090d6dcc4b61482b1d0278
Alex Komoroske <jkomoros@gmail.com>
2021-07-02 13:35:21 -0400

    the configure collection description now shows the collection when you load it up.
    Doesn't do anything esle, still.
    
    Part of #498.

--- src/components/configure-collection-dialog.js
+++ src/components/configure-collection-dialog.js
@@ -8,6 +8,7 @@ import { DialogElement } from './dialog-element.js';
 
 import {
 	selectConfigureCollectionDialogOpen,
+	selectActiveCollectionDescription
 } from '../selectors.js';
 
 import {
@@ -17,7 +18,7 @@ import {
 class ConfigureCollectionDialog extends connect(store)(DialogElement) {
 	innerRender() {
 		return html`
-			<em>TODO: show something useful here</em>
+			<em>${this._collectionDescription.serialize()}</em>
 		`;
 	}
 
@@ -35,9 +36,16 @@ class ConfigureCollectionDialog extends connect(store)(DialogElement) {
 		store.dispatch(closeConfigureCollectionDialog());
 	}
 
+	static get properties() {
+		return {
+			_collectionDescription: {type: Object},
+		};
+	}
+
 	stateChanged(state) {
 		//tODO: it's weird that we manually set our superclasses' public property
 		this.open = selectConfigureCollectionDialogOpen(state);
+		this._collectionDescription = selectActiveCollectionDescription(state);
 	}
 
 }
