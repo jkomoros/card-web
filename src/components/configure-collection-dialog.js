7164979c23fd79de60980a71d165bf61c8a5889e
Alex Komoroske <jkomoros@gmail.com>
2021-07-04 15:03:16 -0400

    Make it so you can delete union filter pieces.
    The delete filter button moves to next to the select. If it's the last item in the set then it removes the whoel filter, but if it's a union filter it removes just that part.
    
    Part of #498.

--- src/components/configure-collection-dialog.js
+++ src/components/configure-collection-dialog.js
@@ -94,12 +94,11 @@ class ConfigureCollectionDialog extends connect(store)(DialogElement) {
 		const unionFilterPieces = splitUnionFilter(firstFilterPart);
 		const isConfigurableFilter = CONFIGURABLE_FILTER_INFO[firstFilterPart] != undefined;
 		return html`<li>
-			${unionFilterPieces.map((filterPiece, i) => html`<select @change=${this._handleModifyFilterChanged} .index=${index} .subIndex=${i}>${this._filterOptions(filterPiece, unionFilterPieces.length <= 1)}</select>${help(this._filterDescriptions[filterPiece])}`)}
+			${unionFilterPieces.map((filterPiece, i) => html`<select @change=${this._handleModifyFilterChanged} .index=${index} .subIndex=${i}>${this._filterOptions(filterPiece, unionFilterPieces.length <= 1)}</select>${help(this._filterDescriptions[filterPiece])}<button class='small' .index=${index} .subIndex=${i} @click=${this._handleRemoveFilterClicked}>${DELETE_FOREVER_ICON}</button>`)}
 			${isConfigurableFilter ? 
 		html`<input type='text' .index=${index} @change=${this._handleModifyFilterRestChanged} .value=${restFilter}>` : 
 		html`<button class='small' .index=${index} @click=${this._handleAddUnionFilterClicked} title='Add new filter to OR with previous filters in this row'>${PLUS_ICON}</button>`
 }
-			<button class='small' .index=${index} @click=${this._handleRemoveFilterClicked}>${DELETE_FOREVER_ICON}</button>
 		</li>`;
 	}
 
@@ -141,6 +140,12 @@ class ConfigureCollectionDialog extends connect(store)(DialogElement) {
 			return;
 		}
 		const index = ele.index;
+		const filterPieces = splitUnionFilter(this._collectionDescription.filters[index]);
+		if (filterPieces.length > 1) {
+			filterPieces.splice(ele.subIndex, 1);
+			store.dispatch(navigateToCollection(collectionDescriptionWithFilterModified(this._collectionDescription, index, filterPieces.join(UNION_FILTER_DELIMITER))));
+			return;
+		}
 		store.dispatch(navigateToCollection(collectionDescriptionWithFilterRemoved(this._collectionDescription, index)));
 	}
 
