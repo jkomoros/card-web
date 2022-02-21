1be94690a5aab6c2527d801ba7ec5f87fd362c5f
Alex Komoroske <jkomoros@gmail.com>
2022-02-21 07:07:35 -0800

    Make it so that the diff / unsaved changes code uses selectEditingNormalizedCard.
    This card changes far less often (it delays after typing ends). Before, on every keystroke we were normalizing the card HTML TWICE just to detect if there were any changes to warn about whether there were unsaved changes in the UI. This could create some lag on keystrokes. The trade off is that it MIGHT not have a race where if you try to save or cancel a card editing before the normalized properties have been calculated it might not warn you properly.
    
    It's a small improvement but not huge. Part of #564.

--- src/selectors.js
+++ src/selectors.js
@@ -747,7 +747,7 @@ export const selectEditingUnderlyingCard = createSelector(
 );
 
 export const selectEditingCardHasUnsavedChanges = createSelector(
-	selectEditingCard,
+	selectEditingNormalizedCard,
 	selectEditingUnderlyingCardSnapshot,
 	(editingCard, snapshot) => cardDiffHasChanges(generateCardDiff(snapshot, editingCard))
 );
@@ -761,7 +761,7 @@ const selectEditingUnderlyingCardSnapshotDiff = createSelector(
 export const selectOvershadowedUnderlyingCardChangesDiff = createSelector(
 	selectEditingOriginalUnderlyingCardSnapshot,
 	selectEditingUnderlyingCard,
-	selectEditingCard,
+	selectEditingNormalizedCard,
 	(original, snapshot, current) => overshadowedDiffChanges(original, snapshot, current)
 );
 
