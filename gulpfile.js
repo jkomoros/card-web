e8b34fc3cf59cbbf80acea269adcf558ebbfadf3
Alex Komoroske <jkomoros@gmail.com>
2022-05-22 10:45:49 -0700

    Remove prpl-server from gulpfile.
    Part of #589.

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,6 @@
 
 const gulp = require('gulp');
 const rename = require('gulp-rename');
-const replace = require('gulp-replace');
-const del = require('del');
 const spawnSync = require('child_process').spawnSync;
 //Only used for `reset-dev` because htat uses subcommands
 const exec = require('child_process').exec;
@@ -487,35 +485,6 @@ gulp.task('reset-dev',
 		GSUTIL_RSYNC_UPLOADS
 	)
 );
-
-/**
- * Cleans the prpl-server build in the server directory.
- */
-gulp.task('prpl-server:clean', () => {
-	return del('server/build');
-});
-
-/**
- * Copies the prpl-server build to the server directory while renaming the
- * node_modules directory so services like App Engine will upload it.
- */
-gulp.task('prpl-server:build', () => {
-	const pattern = 'node_modules';
-	const replacement = 'node_assets';
-
-	return gulp.src('build/**')
-		.pipe(rename(((path) => {
-			path.basename = path.basename.replace(pattern, replacement);
-			path.dirname = path.dirname.replace(pattern, replacement);
-		})))
-		.pipe(replace(pattern, replacement))
-		.pipe(gulp.dest('server/build'));
-});
-
-gulp.task('prpl-server', gulp.series(
-	'prpl-server:clean',
-	'prpl-server:build'
-));
 
 var realFavicon = require ('gulp-real-favicon');
 
