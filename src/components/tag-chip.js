jkomoros/card-web

commit ab94f702eeae32f878f62c4f46b3686df56cbf2c

Use lit styles for tag-chip.
Part of #453.

src/components/tag-chip.js
@@ -1,11 +1,12 @@
 
-import { LitElement, html } from 'lit';
+import { LitElement, html, css } from 'lit';
 import { urlForTag } from '../actions/app';
 
 class TagChip  extends LitElement {
-	render() {
-		return html`
-			<style>
+
+	static get styles() {
+		return [
+			css`
 				:host {
 					margin: 0 0.2em;
 					display: inline-block;
@@ -46,7 +47,12 @@ class TagChip  extends LitElement {
 				span.enabled.editing a.delete {
 					display:inline;
 				}
-			</style>
+			`
+		];
+	}
+
+	render() {
+		return html`
 			<span style=${this._filter ? 'filter: ' + this._filter : ''} class='${this.editing ? 'editing' : ''} ${this.addition ? 'addition' : ''} ${this.deletion ? 'deletion' : ''} ${this._disabled ? 'disabled' : 'enabled'}' title='${this._description}' @mousemove=${this._handleMouseMove}><a class='primary' href='${this._url}' @click=${this._handleTagClicked}>${this._displayName}</a><a class='delete' href='#' @click=${this._handleXClicked}>X</a></span>
 			`;
 	}

commit a3576e61073aec91f6193865921e6e6a65d2c1cf

Fix rendering error introduced in ab94f702eeae32f878f62c4f46b3686df56cbf2c, where this was used in static styles.
Part of #453.

src/components/tag-chip.js
@@ -17,7 +17,6 @@ class TagChip  extends LitElement {
 					padding: 0.2em;
 					border-radius: 0.3em;
 					font-size: 0.7em;
-					background-color: ${this._color};
 					transition: filter 0.1s ease-in-out;
 				}
 				span.enabled:hover {
@@ -53,7 +52,7 @@ class TagChip  extends LitElement {
 
 	render() {
 		return html`
-			<span style=${this._filter ? 'filter: ' + this._filter : ''} class='${this.editing ? 'editing' : ''} ${this.addition ? 'addition' : ''} ${this.deletion ? 'deletion' : ''} ${this._disabled ? 'disabled' : 'enabled'}' title='${this._description}' @mousemove=${this._handleMouseMove}><a class='primary' href='${this._url}' @click=${this._handleTagClicked}>${this._displayName}</a><a class='delete' href='#' @click=${this._handleXClicked}>X</a></span>
+			<span style=${'background-color: ' + this._color + '; ' + (this._filter ? 'filter: ' + this._filter : '')} class='${this.editing ? 'editing' : ''} ${this.addition ? 'addition' : ''} ${this.deletion ? 'deletion' : ''} ${this._disabled ? 'disabled' : 'enabled'}' title='${this._description}' @mousemove=${this._handleMouseMove}><a class='primary' href='${this._url}' @click=${this._handleTagClicked}>${this._displayName}</a><a class='delete' href='#' @click=${this._handleXClicked}>X</a></span>
 			`;
 	}
 