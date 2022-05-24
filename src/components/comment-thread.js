e57eeb6d86b319ce2ceba5346140940593398839
Alex Komoroske <jkomoros@gmail.com>
2022-05-24 12:55:15 -0700

    Use lit styles for comment-thread.
    Part of #453.

--- src/components/comment-thread.js
+++ src/components/comment-thread.js
@@ -1,4 +1,4 @@
-import { LitElement, html } from 'lit';
+import { LitElement, html, css } from 'lit';
 import { repeat } from 'lit/directives/repeat.js';
 
 import './comment-message.js';
@@ -13,43 +13,49 @@ import {
 
 // This element is *not* connected to the Redux store.
 class CommentThread extends LitElement {
+
+	static get styles() {
+		return [
+			css`
+				.container {
+					padding: 0.5em;
+					width: 12em;
+					overflow:hidden;
+					display:flex;
+					flex-direction:column;
+					align-items:center;
+					justify-content:center;
+					background-color: var(--card-color);
+					box-shadow: var(--card-shadow);
+					margin:0.5em;
+					box-sizing:border-box;
+				}
+				.buttons {
+					display:flex;
+					flex-direction:row;
+					justify-content:flex-end;
+					width:100%;
+				}
+				.buttons.left {
+					justify-content:flex-start;
+				}
+				.content {
+					display:none;
+					width:100%;
+				}
+				.content.expanded {
+					display:block;
+				}
+				.flex {
+					flex-grow:1;
+				}
+			`
+		];
+	}
+
 	render() {
 		return html`
       	${ButtonSharedStyles}
-      	<style>
-			.container {
-				padding: 0.5em;
-				width: 12em;
-				overflow:hidden;
-				display:flex;
-				flex-direction:column;
-				align-items:center;
-				justify-content:center;
-				background-color: var(--card-color);
-				box-shadow: var(--card-shadow);
-				margin:0.5em;
-				box-sizing:border-box;
-			}
-			.buttons {
-				display:flex;
-				flex-direction:row;
-				justify-content:flex-end;
-				width:100%;
-			}
-			.buttons.left {
-				justify-content:flex-start;
-			}
-			.content {
-				display:none;
-				width:100%;
-			}
-			.content.expanded {
-				display:block;
-			}
-			.flex {
-				flex-grow:1;
-			}
-		</style>
 		<div class='container'>
 			<div class='buttons'>
 				<div class='flex'>
