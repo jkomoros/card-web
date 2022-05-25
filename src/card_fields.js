jkomoros/card-web

commit c28375e160db9154fb95722ac69f0f7345fae72e

Use lit styles for card-renderer.
Part of #453.

src/components/card-renderer.js
@@ -1,9 +1,9 @@
-import { LitElement } from 'lit';
+import { LitElement, css } from 'lit';
 
 import {
 	unsafeStatic,
 	//We import the static-html so we can use unsafeStatic.
-	html
+	html,
 } from 'lit/static-html.js';
 
 import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-listeners.js';
@@ -72,37 +72,10 @@ var startGestureTimestamps = {};
 
 // This element is *not* connected to the Redux store.
 export class CardRenderer extends GestureEventListeners(LitElement) {
-	render() {
-		const cardType = this._card.card_type || CARD_TYPE_CONTENT;
-		const cardTypeConfig = CARD_TYPE_CONFIGURATION[cardType] || {};
-		const styleBlock = cardTypeConfig.styleBlock || '';
-		const fieldsToRender = editableFieldsForCardType(cardType);
-		let titleFields = [];
-		let nonScrollableFields = [];
-		let scrollableFields = [];
-		for (const [fieldName,fieldConfig] of Object.entries(fieldsToRender)) {
-			if (fieldName == TEXT_FIELD_TITLE) {
-				titleFields.push(fieldName);
-			} else if (fieldConfig.nonScrollable) {
-				nonScrollableFields.push(fieldName);
-			} else {
-				scrollableFields.push(fieldName);
-			}
-		}
-		const condensedReferenceBlocks = [];
-		const normalReferenceBlocks = [];
-		for (const block of this.expandedReferenceBlocks || []) {
-			if (block.condensed) {
-				condensedReferenceBlocks.push(block);
-			} else {
-				normalReferenceBlocks.push(block);
-			}
-		}
-		return html`
-			${SharedStyles}
-			${badgeStyles}
-			${ScrollingSharedStyles}
-			<style>
+
+	static get styles() {
+		return [
+			css`
 				:host {
 					display:block;
 					background-color: var(--card-color);
@@ -311,13 +284,46 @@ export class CardRenderer extends GestureEventListeners(LitElement) {
 				li > p {
 					display:inline;
 				}
+			`
+		];
+	}
 
+	render() {
+		const cardType = this._card.card_type || CARD_TYPE_CONTENT;
+		const cardTypeConfig = CARD_TYPE_CONFIGURATION[cardType] || {};
+		const styleBlock = cardTypeConfig.styleBlock || '';
+		const fieldsToRender = editableFieldsForCardType(cardType);
+		let titleFields = [];
+		let nonScrollableFields = [];
+		let scrollableFields = [];
+		for (const [fieldName,fieldConfig] of Object.entries(fieldsToRender)) {
+			if (fieldName == TEXT_FIELD_TITLE) {
+				titleFields.push(fieldName);
+			} else if (fieldConfig.nonScrollable) {
+				nonScrollableFields.push(fieldName);
+			} else {
+				scrollableFields.push(fieldName);
+			}
+		}
+		const condensedReferenceBlocks = [];
+		const normalReferenceBlocks = [];
+		for (const block of this.expandedReferenceBlocks || []) {
+			if (block.condensed) {
+				condensedReferenceBlocks.push(block);
+			} else {
+				normalReferenceBlocks.push(block);
+			}
+		}
+		return html`
+			${SharedStyles}
+			${badgeStyles}
+			${ScrollingSharedStyles}
+			<style>
 				${Object.entries(this.editing ? {} : (this._card.font_size_boost || {})).map(entry => {
 		return html`[data-field=${entry[0]}] {
 						font-size: ${1.0 + entry[1]}em;
 					}`;
 	})}
-
 			</style>
 			${unsafeStatic(styleBlock)}
 			<div class="container ${this.editing ? 'editing' : ''} ${this._card.published ? 'published' : 'unpublished'}">

commit ae4482961d7174a2ff42a1c75587ec4c23b58957

Fix section-head rendering bug introduced in c28375e.
The problem was caused because when we switched to using static styles, those are adopted stylesheets, which have higher precedence than style blocks in the shadowRoot (see https://github.com/WICG/construct-stylesheets/issues/93). That meant the style block wasn't actually overriding the default styles for card-renderer.

card-renderer container auto-adds a class of the current card type. This allows a pattern of `.${CARD_TYPE_mytype} ` preprended in front of style selectors to increase specificity.

That machinery is also used for CARD_TYPE_SECTION_HEAD and CARD_TYPE_WORKING_NOTES, the only two users of styleBlock. (Note that working-notes styling also broke in c28375e but I hadn't noticed before now).

Part of #453.

src/card_fields.js
@@ -120,7 +120,9 @@ it's not published will always warn.
 styleBlock: if provided, will be rendered as the style block in the card
 renderer when this card type is selected. A string that will be run through html
 tag. This isn't an html tag to avoid having heavyweight imports so this can be
-included in tests.
+included in tests. The adopted stylesheets for card-renderer will take
+precedence, so the best practices is to prepend `.${CARD_TYPE_mytype} ` in front
+of any rules you add. 
 
 dark: if true, the card is considered dark, and styles for e.g. thumbnails,
 including badge color, will swap.
@@ -152,7 +154,7 @@ export const CARD_TYPE_CONFIGURATION = {
 		dark: true,
 		styleBlock: String.raw`
 			<style>
-				.background {
+				.${CARD_TYPE_SECTION_HEAD} .background {
 					position:absolute;
 					display:block;
 					height:50%;
@@ -162,13 +164,13 @@ export const CARD_TYPE_CONFIGURATION = {
 					/* counteract the padding in the base card */
 					margin-left:-1.45em;
 				}
-				h1 {
+				.${CARD_TYPE_SECTION_HEAD} h1 {
 					font:var(--app-header-font-family);
 					font-weight:bold;
 					font-size:3.0em;
 					margin-top:2.25em;
 				}
-				h2 {
+				.${CARD_TYPE_SECTION_HEAD} h2 {
 					color: var(--app-primary-color-subtle);
 					font-size:1.2em;
 					font-weight:normal;
@@ -184,7 +186,7 @@ export const CARD_TYPE_CONFIGURATION = {
 		orphanedByDefault: true,
 		styleBlock: `
 		<style>
-			section {
+			.${CARD_TYPE_WORKING_NOTES} section {
 				font-size:0.8em;
 				line-height:1.2;
 			}

src/components/card-renderer.js
@@ -326,7 +326,7 @@ export class CardRenderer extends GestureEventListeners(LitElement) {
 	})}
 			</style>
 			${unsafeStatic(styleBlock)}
-			<div class="container ${this.editing ? 'editing' : ''} ${this._card.published ? 'published' : 'unpublished'}">
+			<div class="container ${this.editing ? 'editing' : ''} ${this._card.published ? 'published' : 'unpublished'} ${cardType}">
 				<div class='background'></div>
 				<div class='content'>
 					<div class='title-container'>