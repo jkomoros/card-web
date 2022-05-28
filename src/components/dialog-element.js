jkomoros/card-web

commit 66ffa5dd7bc7035789e7ef2fe4f1f7c1a7d2fb07

Refactor dialog-element to not require templating in the styles.
Part of #453.

src/components/dialog-element.js
@@ -20,10 +20,22 @@ export class DialogElement extends LitElement {
 					width:100%;
 					top:0;
 					left:0;
+				}
+
+				.container {
+					position: absolute;
+					height: 100%;
+					width: 100%;
+					top: 0;
+					left: 0;
 					/* Note that card-preview has a z-index higher than this to
 					show up above it */
-					z-index:1000;
-					display: ${this.open ? 'block' : 'none'}
+					z-index: 1000;
+					display: none;
+				}
+
+				.container.open {
+					display: block;
 				}
 
 				.background {
@@ -82,12 +94,14 @@ export class DialogElement extends LitElement {
 
 
 			</style>
-			<div class='background ${this.mobileMode ? 'mobile': ''}' @click=${this._handleBackgroundClicked}>
-				<div class='content'>
-					<button class='small' id='close' @click=${this.cancel}>${CANCEL_ICON}</button>
-					<h2>${this.title || ''}</h2>
-					<div id='inner'>
-					${this.innerRender()}
+			<div class='container ${this.open ? 'open' : 'closed'}'>
+				<div class='background ${this.mobileMode ? 'mobile': ''}' @click=${this._handleBackgroundClicked}>
+					<div class='content'>
+						<button class='small' id='close' @click=${this.cancel}>${CANCEL_ICON}</button>
+						<h2>${this.title || ''}</h2>
+						<div id='inner'>
+						${this.innerRender()}
+						</div>
 					</div>
 				</div>
 			</div>

commit 911b194e5022267d4e656de5d09e94fb78bd829c

Fix a bug introduced in 66ffa5d where the dialog-elements obscured other bits of the page.
:host was positition:absolute and positioned to obscure everything even when the dialog wasn't open. But by removing all styles on :host, and making :host not a positioned element, .container can now be positioned relative to its position parent, and the :host doesn't obscure any content if unnecessary.

Part of #453.

src/components/dialog-element.js
@@ -14,14 +14,6 @@ export class DialogElement extends LitElement {
 		ButtonSharedStyles,
 		SharedStyles,
 		css`
-			:host {
-				position:absolute;
-				height:100%;
-				width:100%;
-				top:0;
-				left:0;
-			}
-
 			.container {
 				position: absolute;
 				height: 100%;