cd864db0b6bd6b206b454f0bf84701bd7a38fb81
Alex Komoroske <jkomoros@gmail.com>
2022-05-30 16:29:08 -0700

    Start resolving a few type problems in nlp.js.
    Part of #601.

--- src/nlp.ts
+++ src/nlp.ts
@@ -39,8 +39,12 @@ import {
 	deepEqualReturnSame
 } from './memoize.js';
 
+import {
+	Cards
+} from './types.js';
+
 //allCards can be raw or normalized. Memoized so downstream memoizing things will get the same thing for the same values
-export const conceptCardsFromCards = deepEqualReturnSame(memoizeFirstArg((allCards) => {
+export const conceptCardsFromCards = deepEqualReturnSame(memoizeFirstArg((allCards : Cards) => {
 	return Object.fromEntries(Object.entries(allCards).filter(entry => entry[1].card_type == CARD_TYPE_CONCEPT));
 }));
 
@@ -70,7 +74,7 @@ export const getAllConceptStringsFromConceptCard = (rawConceptCard) => {
 //concept cards, it might include multiple entries for each concept card if it
 //has title alternates. memoized so downstream memoizing things will get object
 //equality.
-export const getConceptsFromConceptCards = deepEqualReturnSame(memoizeFirstArg((conceptCards) => {
+export const getConceptsFromConceptCards = deepEqualReturnSame(memoizeFirstArg((conceptCards : Cards) => {
 	const result = {};
 	for (const card of Object.values(conceptCards)) {
 		for (const conceptStr of getAllConceptStringsFromConceptCard(card)) {
