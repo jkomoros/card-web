df7861af6085c95ddef2286fd8f14311541ecdcb
Alex Komoroske <jkomoros@gmail.com>
2022-06-01 09:26:06 -0700

    Fix type warnings on store.addReducers.
    Part of #601.

--- src/store.ts
+++ src/store.ts
@@ -1,18 +1,10 @@
-/**
-@license
-Copyright (c) 2018 The Polymer Project Authors. All rights reserved.
-This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
-The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
-The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
-Code distributed by Google as part of the polymer project is also
-subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
-*/
-
 import {
 	createStore,
 	compose,
 	applyMiddleware,
-	combineReducers
+	combineReducers,
+	Store,
+	Reducer
 } from 'redux';
 import thunk from 'redux-thunk';
 import { lazyReducerEnhancer } from 'pwa-helpers/lazy-reducer-enhancer.js';
@@ -24,12 +16,16 @@ import data from './reducers/data.js';
 // See https://github.com/zalmoxisus/redux-devtools-extension for more information.
 const devCompose = window['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__'] || compose;
 
+interface LazyStore extends Store {
+	addReducers(reducers : {[name : string]: Reducer})
+}
+
 // Initializes the Redux store with a lazyReducerEnhancer (so that you can
 // lazily add reducers after the store has been created) and redux-thunk (so
 // that you can dispatch async actions). See the "Redux and state management"
 // section of the wiki for more details:
 // https://github.com/Polymer/pwa-starter-kit/wiki/4.-Redux-and-state-management
-export const store = createStore(
+export const store : LazyStore = createStore(
 	state => state,
 	devCompose(
 		lazyReducerEnhancer(combineReducers),
