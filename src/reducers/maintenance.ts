3710bd3d0b11837b26a316ae7ca6ebccfe68cc23
Alex Komoroske <jkomoros@gmail.com>
2022-06-01 10:37:37 -0700

    Add and use MaintenanceState type.
    Part of #601.

--- src/reducers/maintenance.ts
+++ src/reducers/maintenance.ts
@@ -3,12 +3,22 @@ import {
 	UPDATE_MAINTENANCE_TASK_ACTIVE
 } from '../actions/maintenance.js';
 
-const INITIAL_STATE = {
+import {
+	MaintenanceTaskID,
+	MaintenanceTask
+} from '../types.js';
+
+type MaintenanceState = {
+	executedTasks: {[id : MaintenanceTaskID]: MaintenanceTask},
+	taskActive: false,
+}
+
+const INITIAL_STATE : MaintenanceState = {
 	executedTasks: {},
 	taskActive: false,
 };
 
-const app = (state = INITIAL_STATE, action) => {
+const app = (state : MaintenanceState = INITIAL_STATE, action) : MaintenanceState => {
 	switch (action.type) {
 	case UPDATE_EXECUTED_MAINTENANCE_TASKS: 
 		return {
