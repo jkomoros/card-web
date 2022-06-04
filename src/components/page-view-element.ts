390a9a2d5c9cefc50849bebc2441080025e571fd
Alex Komoroske <jkomoros@gmail.com>
2022-06-04 07:43:31 -0700

    Typescript annotations on page-view-element.ts
    Part of #601.

--- src/components/page-view-element.ts
+++ src/components/page-view-element.ts
@@ -1,24 +1,14 @@
-/**
-@license
-Copyright (c) 2018 The Polymer Project Authors. All rights reserved.
-This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
-The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
-The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
-Code distributed by Google as part of the polymer project is also
-subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
-*/
-
 import { LitElement } from 'lit';
+import { property } from 'lit/decorators.js';
 
 export class PageViewElement extends LitElement {
+
+	@property({ type: Boolean })
+	active : boolean;
+
 	// Only render this page if it's actually visible.
-	shouldUpdate() {
+	override shouldUpdate() {
 		return this.active;
 	}
 
-	static get properties() {
-		return {
-			active: { type: Boolean }
-		};
-	}
 }
