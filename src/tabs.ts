jkomoros/card-web

commit f3fd4c1a266e893fd86debf9bee8257f0fbf4a9f

Have a difference between ExpandedTabConfigItem and TabConfigItem. The latter has an expanded icon and collection in a different property.
Part of #601.

src/tabs.ts
@@ -34,8 +34,9 @@ export const READING_LIST_FALLBACK_CARD = 'about-reading-lists';
 export const STARS_FALLBACK_CARD = 'about-stars';
 
 export type TabConfig = TabConfigItem[];
+export type ExpandedTabConfig = ExpandedTabConfigItem[];
 
-type TabConfigItem = {
+interface TabConfigItem {
 	//If set, will expand in line for the named expansion. See src/tabs.js for named expansions.
 	//Applies recursively until no expansions remain.
 	//Note that 'sections' is a special value that will expand to the current values of sections.
@@ -71,7 +72,12 @@ type TabConfigItem = {
 	default?: boolean
 }
 
-export const tabConfiguration = (config : TabConfig, sections : Sections, tags : Sections) : TabConfig => {
+interface ExpandedTabConfigItem extends TabConfigItem {
+	expandedCollection: CollectionDescription,
+	expandedIcon: TemplateResult
+}
+
+export const tabConfiguration = (config : TabConfig, sections : Sections, tags : Sections) : ExpandedTabConfig => {
 	if (!config) config = DEFAULT_CONFIG;
 	let array = config;
 	let lastArray = [];
@@ -91,19 +97,15 @@ export const tabConfiguration = (config : TabConfig, sections : Sections, tags :
 	return inflateCollectionsAndIcons(array);
 };
 
-const inflateCollectionsAndIcons = (config : TabConfig) : TabConfig => {
+const inflateCollectionsAndIcons = (config : TabConfig) : ExpandedTabConfig => {
 	let result = [];
 	for (let item of config) {
-		let itemToAdd = {...item};
-		if (item.collection && !(item.collection instanceof CollectionDescription)) {
-			itemToAdd.collection = CollectionDescription.deserialize(item.collection);
-		}
-		if (item.icon && typeof item.icon == 'string') {
-			itemToAdd.icon = icons[item.icon];
-			if (!itemToAdd.icon) {
-				console.warn('Invalid icon name: ' + item.icon);
-			}
-		}
+		let itemToAdd : ExpandedTabConfigItem = {
+			...item,
+			expandedCollection:  (item.collection instanceof CollectionDescription) ? item.collection : CollectionDescription.deserialize(item.collection),
+			expandedIcon: (typeof item.icon != 'string') ? item.icon : icons[item.icon]
+		};
+		if (item.icon && !itemToAdd.expandedIcon) console.warn('Invalid icon name: ' + item.icon);
 		result.push(itemToAdd);
 	}
 	return result;

src/components/main-view.ts
@@ -265,7 +265,7 @@ class MainView extends connect(store)(LitElement) {
 					<div main-title>${this._appTitleFirstPart}<span>${this._appTitleSecondPart}</span></div>
 					<div class='spacer'></div>
 					<nav class="toolbar-list">
-						${this._tabs.map(tab => html`<a class='${tab.icon ? 'icon-item' : ''}' title='${tab.display_name}' ?selected=${tab.collection && tab.collection.equivalent(this._collectionDescription)} href='${tab.href ? tab.href : '/' + PAGE_DEFAULT + '/' + tab.collection.serializeShort()}' target=${tab.href ? '_blank': ''} ?hidden=${tab.hide || (tab.hideIfEmpty && this._countsForTabs[tab.collection.serialize()] === 0)}>${tab.icon ? html`${tab.icon} ${tab.count ? html`<span>${this._countsForTabs[tab.collection.serialize()]}</span>` : ''}` : tab.italics ? html`<em>${tab.display_name}</em>` : html`${tab.display_name}`}</a>`)}
+						${this._tabs.map(tab => html`<a class='${tab.icon ? 'icon-item' : ''}' title='${tab.display_name}' ?selected=${tab.expandedCollection && tab.expandedCollection.equivalent(this._collectionDescription)} href='${tab.href ? tab.href : '/' + PAGE_DEFAULT + '/' + tab.expandedCollection.serializeShort()}' target=${tab.href ? '_blank': ''} ?hidden=${tab.hide || (tab.hideIfEmpty && this._countsForTabs[tab.expandedCollection.serialize()] === 0)}>${tab.icon ? html`${tab.icon} ${tab.count ? html`<span>${this._countsForTabs[tab.expandedCollection.serialize()]}</span>` : ''}` : tab.italics ? html`<em>${tab.display_name}</em>` : html`${tab.display_name}`}</a>`)}
 					</nav>
 					<div class='spacer dev'>
 						${this._devMode ? html`DEVMODE` : ''}

src/selectors.ts
@@ -1169,7 +1169,7 @@ export const selectTabCollectionFallbacks = createSelector(
 		let result = {};
 		for (const item of config) {
 			if (!item.fallback_cards) continue;
-			result[item.collection.serialize()] = item.fallback_cards.map(idOrSlug => slugIndex[idOrSlug] || idOrSlug);
+			result[item.expandedCollection.serialize()] = item.fallback_cards.map(idOrSlug => slugIndex[idOrSlug] || idOrSlug);
 		}
 		return result;
 	}
@@ -1182,7 +1182,7 @@ export const selectTabCollectionStartCards = createSelector(
 		let result = {};
 		for (const item of config) {
 			if (!item.start_cards) continue;
-			result[item.collection.serialize()] = item.start_cards.map(idOrSlug => slugIndex[idOrSlug] || idOrSlug);
+			result[item.expandedCollection.serialize()] = item.start_cards.map(idOrSlug => slugIndex[idOrSlug] || idOrSlug);
 		}
 		return result;
 	}
@@ -1458,7 +1458,7 @@ export const selectCountsForTabs = createSelector(
 		let result = {};
 		for (let tab of tabs) {
 			if (!tab.count) continue;
-			result[tab.collection.serialize()] = tab.collection.collection(args).numCards;
+			result[tab.expandedCollection.serialize()] = tab.expandedCollection.collection(args).numCards;
 		}
 		return result;
 	}

commit 7a1403f520f9a2debb6f22ae3000370e8772cb7b

Fixed a bug introduced in f3fd4c1a2 where tab configurations without collections would error.
Part of #601.

src/tabs.ts
@@ -102,7 +102,7 @@ const inflateCollectionsAndIcons = (config : TabConfig) : ExpandedTabConfig => {
 	for (let item of config) {
 		let itemToAdd : ExpandedTabConfigItem = {
 			...item,
-			expandedCollection:  (item.collection instanceof CollectionDescription) ? item.collection : CollectionDescription.deserialize(item.collection),
+			expandedCollection:  (item.collection instanceof CollectionDescription) ? item.collection : (item.collection ? CollectionDescription.deserialize(item.collection) : item.collection),
 			expandedIcon: (typeof item.icon != 'string') ? item.icon : icons[item.icon]
 		};
 		if (item.icon && !itemToAdd.expandedIcon) console.warn('Invalid icon name: ' + item.icon);