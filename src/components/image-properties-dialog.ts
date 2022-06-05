f10808288674b8c2bac87d2b57013979e9ce4e06
Alex Komoroske <jkomoros@gmail.com>
2022-06-04 17:17:35 -0700

    .property to data-property, resolving more warnings. Just one more!
    Part of #601.

--- src/components/image-properties-dialog.ts
+++ src/components/image-properties-dialog.ts
@@ -88,14 +88,14 @@ class ImagePropertiesDialog extends connect(store)(DialogElement) {
 }
 			<div class='row'>
 				<div>
-					<label>Size</label><input type='number' min='0.5' max='30.0' step='0.5' .property=${'emSize'} .value=${String(img.emSize)} @input=${this._handleTextInput}></input>
+					<label>Size</label><input type='number' min='0.5' max='30.0' step='0.5' data-property=${'emSize'} .value=${String(img.emSize)} @input=${this._handleTextInput}></input>
 				</div>
 				<div>
-					<label>Margin</label><input type='number' min='0.0' max='10.0' step='0.25' .property=${'margin'} .value=${String(img.margin)} @input=${this._handleTextInput}></input>
+					<label>Margin</label><input type='number' min='0.0' max='10.0' step='0.25' data-property=${'margin'} .value=${String(img.margin)} @input=${this._handleTextInput}></input>
 				</div>
 				<div>
 					<label>Position</label>
-					<select @change=${this._handleTextInput} .value=${img.position} .property=${'position'}>
+					<select @change=${this._handleTextInput} .value=${img.position} data-property=${'position'}>
 						${Object.keys(LEGAL_IMAGE_POSITIONS).map(item => html`<option value=${item} ?selected=${item == img.position}>${item}</option>`)}
 					</select>
 				</div>
@@ -110,8 +110,8 @@ class ImagePropertiesDialog extends connect(store)(DialogElement) {
 					<label>Height x Width</label><em>${img.height || 'Unknown'} x ${img.width || 'Unknown'}</em>
 				</div>
 			</div>
-			<label>Alt Text</label> <input type='text' .property=${'alt'} .value=${img.alt} @input=${this._handleTextInput}></input>
-			<label>Original Location</label> <input type='text' .property=${'original'} .value=${img.original} @input=${this._handleTextInput}></input>
+			<label>Alt Text</label> <input type='text' data-property=${'alt'} .value=${img.alt} @input=${this._handleTextInput}></input>
+			<label>Original Location</label> <input type='text' data-property=${'original'} .value=${img.original} @input=${this._handleTextInput}></input>
 			<div class='buttons'>
 				<button class='round' @click='${this._handleDoneClicked}'>${CHECK_CIRCLE_OUTLINE_ICON}</button>
 			</div>
@@ -125,7 +125,7 @@ class ImagePropertiesDialog extends connect(store)(DialogElement) {
 
 	_handleTextInput(e) {
 		const ele = e.composedPath()[0];
-		store.dispatch(changeImagePropertyAtIndex(this._index, ele.property, ele.value));
+		store.dispatch(changeImagePropertyAtIndex(this._index, ele.dataset.property, ele.value));
 	}
 
 	_handleLeftClicked() {
