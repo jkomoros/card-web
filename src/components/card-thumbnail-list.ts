0fb99eb4d1d64ab522bda993cb4695319df7f8c9
Alex Komoroske <jkomoros@gmail.com>
2022-06-04 17:06:40 -0700

    Move cardid, after to dataset to resolve an unknown property warning.
    Part of #601.

--- src/components/card-thumbnail-list.ts
+++ src/components/card-thumbnail-list.ts
@@ -295,10 +295,10 @@ class CardThumbnailList  extends connect(store)(LitElement) {
 			${this.renderOffset ? html`<div class='row'><button id='prev' class='small' title='Previous cards' @click=${this._handlePreviousClicked}>${ARROW_UPWARD_ICON}</button><label class='interactive' for='prev'>Previous ${this._offsetChunk} cards</label></div>` : ''}
 			<div class='${this._dragging ? 'dragging' : ''} ${this.grid ? 'grid' : ''}'>
 				${repeat(this._cards, (i) => i.id, (i, index) => html`
-				${index >= this.collection.numStartCards ? html`<div class='spacer' .cardid=${i.id} .after=${false} @dragover='${this._handleDragOver}' @dragenter='${this._handleDragEnter}' @dragleave='${this._handleDragLeave}' @drop='${this._handleDrop}'></div>` : ''}
+				${index >= this.collection.numStartCards ? html`<div class='spacer' data-cardid=${i.id} data-after=${false} @dragover='${this._handleDragOver}' @dragenter='${this._handleDragEnter}' @dragleave='${this._handleDragLeave}' @drop='${this._handleDrop}'></div>` : ''}
 				${this._labels && this._labels[index] !== undefined && this._labels[index] !== '' ? html`<div class='label'><span>${this.collection ? this.collection.sortLabelName : ''} <strong>${this._labels[index]}</strong></span></div>` : html``}
 				${this._thumbnail(i, index)}
-				${index == (this.collection.finalSortedCards.length - 1) ? html`<div class='spacer' .cardid=${i.id} .after=${true} @dragover='${this._handleDragOver}' @dragenter='${this._handleDragEnter}' @dragleave='${this._handleDragLeave}' @drop='${this._handleDrop}'></div>` : ''}
+				${index == (this.collection.finalSortedCards.length - 1) ? html`<div class='spacer' data-cardid=${i.id} data-after=${true} @dragover='${this._handleDragOver}' @dragenter='${this._handleDragEnter}' @dragleave='${this._handleDragLeave}' @drop='${this._handleDrop}'></div>` : ''}
 				`)}
 			</div>
 			${this._cardsClipped ? html`<div class='row'><button id='next' class='small' title='Next cards' @click=${this._handleNextClicked}>${ARROW_DOWNWARD_ICON}</button><label class='interactive' for='next'>Next ${this._offsetChunk} cards</label></div>` : ''}
@@ -430,8 +430,8 @@ class CardThumbnailList  extends connect(store)(LitElement) {
 			console.log('Start card can\'t be reordered');
 			return;
 		}
-		let otherID = target.cardid;
-		let isAfter = target.after ? true : false;
+		let otherID = target.dataset.cardid;
+		let isAfter = target.dataset.after ? true : false;
 		this.dispatchEvent(new CustomEvent('reorder-card', {composed: true, detail: {card: thumbnail.card, otherID: otherID, isAfter: isAfter}}));
 	}
 
