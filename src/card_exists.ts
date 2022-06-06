12dd76f253347c92066af06b401d0a891a8b2f89
Alex Komoroske <jkomoros@gmail.com>
2022-06-06 16:06:18 -0700

    Fixed a number of noImplicitAny type problems.
    Part of #601.

--- src/card_exists.ts
+++ src/card_exists.ts
@@ -1,13 +1,19 @@
+import {
+	CardID,
+	CardType,
+	State
+} from "./types.js";
+
 //selectRawCards is duplicated from selectors.js
-const selectRawCards = (state) => state.data ? state.data.cards : {};
-const selectPendingNewCardID = (state) => state.data ? state.data.pendingNewCardID : '';
-const selectPendingNewCardType = (state) => state.data ? state.data.pendingNewCardType : '';
+const selectRawCards = (state : State) => state.data ? state.data.cards : {};
+const selectPendingNewCardID = (state : State) => state.data ? state.data.pendingNewCardID : '';
+const selectPendingNewCardType = (state : State) => state.data ? state.data.pendingNewCardType : '';
 
 //getCardExists checks if the card with the given ID is known to exist. This is
 //typicaly because a card with that ID is in the set of cards on the client, but
 //also might be because we just created that card and know it will exist soon
 //even though it's not yet on the client.
-export const getCardExists = (state, cardID) => {
+export const getCardExists = (state : State, cardID : CardID) : boolean => {
 	if (cardID == selectPendingNewCardID(state)) return true;
 	return Object.keys(selectRawCards(state)).some(key => key === cardID);
 };
@@ -15,9 +21,10 @@ export const getCardExists = (state, cardID) => {
 //Returns the cardType for the card with the given ID. It looks over the set of
 //cards in client, but also will return the pendingNewCardType if the cardID
 //matches. See also getCardExists.
-export const getCardType = (state, cardID) => {
+export const getCardType = (state : State, cardID : CardID) : CardType => {
 	if (cardID == selectPendingNewCardID(state)) return selectPendingNewCardType(state);
 	const cards = selectRawCards(state);
-	const card = cards[cardID] || {};
-	return card.card_type || '';
+	const card = cards[cardID];
+	if (!card) return '';
+	return card.card_type;
 };
