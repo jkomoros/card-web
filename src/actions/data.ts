jkomoros/card-web

commit 997123f7f91fb0161e7b48e5f51a675341e7c1f7

Resolved a few noImplicitAny in cardOverflowsFieldForBoost.
Part of #601.

src/card_fields.ts
@@ -6,7 +6,12 @@ import {
 import * as CONSTANTS from './card_field_constants.js';
 
 import {
+	EMPTY_PROCESSED_CARD
+} from './actions/data.js';
+
+import {
 	Card,
+	ProcessedCard,
 	CardTypeConfigurationMap,
 	ReferenceTypeConfigurationMap,
 	CardFieldTypeConfigurationMap,
@@ -14,13 +19,16 @@ import {
 	CardType,
 	ReferenceType,
 	FontSizeBoostMap,
-	CardFieldType
+	CardFieldTypeEditable
 } from './types.js';
 
 import {
 	TypedObject
 } from './typed_object.js';
-import { CardRenderer } from './components/card-renderer.js';
+
+import {
+	CardRenderer
+} from './components/card-renderer.js';
 
 /*
 
@@ -546,7 +554,7 @@ export const DERIVED_FIELDS_FOR_CARD_TYPE = Object.fromEntries(TypedObject.keys(
 	return [typ, Object.fromEntries(TypedObject.entries(TEXT_FIELD_CONFIGURATION).filter(entry => entry[1].derivedForCardTypes ? entry[1].derivedForCardTypes[typ] : false).map(entry => [entry[0], true]))];
 }));
 
-const AUTO_FONT_SIZE_BOOST_FIELDS_FOR_CARD_TYPE : {[cardType in CardType]+?: {[fieldType in CardFieldType]+?: true}} = Object.fromEntries(TypedObject.keys(CARD_TYPE_CONFIGURATION).map(typ => {
+const AUTO_FONT_SIZE_BOOST_FIELDS_FOR_CARD_TYPE : {[cardType in CardType]+?: {[fieldType in CardFieldTypeEditable]+?: true}} = Object.fromEntries(TypedObject.keys(CARD_TYPE_CONFIGURATION).map(typ => {
 	return [typ, Object.fromEntries(TypedObject.entries(TEXT_FIELD_CONFIGURATION).filter(entry => entry[1].autoFontSizeBoostForCardTypes ? entry[1].autoFontSizeBoostForCardTypes[typ] : false).map(entry => [entry[0], true]))];
 }));
 
@@ -622,7 +630,7 @@ const MAX_FONT_BOOST_BISECT_STEPS = 3;
 //calculateBoostForCardField is an expensive method because it repeatedly
 //changes layout and then reads it back. But even with that, it typically takes
 //less than 15ms or so on a normal computer.
-const calculateBoostForCardField = async (card, field) => {
+const calculateBoostForCardField = async (card : Card, field : CardFieldTypeEditable) : Promise<number> => {
 
 	const max = TEXT_FIELD_CONFIGURATION[field].autoFontSizeBoostForCardTypes[card.card_type];
 	let low = 0.0;
@@ -660,7 +668,7 @@ const calculateBoostForCardField = async (card, field) => {
 	return middle;
 };
 
-const cardOverflowsFieldForBoost = async (card, field, proposedBoost) => {
+const cardOverflowsFieldForBoost = async (card : Card, field : CardFieldTypeEditable, proposedBoost : number) : Promise<boolean> => {
 	if (!cardRendererProvider) {
 		console.warn('No card renderer provider provided');
 		return false;
@@ -670,7 +678,7 @@ const cardOverflowsFieldForBoost = async (card, field, proposedBoost) => {
 		console.warn('No active card renderer');
 		return false;
 	}
-	let tempCard = {...card, font_size_boost: {...card.font_size_boost, [field]:proposedBoost}};
+	let tempCard : ProcessedCard = {...EMPTY_PROCESSED_CARD, ...card, font_size_boost: {...card.font_size_boost, [field]:proposedBoost}};
 	ele.card = tempCard;
 	await ele.updateComplete;
 	const isOverflowing = ele.isOverflowing();

commit f8e67926d34d8787792c990ed082f9300608a4c9

Move EMPTY_CARD and EMPTY_PROCESSED_CARD from actions/data.ts to card_fields.ts.
This fixes app load break introduced in 997123f7f91fb0161e7b48e5f51a675341e7c1f7

Part of #601.

src/actions/data.ts
@@ -33,7 +33,6 @@ import {
 } from '../typed_object.js';
 
 import {
-	currentTimestamp,
 	db,
 	deepEqualIgnoringTimestamps
 } from '../firebase.js';
@@ -141,7 +140,6 @@ import {
 	REFERENCE_TYPE_MINED_FROM,
 	TEXT_FIELD_TITLE,
 	REFERENCE_TYPE_LINK,
-	CARD_TYPE_CONTENT
 } from '../card_field_constants.js';
 
 import {
@@ -179,7 +177,6 @@ import {
 	UserInfo,
 	SectionID,
 	Slug,
-	ProcessedCard
 } from '../types.js';
 
 
@@ -690,53 +687,6 @@ export const defaultCardObject = (id : CardID, user : UserInfo, section : Sectio
 		tweet_count: 0
 	};
 };
-
-export const EMPTY_CARD : Card = {
-	created: currentTimestamp(),
-	updated: currentTimestamp(),
-	author: '',
-	permissions: {
-		[PERMISSION_EDIT_CARD]: [],
-	},
-	collaborators: [],
-	updated_substantive: currentTimestamp(),
-	updated_message: currentTimestamp(),
-	star_count: 0,
-	star_count_manual: 0,
-	tweet_favorite_count: 0,
-	tweet_retweet_count: 0,
-	thread_count: 0,
-	thread_resolved_count: 0,
-	sort_order: Number.MAX_SAFE_INTEGER / 2,
-	title: '',
-	section: '',
-	body: '',
-	[REFERENCES_INFO_CARD_PROPERTY]: {},
-	[REFERENCES_INFO_INBOUND_CARD_PROPERTY]: {},
-	[REFERENCES_CARD_PROPERTY]: {},
-	[REFERENCES_INBOUND_CARD_PROPERTY]: {},
-	font_size_boost: {},
-	card_type: CARD_TYPE_CONTENT,
-	notes: '',
-	todo: '',
-	slugs: [],
-	name: '',
-	tags: [],
-	id: 'INVALID-CARD',
-	published: false,
-	images: [],
-	auto_todo_overrides: {},
-	last_tweeted: currentTimestamp(),
-	tweet_count: 0
-};
-
-export const EMPTY_PROCESSED_CARD : ProcessedCard = {
-	...EMPTY_CARD,
-	fallbackText: {},
-    importantNgrams: {},
-    synonymMap: {},
-    nlp: {}
-}
 
 //createCard creates an inserts a new card. see also createWorkingNotesCard
 //which is similar but simpler.

src/card_fields.ts
@@ -6,10 +6,6 @@ import {
 import * as CONSTANTS from './card_field_constants.js';
 
 import {
-	EMPTY_PROCESSED_CARD
-} from './actions/data.js';
-
-import {
 	Card,
 	Cards,
 	ProcessedCard,
@@ -30,6 +26,55 @@ import {
 import {
 	CardRenderer
 } from './components/card-renderer.js';
+
+import {
+	Timestamp
+} from 'firebase/firestore';
+
+export const EMPTY_CARD : Card = {
+	created: Timestamp.now(),
+	updated: Timestamp.now(),
+	author: '',
+	permissions: {},
+	collaborators: [],
+	updated_substantive: Timestamp.now(),
+	updated_message: Timestamp.now(),
+	star_count: 0,
+	star_count_manual: 0,
+	tweet_favorite_count: 0,
+	tweet_retweet_count: 0,
+	thread_count: 0,
+	thread_resolved_count: 0,
+	sort_order: Number.MAX_SAFE_INTEGER / 2,
+	title: '',
+	section: '',
+	body: '',
+	[CONSTANTS.REFERENCES_INFO_CARD_PROPERTY]: {},
+	[CONSTANTS.REFERENCES_INFO_INBOUND_CARD_PROPERTY]: {},
+	[CONSTANTS.REFERENCES_CARD_PROPERTY]: {},
+	[CONSTANTS.REFERENCES_INBOUND_CARD_PROPERTY]: {},
+	font_size_boost: {},
+	card_type: CONSTANTS.CARD_TYPE_CONTENT,
+	notes: '',
+	todo: '',
+	slugs: [],
+	name: '',
+	tags: [],
+	id: 'INVALID-CARD',
+	published: false,
+	images: [],
+	auto_todo_overrides: {},
+	last_tweeted: Timestamp.now(),
+	tweet_count: 0
+};
+
+export const EMPTY_PROCESSED_CARD : ProcessedCard = {
+	...EMPTY_CARD,
+	fallbackText: {},
+    importantNgrams: {},
+    synonymMap: {},
+    nlp: {}
+}
 
 /*
 

src/components/card-images-editor.ts
@@ -11,7 +11,8 @@ import {
 } from '../selectors.js';
 
 import {
-	IMAGE_CARD_TYPES
+	IMAGE_CARD_TYPES,
+	EMPTY_CARD
 } from '../card_fields.js';
 
 import {
@@ -32,7 +33,6 @@ import {
 	Card,
 	State
 } from '../types.js';
-import { EMPTY_CARD } from '../actions/data.js';
 
 @customElement('card-images-editor')
 class CardImagesEditor extends connect(store)(LitElement) {

src/components/card-renderer.ts
@@ -31,7 +31,8 @@ import {
 	TEXT_FIELD_CONFIGURATION,
 	CARD_TYPE_CONFIGURATION,
 	editableFieldsForCardType,
-	IMAGES_TEXT_FIELD
+	IMAGES_TEXT_FIELD,
+	EMPTY_PROCESSED_CARD
 } from '../card_fields.js';
 
 import {
@@ -67,10 +68,6 @@ import {
 import {
 	ExpandedReferenceBlocks
 } from '../reference_blocks.js';
-
-import {
-	EMPTY_PROCESSED_CARD
-} from '../actions/data.js';
 
 export const CARD_WIDTH_IN_EMS = 43.63;
 export const CARD_HEIGHT_IN_EMS = 24.54;

src/reducers/app.ts
@@ -30,7 +30,7 @@ import {
 
 import {
 	EMPTY_CARD
-} from '../actions/data.js';
+} from '../card_fields.js';
 
 import {
 	AppState,