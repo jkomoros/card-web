jkomoros/card-web

commit 0a328071163e7b7ef8cd6d9259429d89dbf8d5cd

Add type info so makeCardsConfigurableFilter is allowed in filters map.
Part of #601.

src/filters.ts
@@ -304,17 +304,17 @@ const makeCardLinksConfigurableFilter = (filterName : string, cardID : string, c
 export const parseMultipleCardIDs = (str) => str.split(INCLUDE_KEY_CARD_PREFIX);
 export const combineMultipleCardIDs = (cardIDs) => cardIDs.join(INCLUDE_KEY_CARD_PREFIX);
 
-const makeCardsConfigurableFilter = (filterName, idString) => {
+const makeCardsConfigurableFilter = (_, idString : string) : ConfigurableFilterFuncFactoryResult => {
 	//ids can be a single id or slug, or a conjunction of them delimited by '+'
 	const rawIdsToMatch = Object.fromEntries(parseMultipleCardIDs(idString).map(id => [id, true]));
 
 	//TODO: we could check if KEY_CARD_ID_PLACEHOLDER is in any of them, and if not
 	//never generate a new set of expanded ids to match to save a little
 	//performance.
-	const generator = memoize(keyCardID => Object.fromEntries(Object.entries(rawIdsToMatch).map(entry => [entry[0], entry[1] == KEY_CARD_ID_PLACEHOLDER ? keyCardID : entry[1]])));
+	const generator = memoize((keyCardID : CardID) => Object.fromEntries(Object.entries(rawIdsToMatch).map(entry => [entry[0], entry[1] == KEY_CARD_ID_PLACEHOLDER ? keyCardID : entry[1]])));
 
 	//TODO: only calculate the slug --> id once so subsequent matches can be done with one lookup
-	const func = function(card, extras) {
+	const func = function(card : ProcessedCard, extras : FilterExtras) {
 		const idsToMatch = generator(extras.keyCardID);
 		if (idsToMatch[card.id]) return true;
 		if (!card.slugs) return false;

commit de9df6e2d0ff86b6bf64ffaa4dce79a33670ff48

Fix bug, introduced in 0a328071163e7b7ef8cd6d9259429d89dbf8d5cd, where all query searches show up as partial.
The return type of filterFunc erroneously had a string as last item.

Part of #601.

src/filters.ts
@@ -590,11 +590,11 @@ const makeQueryConfigurableFilter = (filterName : string, rawQueryString : strin
 
 	const strict = filterName === QUERY_STRICT_FILTER_NAME;
 
-	const func = function(card : ProcessedCard) : [boolean, number, string] {
+	const func = function(card : ProcessedCard) : [boolean, number, boolean] {
 		const [score, fullMatch] = query.cardScore(card);
 		const matches = strict ? fullMatch && score > 0.0 : score > 0.0;
 		//TODO: is returning a boolean for last argument intentional?
-		return [matches, score, '' + !fullMatch];
+		return [matches, score, !fullMatch];
 	};
 
 	return [func, false];

src/types.ts
@@ -261,7 +261,7 @@ export type SortConfigurationMap = {
     }
 }
 
-export type ConfigurableFilterFunc = (card : ProcessedCard, extras? : FilterExtras) => ([matches : boolean] | [ matches : boolean, sortExtra : number] | [ matches : boolean, sortExtra : number, label : string]);
+export type ConfigurableFilterFunc = (card : ProcessedCard, extras? : FilterExtras) => ([matches : boolean] | [ matches : boolean, sortExtra : number] | [ matches : boolean, sortExtra : number, partialMatch : boolean]);
 
 export type ConfigurableFilterFuncFactoryResult = [func : ConfigurableFilterFunc, reverse : boolean];
 