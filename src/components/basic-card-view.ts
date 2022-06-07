jkomoros/card-web

commit 296fb21b058e55d499207c02a6df960228b1f78b

Resolve remaining type issues in basic-card-view by asserting types from card-stage.
Part of #601.

src/components/basic-card-view.ts
@@ -25,7 +25,10 @@ import {
 	updateCards,
 } from '../actions/data.js';
 
-import './card-stage.js';
+//We also import this implicitly for the custom element to be loaded.
+import {
+	CardStage
+} from './card-stage.js';
 
 import {
 	Card,
@@ -102,7 +105,8 @@ class BasicCardView extends connect(store)(PageViewElement) {
 			//Screenshot service will check for this to know when to take a
 			//screenshot.
 			this.updateComplete.then(() => {
-				this.shadowRoot.querySelector('card-stage').mainCardRenderer.imagesLoaded().then(() => {
+				const cardStage : CardStage = this.shadowRoot.querySelector('card-stage') as CardStage;
+				cardStage.mainCardRenderer.imagesLoaded().then(() => {
 					window[WINDOW_CARD_RENDERED_VARIABLE] = true;
 				});
 			});

src/components/card-stage.ts
@@ -13,7 +13,8 @@ import {
 	CARD_WIDTH_IN_EMS,
 	CARD_HORIZONTAL_PADDING_IN_EMS,
 	CARD_HEIGHT_IN_EMS,
-	CARD_VERTICAL_PADDING_IN_EMS
+	CARD_VERTICAL_PADDING_IN_EMS,
+	CardRenderer
 } from './card-renderer.js';
 
 import {
@@ -31,7 +32,7 @@ import {
 } from '../reference_blocks.js';
 
 @customElement('card-stage')
-class CardStage extends LitElement {
+export class CardStage extends LitElement {
 
 	@property({ type : Boolean })
 	mobile: boolean;
@@ -256,12 +257,12 @@ class CardStage extends LitElement {
 		renderer.style.fontSize = '' + fontSize + 'px';
 	}
 
-	get mainCardRenderer() {
-		return this.shadowRoot.querySelector('#main');
+	get mainCardRenderer() : CardRenderer {
+		return this.shadowRoot.querySelector('#main') as CardRenderer;
 	}
 
-	get sizingCardRenderer() {
-		return this.shadowRoot.querySelector('#sizing');
+	get sizingCardRenderer() : CardRenderer {
+		return this.shadowRoot.querySelector('#sizing') as CardRenderer;
 	}
 
 	override firstUpdated() {

commit 22f18d50e11f57e94da23f14d554f468e2b3bf6a

Fix basic-card viewer, which was broken in 296fb21b058e55d499207c02a6df960228b1f78b.
We needed a card-stage side-effect import as well as the named import.

Part of #601.

src/components/basic-card-view.ts
@@ -25,7 +25,8 @@ import {
 	updateCards,
 } from '../actions/data.js';
 
-//We also import this implicitly for the custom element to be loaded.
+//We need the implicit import AND the specific class CardStage.
+import './card-stage.js';
 import {
 	CardStage
 } from './card-stage.js';