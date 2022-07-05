jkomoros/card-web

commit 64cdda72fa475fbb460fbff9ed460b855033bece

Fix an error where we called execCommand with an erroneous argument.
Part of #601.

src/util.ts
@@ -585,7 +585,7 @@ export const makeElementContentEditable = (ele : HTMLElement) => {
 
 	//styleWithCSS turns off styling spans with CSS and just uses presentational
 	//attributes. 
-	document.execCommand('styleWithCSS', false, false);
+	document.execCommand('styleWithCSS', false);
 	//Browsers currently insert a "<div>" as default paragraph separator but we
 	//want 'p'; 
 	document.execCommand('defaultParagraphSeparator', false, 'p');

commit 25db35cbd77f725cee06f048befb934f08068080

Fix it so styling bold/italic in contenteditable works again.
Was broken in 64cdda7. execCommand('styleWithCSS', false, false) is not allowed by typescript (it wants the last argument to be a string). The only value string that actually equates to false is 'false', fun fact, not '' as you'd expect.

Fixes #612.

src/util.ts
@@ -588,7 +588,7 @@ export const makeElementContentEditable = (ele : HTMLElement) => {
 
 	//styleWithCSS turns off styling spans with CSS and just uses presentational
 	//attributes. 
-	document.execCommand('styleWithCSS', false);
+	document.execCommand('styleWithCSS', false, 'false');
 	//Browsers currently insert a "<div>" as default paragraph separator but we
 	//want 'p'; 
 	document.execCommand('defaultParagraphSeparator', false, 'p');