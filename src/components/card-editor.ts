13bb600295c6e10a94c8ce33213eea74f3f97f7e
Alex Komoroske <jkomoros@gmail.com>
2022-07-23 17:56:55 -0700

    Make it so 'Add enabled...' is 'Add TODO...' in the minimized editor.
    Part of #616.

--- src/components/card-editor.ts
+++ src/components/card-editor.ts
@@ -575,7 +575,7 @@ class CardEditor extends connect(store)(LitElement) {
 			<div class='flex'></div>
 			<div class='tags'>
 				<tag-list .defaultColor=${autoTODOColor} .tags=${this._autoTodos} .overrideTypeName=${'Auto TODO'} .tagInfos=${TODO_ALL_INFOS} .hideOnEmpty=${true} .disableAdd=${true} .editing=${true} @tag-removed=${this._handleAddTodoOverrideDisabled}></tag-list>
-				<tag-list .defaultColor=${enableTODOColor} .tags=${todoOverridesEnabled} .previousTags=${todoOverridesPreviouslyEnabled} .disableNew=${true} .overrideTypeName=${'Enabled'} .editing=${true} .tagInfos=${TODO_AUTO_INFOS} @tag-added=${this._handleAddTodoOverrideEnabled} @tag-removed=${this._handleRemoveTodoOverride} .hideMessageOnEmpty=${true}></tag-list>
+				<tag-list .defaultColor=${enableTODOColor} .tags=${todoOverridesEnabled} .previousTags=${todoOverridesPreviouslyEnabled} .disableNew=${true} .overrideTypeName=${'TODO'} .editing=${true} .tagInfos=${TODO_AUTO_INFOS} @tag-added=${this._handleAddTodoOverrideEnabled} @tag-removed=${this._handleRemoveTodoOverride} .hideMessageOnEmpty=${true}></tag-list>
 			</div>
 			<div class='flex'></div>
 			<div class='tags'>
