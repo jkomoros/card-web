3486119318083064be9a09cda4168ad8bc2c23cc
Alex Komoroske <jkomoros@gmail.com>
2023-05-29 06:30:49 -0700

    openai callable checks that hte user has sufficient permissions to use the API.
    Part of #647

--- functions/openai.js
+++ functions/openai.js
@@ -4,16 +4,31 @@ const common = require('./common.js');
 const openaiConfig = common.config.openai || null;
 const openai_api_key = openaiConfig ? (openaiConfig.api_key || '') : '';
 
-const handler = async () => {
-    //TODO: check that request.uid.permissions is admin or api-callable and error if not
+const mayUseAI = (permissions) => {
+    if (!permissions) return false;
+    if (permissions.admin) return true;
+    //TODO: also allow it if the ai permission is true.
+    return false;
+}
+
+const handler = async (_, context) => {
 
     if (!openai_api_key) {
         throw new functions.https.HttpsError('failed-precondition', 'OPENAI_API_KEY not set');
     }
 
+    if (!context.auth) {
+        throw new functions.https.HttpsError('unauthenticated', 'A valid user authentication must be passed');
+    }
+
+    const permissions = await common.userPermissions(context.auth.uid);
+
+    if (!mayUseAI(permissions)) {
+        throw new functions.https.HttpsError('permission-denied', 'The user does not have adequate permissions to perrform this action');
+    }
+
     //TODO: Take the provided openai API request, inject the secret key, and proxy results.
     
-    //Must be one of the error codes here: https://firebase.google.com/docs/reference/functions/firebase-functions.https.md#httpsfunctionserrorcode
     throw new functions.https.HttpsError('unimplemented', 'Not yet implemented');
 };
 
