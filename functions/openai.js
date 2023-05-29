b654026acabe607f83a8025c525671016d3cdca4
Alex Komoroske <jkomoros@gmail.com>
2023-05-29 06:08:46 -0700

    Fix to be a supported error code on openai endpoint.
    Part of #647.

--- functions/openai.js
+++ functions/openai.js
@@ -6,7 +6,9 @@ const handler = async () => {
     //TODO: Take the provided openai API request, inject the secret key, and proxy results.
 
     //TODO: this doesn't actually get through to the client yet.
-    throw new functions.https.HttpsError('not-implemented', 'Not yet implemented');
+    
+    //Must be one of the error codes here: https://firebase.google.com/docs/reference/functions/firebase-functions.https.md#httpsfunctionserrorcode
+    throw new functions.https.HttpsError('unimplemented', 'Not yet implemented');
 };
 
 exports.handler = handler;
