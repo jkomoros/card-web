c0e301166c5ea93476d118d96d2fe3666898dcc1
Alex Komoroske <jkomoros@gmail.com>
2023-05-28 19:23:53 -0700

    Updated the calling of the endpoint to actually call the error.
    Still not received on the client.
    
    Part of #647.

--- functions/openai.js
+++ functions/openai.js
@@ -1,7 +1,10 @@
 const functions = require('firebase-functions');
 
 const handler = async () => {
-    functions.https.HttpsError('not-implemented', 'Not yet implemented');
+    //TODO: check that request.uid.permissions is admin or api-callable and error if not
+    //TODO: error if no openai key is set
+    //TODO: Take the provided openai API request, inject the secret key, and proxy results.
+    throw new functions.https.HttpsError('not-implemented', 'Not yet implemented');
 };
 
 exports.handler = handler;
