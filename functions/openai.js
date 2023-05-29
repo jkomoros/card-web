259dd9521c0d1acbebab313ca3f8993e26ef7aa2
Alex Komoroske <jkomoros@gmail.com>
2023-05-28 19:26:47 -0700

    Update a TODO based on what's currently working.
    Part of #647.

--- functions/openai.js
+++ functions/openai.js
@@ -4,6 +4,8 @@ const handler = async () => {
     //TODO: check that request.uid.permissions is admin or api-callable and error if not
     //TODO: error if no openai key is set
     //TODO: Take the provided openai API request, inject the secret key, and proxy results.
+
+    //TODO: this doesn't actually get through to the client yet.
     throw new functions.https.HttpsError('not-implemented', 'Not yet implemented');
 };
 
