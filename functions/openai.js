df5e27dd94167d36dd5f26b206e8b32e522806b1
Alex Komoroske <jkomoros@gmail.com>
2023-05-29 06:13:56 -0700

    openai endpoint checks for api key being set.
    Part of #647.

--- functions/openai.js
+++ functions/openai.js
@@ -1,11 +1,17 @@
 const functions = require('firebase-functions');
+const common = require('./common.js');
+
+const openaiConfig = common.config.openai || null;
+const openai_api_key = openaiConfig ? (openaiConfig.api_key || '') : '';
 
 const handler = async () => {
     //TODO: check that request.uid.permissions is admin or api-callable and error if not
-    //TODO: error if no openai key is set
-    //TODO: Take the provided openai API request, inject the secret key, and proxy results.
 
-    //TODO: this doesn't actually get through to the client yet.
+    if (!openai_api_key) {
+        throw new functions.https.HttpsError('failed-precondition', 'OPENAI_API_KEY not set');
+    }
+
+    //TODO: Take the provided openai API request, inject the secret key, and proxy results.
     
     //Must be one of the error codes here: https://firebase.google.com/docs/reference/functions/firebase-functions.https.md#httpsfunctionserrorcode
     throw new functions.https.HttpsError('unimplemented', 'Not yet implemented');
