104299994950d4326004c9bdad7b3e8ffbc29d8a
Alex Komoroske <jkomoros@gmail.com>
2023-06-04 16:56:51 -0400

    Factor out getFirebaseConfig into util.ts
    Part of #663.

--- tools/seo.ts
+++ tools/seo.ts
@@ -2,11 +2,9 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as process from 'process';
 import striptags from 'striptags';
-import { exec } from 'child_process';
 
 import {
-	Config,
-	FirebaseProdDevOptions
+	Config
 } from './types.js';
 
 import {
@@ -32,7 +30,8 @@ import {
 } from '../src/type_constants.js';
 
 import {
-	getProjectConfig
+	getProjectConfig,
+	getFirebaseConfig
 } from './util.js';
 
 const SEO_PATH = 'seo/';
@@ -55,44 +54,6 @@ type FirebaseConfig = {
 }
 
 const log = (msg : string) => console.log(msg);
-
-const runCommand = async (command : string) : Promise<string> => {
-	return new Promise((resolve, reject) => {
-		exec(command, (error, stdout) => {
-			if (error) {
-				reject(error);
-				return;
-			}
-			resolve(stdout.trim());
-		});
-	});
-};
-
-const selectedProjectID = async () : Promise<string> => {
-	//Oddly enough firebase use I guess does something special for stderr and
-	//stdout, because this just returns the direct project ID.
-	const result = await runCommand('firebase use');
-	return result;
-};
-
-const getFirebaseConfig = async (config : Config) : Promise<FirebaseOptions> => {
-	if (!config.firebase) throw new Error('No firebase property');
-	if ('apiKey' in config.firebase) {
-		return config.firebase;
-	}
-	const fb = config.firebase as FirebaseProdDevOptions;
-	const prod = fb.prod;
-	const dev = fb.dev;
-	//We need to detect whether to use the prod or dev config.
-	if (!prod && dev) return dev;
-	if (!dev && prod) return prod;
-	if (!dev && !prod) throw new Error('No sub configs provided');
-	//We have both prod and dev and need to select which one to use.
-	const projectID = await selectedProjectID();
-	if (prod?.projectId == projectID) return prod;
-	if (dev?.projectId == projectID) return dev;
-	throw new Error(`Neither prod nor dev options matched projectid ${projectID}`);
-};
 
 const fetchCards = async (config : FirebaseOptions) : Promise<Card[]> => {
 	// Initialize Firebase
--- tools/util.ts
+++ tools/util.ts
@@ -4,10 +4,48 @@ import {
 } from './types.js';
 
 import fs from 'fs';
+import { exec } from 'child_process';
+
+import {
+	FirebaseOptions
+} from 'firebase/app';
 
 const PROJECT_CONFIG = 'config.SECRET.json';
 
+const runCommand = async (command : string) : Promise<string> => {
+	return new Promise((resolve, reject) => {
+		exec(command, (error, stdout) => {
+			if (error) {
+				reject(error);
+				return;
+			}
+			resolve(stdout.trim());
+		});
+	});
+};
+
+const selectedProjectID = async () : Promise<string> => {
+	//Oddly enough firebase use I guess does something special for stderr and
+	//stdout, because this just returns the direct project ID.
+	const result = await runCommand('firebase use');
+	return result;
+};
+
+export const getFirebaseConfig = async (config : Config) : Promise<FirebaseOptions> => {
+
+	const {prod, dev} = devProdFirebaseConfig(config);
+
+	//We have both prod and dev and need to select which one to use.
+	const projectID = await selectedProjectID();
+	if (prod?.projectId == projectID) return prod;
+	if (dev?.projectId == projectID) return dev;
+	throw new Error(`Neither prod nor dev options matched projectid ${projectID}`);
+};
+
 export const devProdFirebaseConfig = (config : Config) : Required<FirebaseProdDevOptions> => {
+	if (!config.firebase) {
+		throw new Error('No firebase property');
+	}
 	if ('apiKey' in config.firebase) {
 		return {prod: config.firebase, dev: config.firebase};
 	}
@@ -15,6 +53,7 @@ export const devProdFirebaseConfig = (config : Config) : Required<FirebaseProdDe
 	if (!fb.prod) throw new Error('No prod');
 	const prod = fb.prod;
 	const dev = fb.dev || prod;
+	if (!prod && !dev) throw new Error('No sub configs provided');
 	return {prod, dev};
 };
 
