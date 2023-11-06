77cd52cfc62c7b8bd3fc29f1121b8ada00679625
Alex Komoroske <jkomoros@gmail.com>
2023-11-05 16:08:13 -0800

    Clean up imports.
    Part of #646.

--- functions/src/openai.ts
+++ functions/src/openai.ts
@@ -1,5 +1,3 @@
-import * as functions from 'firebase-functions';
-
 import {
 	Configuration,
 	OpenAIApi
@@ -15,7 +13,8 @@ import {
 } from './types.js';
 
 import {
-	CallableRequest
+	CallableRequest,
+	HttpsError
 } from 'firebase-functions/v2/https';
 
 export const openai_endpoint = OPENAI_API_KEY ? new OpenAIApi(new Configuration({
@@ -47,33 +46,33 @@ export const handler = async (request : CallableRequest<OpenAIData>) => {
 	const data = request.data;
 
 	if (!openai_endpoint) {
-		throw new functions.https.HttpsError('failed-precondition', 'OPENAI_API_KEY not set');
+		throw new HttpsError('failed-precondition', 'OPENAI_API_KEY not set');
 	}
 
 	if (!request.auth) {
-		throw new functions.https.HttpsError('unauthenticated', 'A valid user authentication must be passed');
+		throw new HttpsError('unauthenticated', 'A valid user authentication must be passed');
 	}
 
 	const permissions = await userPermissions(request.auth.uid);
 
 	if (!mayUseAI(permissions)) {
-		throw new functions.https.HttpsError('permission-denied', 'The user does not have adequate permissions to perrform this action');
+		throw new HttpsError('permission-denied', 'The user does not have adequate permissions to perrform this action');
 	}
 
 	if (!data || typeof data !== 'object') {
-		throw new functions.https.HttpsError('invalid-argument', 'data must be an object');
+		throw new HttpsError('invalid-argument', 'data must be an object');
 	}
 
 	if (!data.endpoint) {
-		throw new functions.https.HttpsError('invalid-argument', 'endpoint must be provided');
+		throw new HttpsError('invalid-argument', 'endpoint must be provided');
 	}
 
 	if (!data.payload) {
-		throw new functions.https.HttpsError('invalid-argument', 'payload must be provided');
+		throw new HttpsError('invalid-argument', 'payload must be provided');
 	}
 
 	if (!ALLOWED_ENDPOINTS[data.endpoint]) {
-		throw new functions.https.HttpsError('invalid-argument', 'endpoint must be set to an allowed endpoint type');
+		throw new HttpsError('invalid-argument', 'endpoint must be set to an allowed endpoint type');
 	}
 
 	try {
@@ -81,16 +80,16 @@ export const handler = async (request : CallableRequest<OpenAIData>) => {
 		const result = await openai_endpoint[data.endpoint](data.payload as any);
 		return result.data;
 	} catch(err) {
-		if (!err || typeof err != 'object') throw new functions.https.HttpsError('unknown', String(err));
+		if (!err || typeof err != 'object') throw new HttpsError('unknown', String(err));
 		//err is either an err.response.statusText/status or err.message
 		if ('response' in err) {
 			const response = err.response as {statusText: string, status: number};
-			throw new functions.https.HttpsError('unknown', response.statusText, {
+			throw new HttpsError('unknown', response.statusText, {
 				status: response.status,
 				statusText: response.statusText
 			});
 		}
 		const errAsError = err as Error;
-		throw new functions.https.HttpsError('unknown', errAsError.message);
+		throw new HttpsError('unknown', errAsError.message);
 	}
 };
