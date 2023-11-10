f3e8fe44a9f04a869c10b8c27ad5ca48cbbec485
Alex Komoroske <jkomoros@gmail.com>
2023-11-09 19:39:16 -0700

    Start roughing in a Suggestion type.
    Part of #670.

---
+++ src/suggestions.ts
@@ -0,0 +1,29 @@
+import {
+	CardDiff
+} from './types.js';
+
+import {
+	CardID
+} from './types_simple.js';
+
+type SuggestionDiff = {
+	keyCard: CardDiff,
+	//The diff to apply to each supportingCard.
+	supportingCards?: CardDiff
+};
+
+type SuggestionType = 'noop';
+
+export type Suggestion = {
+	type: SuggestionType,
+	keyCard: CardID,
+	supportingCards: CardID[],
+	//TODO: add contextCards
+
+	//The diff to apply if the action is accepted
+	action: SuggestionDiff,
+	//An alternate action. Often the mirror of the primary.
+	alternateAction?: SuggestionDiff
+	//The diff to apply if the action is rejected. Typically an `ack` reference.
+	rejection?: SuggestionDiff
+};
