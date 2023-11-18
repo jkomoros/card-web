jkomoros/card-web

commit b0f01125b1ed259f35a21815b8745b856ac9cd55

A few strictNullCheck fixkes.
Part of #601.

src/nlp.ts
@@ -46,7 +46,8 @@ import {
 	StringCardMap,
 	ReferencesInfoMap,
 	FilterMap,
-	SortExtra
+	SortExtra,
+	cardFieldTypeEditableSchema
 } from './types.js';
 import { innerTextForHTML } from './util.js';
 
@@ -541,14 +542,14 @@ const extractRawContentRunsForCardField = (card : Card, fieldName : CardFieldTyp
 	const cardType = card.card_type;
 	const config = TEXT_FIELD_CONFIGURATION[fieldName];
 	if ((DERIVED_FIELDS_FOR_CARD_TYPE[cardType] || {})[fieldName]) return [];
-	const safeFieldName = fieldName as CardFieldTypeEditable;
+	const safeFieldName = cardFieldTypeEditableSchema.parse(fieldName);
 	let fieldValue = '';
 	if (config.overrideExtractor) {
 		const extractor = OVERRIDE_EXTRACTORS[safeFieldName];
 		if (!extractor) throw new Error(safeFieldName + ' had overrideExtractor set, but no entry in OVERRIDE_EXTRACTORS');
 		fieldValue = extractor(card);
 	} else {
-		fieldValue = extractFieldValueForIndexing(card[safeFieldName]);
+		fieldValue = extractFieldValueForIndexing(card[safeFieldName] || '');
 	}
 	if (!fieldValue) fieldValue = '';
 	//If the text is the defaultBody for that card type, just pretend

commit acb1c9e29e28acb8291e40411777febffb870300

Fix a bug broken in b0f01125b1ed259f35a21815b8745b856ac9cd55
We were using the wrong type (the code technically before the change was wrong) that was leading to a
runtime zod error.

Relaxed the typing and tightened another type/

Part of #601.

src/nlp.ts
@@ -47,7 +47,7 @@ import {
 	ReferencesInfoMap,
 	FilterMap,
 	SortExtra,
-	cardFieldTypeEditableSchema
+	cardFieldTypeSchema
 } from './types.js';
 import { innerTextForHTML } from './util.js';
 
@@ -542,14 +542,17 @@ const extractRawContentRunsForCardField = (card : Card, fieldName : CardFieldTyp
 	const cardType = card.card_type;
 	const config = TEXT_FIELD_CONFIGURATION[fieldName];
 	if ((DERIVED_FIELDS_FOR_CARD_TYPE[cardType] || {})[fieldName]) return [];
-	const safeFieldName = cardFieldTypeEditableSchema.parse(fieldName);
+	const safeFieldName = cardFieldTypeSchema.parse(fieldName);
 	let fieldValue = '';
 	if (config.overrideExtractor) {
 		const extractor = OVERRIDE_EXTRACTORS[safeFieldName];
 		if (!extractor) throw new Error(safeFieldName + ' had overrideExtractor set, but no entry in OVERRIDE_EXTRACTORS');
 		fieldValue = extractor(card);
 	} else {
-		fieldValue = extractFieldValueForIndexing(card[safeFieldName] || '');
+		//We know that safeFieldName exists on card at this point, because the
+		//ones that don't all have overrideExtractor: true.
+		//eslint-disable-next-line @typescript-eslint/no-explicit-any
+		fieldValue = extractFieldValueForIndexing((card as any)[safeFieldName] || '');
 	}
 	if (!fieldValue) fieldValue = '';
 	//If the text is the defaultBody for that card type, just pretend

src/types.ts
@@ -66,7 +66,7 @@ export type CardFieldTypeEditable = z.infer<typeof cardFieldTypeEditableSchema>;
 
 export type CardFieldTypeNonEditable = z.infer<typeof cardFieldTypeNonEditableSchema>;
 
-const cardFieldTypeSchema = z.union([
+export const cardFieldTypeSchema = z.union([
 	cardFieldTypeEditableSchema,
 	cardFieldTypeNonEditableSchema
 ]);