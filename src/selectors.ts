jkomoros/card-web

commit c3eaf9018a833fbc46ac93086d8d09e4c648b920

Fix a strictNullChecks in config.GENERATED.SECRET.ts
Re-run `npm run generate:config`.

Part of #601.

src/selectors.ts
@@ -1201,7 +1201,7 @@ export const selectUserMayAddCardToActiveCollection = createSelector(
 export const selectExpandedTabConfig = createSelector(
 	selectSections,
 	selectTags,
-	(sections, tags) => tabConfiguration(TAB_CONFIGURATION, TAB_OVERRIDES_CONFIGURATION, sections, tags)
+	(sections, tags) => tabConfiguration(TAB_CONFIGURATION || [], TAB_OVERRIDES_CONFIGURATION, sections, tags)
 );
 
 //The CollectionDescription to load up if not provided one

tools/config.ts
@@ -50,7 +50,7 @@ const generateConfig = () => {
 	CONFIG_JS_CONTENT += 'export const USER_DOMAIN = \'' + USER_DOMAIN + '\';\n';
 	CONFIG_JS_CONTENT += 'export const FIREBASE_REGION = \'' + FIREBASE_REGION + '\';\n';
 	CONFIG_JS_CONTENT += 'export const TWITTER_HANDLE = \'' + TWITTER_HANDLE + '\';\n';
-	CONFIG_JS_CONTENT += 'export const TAB_CONFIGURATION : TabConfigInput = ' + JSON.stringify(TAB_CONFIGURATION).split('"').join('\'') + ';\n';
+	CONFIG_JS_CONTENT += 'export const TAB_CONFIGURATION : TabConfigInput | null = ' + JSON.stringify(TAB_CONFIGURATION).split('"').join('\'') + ';\n';
 	CONFIG_JS_CONTENT += 'export const TAB_OVERRIDES_CONFIGURATION : TabConfigOverrides = ' + JSON.stringify(TAB_OVERRIDES_CONFIGURATION).split('"').join('\'') + ';\n';
 	CONFIG_JS_CONTENT += 'export const DISABLE_PERSISTENCE = ' + (DISABLE_PERSISTENCE ? 'true' : 'false') + ';\n';
 	CONFIG_JS_CONTENT += 'export const DISABLE_ANONYMOUS_LOGIN = ' + (DISABLE_ANONYMOUS_LOGIN ? 'true' : 'false') + ';\n';

commit e930c793b5a6823fea4f6aa99e93c794f9f40220

Fix a bug where tabs wouldn't show.
Broken in c3eaf9018a833fbc46ac93086d8d09e4c648b920

Part of #601.

src/selectors.ts
@@ -1201,7 +1201,7 @@ export const selectUserMayAddCardToActiveCollection = createSelector(
 export const selectExpandedTabConfig = createSelector(
 	selectSections,
 	selectTags,
-	(sections, tags) => tabConfiguration(TAB_CONFIGURATION || [], TAB_OVERRIDES_CONFIGURATION, sections, tags)
+	(sections, tags) => tabConfiguration(TAB_CONFIGURATION, TAB_OVERRIDES_CONFIGURATION, sections, tags)
 );
 
 //The CollectionDescription to load up if not provided one

src/tabs.ts
@@ -37,7 +37,7 @@ export const STARS_FALLBACK_CARD = 'about-stars';
 
 const DEFAULT_OVERRIDES : TabConfigOverrides = {before: {}, after: {}};
 
-export const tabConfiguration = (config : TabConfig, overrides : TabConfigOverrides, sections : Sections, tags : Sections) : ExpandedTabConfig => {
+export const tabConfiguration = (config : TabConfig | null, overrides : TabConfigOverrides, sections : Sections, tags : Sections) : ExpandedTabConfig => {
 	if (!config) config = DEFAULT_CONFIG;
 	if (!overrides) overrides = DEFAULT_OVERRIDES;
 	let array = config;