jkomoros/card-web

commit 6e0c8759a5a5d410ad676c8e9ff87335b946529b

Fix a coupel of strictNullCheck
Part of #601.

src/components/card-drawer.ts
@@ -155,6 +155,11 @@ class CardDrawer extends LitElement {
 	];
 
 	override render() {
+
+		const cardTypeToAddConfiguration = CARD_TYPE_CONFIGURATION[this.cardTypeToAdd];
+
+		if (!cardTypeToAddConfiguration) throw new Error('no cardTypeToAdd');
+
 		return html`
 			<div ?hidden='${!this.showing}' class='container ${this.reorderPending ? 'reordering':''} ${this.grid ? 'grid' : ''}'>
 				<div class='scrolling scroller'>
@@ -176,7 +181,7 @@ class CardDrawer extends LitElement {
 				</div>
 				<div class='buttons'>
 					<button class='round' @click='${this._handleCreateWorkingNotes}' ?hidden='${!this.showCreateWorkingNotes}' title="Create a new working notes card (Cmd-Shift-M)">${INSERT_DRIVE_FILE_ICON}</button>
-					<button class='round' @click='${this._handleAddSlide}' ?hidden='${!this.showCreateCard}' title=${'Add a new card of type ' + this.cardTypeToAdd + ' in this section (Cmd-M)'}>${!this.cardTypeToAdd || this.cardTypeToAdd == DEFAULT_CARD_TYPE || !CARD_TYPE_CONFIGURATION[this.cardTypeToAdd].iconName ? PLUS_ICON : icons[CARD_TYPE_CONFIGURATION[this.cardTypeToAdd].iconName] }</button>
+					<button class='round' @click='${this._handleAddSlide}' ?hidden='${!this.showCreateCard}' title=${'Add a new card of type ' + this.cardTypeToAdd + ' in this section (Cmd-M)'}>${!this.cardTypeToAdd || this.cardTypeToAdd == DEFAULT_CARD_TYPE || !cardTypeToAddConfiguration.iconName ? PLUS_ICON : icons[cardTypeToAddConfiguration.iconName] }</button>
 				</div>
 			</div>
 		`;

commit 24837bf8ec9bd88304f4f9e5c1f3572379a3e4d1

Fix a bug introduced in 6e0c8759a5a5d410ad676c8e9ff87335b946529b
An error on the console.

Part of #601.

src/components/card-drawer.ts
@@ -158,8 +158,6 @@ class CardDrawer extends LitElement {
 
 		const cardTypeToAddConfiguration = CARD_TYPE_CONFIGURATION[this.cardTypeToAdd];
 
-		if (!cardTypeToAddConfiguration) throw new Error('no cardTypeToAdd');
-
 		return html`
 			<div ?hidden='${!this.showing}' class='container ${this.reorderPending ? 'reordering':''} ${this.grid ? 'grid' : ''}'>
 				<div class='scrolling scroller'>
@@ -181,7 +179,7 @@ class CardDrawer extends LitElement {
 				</div>
 				<div class='buttons'>
 					<button class='round' @click='${this._handleCreateWorkingNotes}' ?hidden='${!this.showCreateWorkingNotes}' title="Create a new working notes card (Cmd-Shift-M)">${INSERT_DRIVE_FILE_ICON}</button>
-					<button class='round' @click='${this._handleAddSlide}' ?hidden='${!this.showCreateCard}' title=${'Add a new card of type ' + this.cardTypeToAdd + ' in this section (Cmd-M)'}>${!this.cardTypeToAdd || this.cardTypeToAdd == DEFAULT_CARD_TYPE || !cardTypeToAddConfiguration.iconName ? PLUS_ICON : icons[cardTypeToAddConfiguration.iconName] }</button>
+					<button class='round' @click='${this._handleAddSlide}' ?hidden='${!this.showCreateCard}' title=${'Add a new card of type ' + this.cardTypeToAdd + ' in this section (Cmd-M)'}>${!this.cardTypeToAdd || this.cardTypeToAdd == DEFAULT_CARD_TYPE || !cardTypeToAddConfiguration?.iconName ? PLUS_ICON : icons[cardTypeToAddConfiguration.iconName] }</button>
 				</div>
 			</div>
 		`;