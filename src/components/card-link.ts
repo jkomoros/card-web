jkomoros/card-web

commit 3977fe4358fd8be5f102896f3b2a56669a4853cc

A few more strictNullCheck
Part of #601.

src/components/card-link.ts
@@ -174,7 +174,7 @@ class CardLink extends connect(store)(LitElement) {
 	_handleMouseClick(e : MouseEvent) {
 		//If the user ctrl- or cmd-clicks, we should toggle reading list,
 		//otherwise we should return and allow default action.
-		if (!this.card) return;
+		if (!this.card || !this._cardObj) return;
 		if (this.noNavigate) {
 			e.preventDefault();
 			return;
@@ -211,7 +211,7 @@ class CardLink extends connect(store)(LitElement) {
 		if (!this._cardObj) return '';
 		const cardTypeConfig = CARD_TYPE_CONFIGURATION[this._cardObj.card_type];
 		if (!cardTypeConfig) return '';
-		return icons[cardTypeConfig.iconName] || '';
+		return icons[cardTypeConfig.iconName || 'WARNING_ICON'] || '';
 	}
 
 	get _inReadingList() {

commit ca47a1742679f88ca4551d756038d4d949ae54c4

Fix bug broken in 3977fe4358fd8be5f102896f3b2a56669a4853cc part of #601.
Card types without an icon type would have an alert icon.

Part of #601.

src/components/card-link.ts
@@ -211,7 +211,8 @@ class CardLink extends connect(store)(LitElement) {
 		if (!this._cardObj) return '';
 		const cardTypeConfig = CARD_TYPE_CONFIGURATION[this._cardObj.card_type];
 		if (!cardTypeConfig) return '';
-		return icons[cardTypeConfig.iconName || 'WARNING_ICON'] || '';
+		if (!cardTypeConfig.iconName) return '';
+		return icons[cardTypeConfig.iconName] || '';
 	}
 
 	get _inReadingList() {