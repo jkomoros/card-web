jkomoros/card-web

commit a37f904769f604d15512868f5fc41ccf21ec718d

Make it so if an item is selected in suggestions-summary we render all the other ones subtle.
This is better than a totally different color.

Part of #670.

src/components/suggestions-summary.ts
@@ -30,19 +30,13 @@ class SuggestionsSummary extends LitElement {
 	override render() {
 		if (!this.suggestions || this.suggestions.length == 0) return '';
 
-		let tagInfos = tagInfosForSuggestions(this.suggestions);
+		const tagInfos = tagInfosForSuggestions(this.suggestions);
+
+		let subtleTags : string[] = [];
 
 		if (this.selectedIndex !== undefined) {
 			const id = String(this.selectedIndex);
-			//Make a copy because tagInfosForSuggestions returns a memoized object
-			tagInfos = {
-				...tagInfos,
-				[id]: {
-					...tagInfos[id],
-					//navy
-					color: '#000080'
-				}
-			};
+			subtleTags = this.suggestions.map((_, index) => String(index)).filter(index => index != id);
 		}
 
 		const defaultColor = '#006400'; //darkgreen
@@ -56,6 +50,7 @@ class SuggestionsSummary extends LitElement {
 				.hideOnEmpty=${true}
 				.tapEvents=${true}
 				.tagInfos=${tagInfos}
+				.subtleTags=${subtleTags}
 			>
 			</tag-list>
 			`;

commit 091ade076bbf1b1fae99b4a2cbbedb41ec6207fc

Fix a bug where switching which item is selected owuldn't change view.
It was broken in a37f904769f604d15512868f5fc41ccf21ec718d. subtleTags is a dict, but we were passing an
array (why did Typescript let me??)

Part of #670.

src/components/suggestions-summary.ts
@@ -32,11 +32,11 @@ class SuggestionsSummary extends LitElement {
 
 		const tagInfos = tagInfosForSuggestions(this.suggestions);
 
-		let subtleTags : string[] = [];
+		let subtleTags : {[name : string] : true} = {};
 
 		if (this.selectedIndex !== undefined) {
 			const id = String(this.selectedIndex);
-			subtleTags = this.suggestions.map((_, index) => String(index)).filter(index => index != id);
+			subtleTags = Object.fromEntries(this.suggestions.map((_, index) => String(index)).filter(index => index != id).map(id => [id, true]));
 		}
 
 		const defaultColor = '#006400'; //darkgreen