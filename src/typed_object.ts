86d3d6599562fc8fb5964ad2272a2f3cca7c932c
Alex Komoroske <jkomoros@gmail.com>
2023-11-22 19:21:03 -0500

    Tweak implementatation of TypedObject ot resolve an issue.
    VSCode was giving a compile error but ts from CLI was not.
    
    ChatGPT suggested the change in https://chat.openai.com/share/21677137-55cd-4c59-8acf-b425ab8747cc
    
    Part of #670.

--- src/typed_object.ts
+++ src/typed_object.ts
@@ -6,15 +6,14 @@
 type Entries<T> = { [K in Extract<keyof T, string>]: [K, T[K]] }[Extract<keyof T, string>][];
 
 export class TypedObject {
-
-	//Based on https://stackoverflow.com/a/59459000
-	static keys<T extends object>(t : T): Array<keyof T> {
-		// eslint-disable-next-line @typescript-eslint/no-explicit-any
-		return Object.keys(t) as any;
+	// Ensures T is an object with string keys
+	//eslint-disable-next-line @typescript-eslint/no-explicit-any
+	static keys<T extends { [key: string]: any }>(obj: T): Array<keyof T> {
+		return Object.keys(obj) as Array<keyof T>;
 	}
 
-	//Based on https://stackoverflow.com/a/62055863 and https://chat.openai.com/c/83d8e6d6-8ac1-4301-9bc3-6c0eb4f2af36
-	static entries<T>(obj: T): Entries<T> {
+	//eslint-disable-next-line @typescript-eslint/no-explicit-any
+	static entries<T extends { [key: string]: any }>(obj: T): Entries<T> {
 		return Object.entries(obj) as unknown as Entries<T>;
 	}
 }
