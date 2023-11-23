1237b3b0854f4388d382479715e2080b9310a11b
Alex Komoroske <jkomoros@gmail.com>
2023-11-23 15:37:31 -0500

    Correct a typo in the synthesize-cluster prompt.
    The results still need some tweaking but are better than nothing.
    
    Part of #670.

--- src/suggestions/synthesize-cluster.ts
+++ src/suggestions/synthesize-cluster.ts
@@ -50,7 +50,7 @@ export const synthesizeCluster = async (args: SuggestorArgs) : Promise<Suggestio
 			items: cliqueCards.map(card => cardPlainContent(card)),
 			delimiter: '\n\n',
 			//TODO: this prompt still gets a lot of unncessary editing of words.
-			suffix: '----\nPlay back the essay word for word, omitting any direct duplication, keeping as much of the original\'s precide wording and content as possible.',
+			suffix: '----\nPlay back the essay word for word, omitting any direct duplication, keeping as much of the original\'s precise wording and content as possible.',
 			modelName: model
 		});
 		logger.info(`Prompt: ${prompt}`);
