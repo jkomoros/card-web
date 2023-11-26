jkomoros/card-web

commit fa56a8c980c0c0bb6562e4c6fce0bf24d0005aeb

Make it so users aren't asked during build if they want to generate SEO.
The actual generation is extremely quick, and the question was asked mid-way through a release, the only
question then, which could lead to you coming back and seeing it stuck.

Just remove it.

gulpfile.js
@@ -506,31 +506,6 @@ gulp.task(ASK_IF_WANT_BUILD, async (cb) => {
 
 });
 
-let wantsToSkipSEO = undefined;
-
-gulp.task(ASK_IF_WANT_SEO_PAGES, async (cb) => {
-	if (wantsToSkipSEO !== undefined) {
-		console.log('Already asked if the user wants an SEO');
-		cb();
-		return;
-	}
-	if (!SEO_ENABLED) {
-		console.log('SEO not enabled in config.SECRET.json');
-		cb();
-		return;
-	}
-	const response = await prompts({
-		type:'confirm',
-		name: 'value',
-		initial: false,
-		message: 'Do you want to skip SEO generation? This can take a long time and only needs to be re-run if published card content has changed.',
-	});
-
-	wantsToSkipSEO = response.value;
-	cb();
-
-});
-
 gulp.task(WARN_MAINTENANCE_TASKS, (cb) => {
 	console.log(`******************************************************************
 *                 WARNING 
@@ -556,8 +531,8 @@ gulp.task(BUILD_OPTIONALLY, async (cb) => {
 
 gulp.task(GENERATE_SEO_PAGES_OPTIONALLY, async (cb) => {
 	const task = gulp.task(GENERATE_SEO_PAGES);
-	if (wantsToSkipSEO) {
-		console.log('Skipping SEO because the user asked to skip it');
+	if (!SEO_ENABLED) {
+		console.log('Skipping SEO because it\'s not enabled');
 		cb();
 		return;
 	}

commit 1f0e2a3ef884d8aa5f6c09922b67cda4ddb03d9d

Fix gulp build errors.
Been broken since fa56a8c980c0c0bb6562e4c6fce0bf24d0005aeb

Part of #676.

gulpfile.js
@@ -146,7 +146,6 @@ const BUILD_OPTIONALLY = 'build-optionally';
 const ASK_IF_WANT_BUILD = 'ask-if-want-build';
 const GENERATE_SEO_PAGES = 'generate-seo-pages';
 const GENERATE_SEO_PAGES_OPTIONALLY = 'generate-seo-pages-optionally';
-const ASK_IF_WANT_SEO_PAGES = 'ask-if-want-seo-pages';
 const FIREBASE_ENSURE_PROD_TASK = 'firebase-ensure-prod';
 const FIREBASE_DEPLOY_TASK = 'firebase-deploy';
 const FIREBASE_SET_CONFIG_LAST_DEPLOY_AFFECTING_RENDERING = 'firebase-set-config-last-deploy-affecting-rendering';
@@ -551,7 +550,6 @@ gulp.task('dev-deploy',
 	gulp.series(
 		REGENERATE_FILES_FROM_CONFIG_TASK,
 		ASK_IF_WANT_BUILD,
-		ASK_IF_WANT_SEO_PAGES,
 		ASK_IF_DEPLOY_AFFECTS_RENDERING,
 		BUILD_OPTIONALLY,
 		GENERATE_SEO_PAGES_OPTIONALLY,
@@ -569,7 +567,6 @@ gulp.task('deploy',
 	gulp.series(
 		REGENERATE_FILES_FROM_CONFIG_TASK,
 		ASK_IF_WANT_BUILD,
-		ASK_IF_WANT_SEO_PAGES,
 		ASK_IF_DEPLOY_AFFECTS_RENDERING,
 		BUILD_OPTIONALLY,
 		GENERATE_SEO_PAGES_OPTIONALLY,