20243ad105bad7f1713308eb351bfaf046ecfffb
Alex Komoroske <jkomoros@gmail.com>
2023-12-02 08:20:43 -0500

    Field validator gets a cardType second argument.
    Part of #465.

--- src/types.ts
+++ src/types.ts
@@ -605,7 +605,7 @@ type CardFieldTypeConfiguration = {
 	//Validator takes the proposed stirng and returns an error string or '' if
 	//no problem. If it is provided and returns a non-empty string, then edits
 	//to that field will not be allowed to be saved.
-	validator? : (input? : string) => string,
+	validator? : (input : string | undefined, cardType : CardType) => string,
 	// extraRunDelimiter: if provided, then when deciding where to break runs, that
 	// character will also be considered as a run break (in addition to the default
 	// `\n`)
