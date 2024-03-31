ef58df894a17ecd34b1656f275e08d80cdcf4403
Alex Komoroske <jkomoros@gmail.com>
2024-03-31 08:21:44 -0700

    Start roughing in importer code.
    Part of #688.

--- src/actions/bulk-import.ts
+++ src/actions/bulk-import.ts
@@ -13,11 +13,34 @@ export const closeBulkImportDialog = () : SomeAction =>  ({
 	type: BULK_IMPORT_DIALOG_CLOSE
 });
 
+const extractTopLevelULs = (ele : HTMLElement) : HTMLUListElement[] => {
+	const result : HTMLUListElement[] = [];
+	for (const child of ele.children) {
+		if (child.tagName === 'UL') {
+			result.push(child as HTMLUListElement);
+			continue;
+		}
+		if (child.children.length && child instanceof HTMLElement) {
+			result.push(...extractTopLevelULs(child));
+		}
+	}
+	return result;
+};
+
+const processUL = (ul : HTMLUListElement) : string => {
+	//TODO: process and simplify
+	return ul.innerHTML;
+};
+
+const extractBodies = (content : string) : string[] => {
+	const ele = document.createElement('div');
+	ele.innerHTML = content;
+	const uls = extractTopLevelULs(ele);
+	return uls.map(processUL);
+};
+
 export const processBulkImportContent = (content : string) : SomeAction => {
-
-	//TODO: actually process.
-	const bodies = content.split('\n');
-
+	const bodies = extractBodies(content);
 	return {
 		type: BULK_IMPORT_SET_BODIES,
 		bodies
