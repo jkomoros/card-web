15d4179c5fff3865912f59ea25bba395d01dc256
Alex Komoroske <jkomoros@gmail.com>
2025-02-16 11:13:07 -0800

    create limit-warning connected component.
    Not yet instantiated anywhere.
    
    Part of #698.

---
+++ src/components/limit-warning.ts
@@ -0,0 +1,56 @@
+
+import { LitElement, html, css } from 'lit';
+import { customElement, state } from 'lit/decorators.js';
+import { connect } from 'pwa-helpers/connect-mixin.js';
+
+// This element is connected to the Redux store so it can render visited links
+import { store } from '../store.js';
+
+import { 
+	selectCardLimitReached
+} from '../selectors.js';
+
+import {
+	WARNING_ICON
+} from './my-icons.js';
+
+import {
+	State
+} from '../types.js';
+
+@customElement('limit-warning')
+class LimitWarning extends connect(store)(LitElement) {
+	
+
+	@state()
+		_cardLimitReached: boolean;
+
+	static override styles = [
+		css`
+			:host {
+				display:inline;
+			}
+		`
+	];
+	
+	override render() {
+		if (this._cardLimitReached) {
+			return html`
+				${WARNING_ICON}
+				<span>Card limit reached</span>
+			`;
+		}
+		return html``;
+	}
+
+	override stateChanged(state : State) {
+		this._cardLimitReached = selectCardLimitReached(state);
+	}
+
+}
+
+declare global {
+	interface HTMLElementTagNameMap {
+		'limit-warning': LimitWarning;
+	}
+}
