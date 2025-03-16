b735b9bb1e8b4a9f6cd299bdd4ebcc7af4a818d5
Alex Komoroske <jkomoros@gmail.com>
2025-03-16 07:19:10 -0700

    Fix lint errors preventing deploys in functions.
    Part of #710. Likely broke when we were working on stuff in #134.

--- functions/src/chat.ts
+++ functions/src/chat.ts
@@ -3,34 +3,34 @@ import {
 } from 'firebase-functions/v2/https';
 
 import {
-    CreateChatRequestData,
-    CreateChatResponseData
+	CreateChatRequestData,
+	CreateChatResponseData
 } from '../../shared/types';
 
 import {
-    throwIfUserMayNotUseAI
+	throwIfUserMayNotUseAI
 } from './common';
 
 export const createChat = async (request : CallableRequest<CreateChatRequestData>) : Promise<CreateChatResponseData> => {
-    const { auth } = request;
+	const { auth } = request;
 
-    if (!auth || !auth.uid) {
-        throw new Error('Unauthorized request');
-    }
+	if (!auth || !auth.uid) {
+		throw new Error('Unauthorized request');
+	}
 
-    try {
-        await throwIfUserMayNotUseAI(request);
-    } catch(err) {
-        return {
-            success: false,
-            error: String(err)
-        };
-    }
+	try {
+		await throwIfUserMayNotUseAI(request);
+	} catch(err) {
+		return {
+			success: false,
+			error: String(err)
+		};
+	}
 
-    //TOOD: actually create the thread.
+	//TOOD: actually create the thread.
 
-    return { 
-        success: false,
-        error: 'Not yet implemented'
-    };
+	return { 
+		success: false,
+		error: 'Not yet implemented'
+	};
 };
